891cca068f0abeb02a2bd26d799af73062820102
Huck Elliott <huck.elliott@forgerock.com>
2019-08-19 11:20:57 -0700

    changes ReCaptchaCallback into a vue component

---
+++ packages/platform-login/tests/unit/ReCaptchaCallback.spec.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import Sinon from 'sinon';
+import ReCaptchaCallback from '@/components/callbacks/ReCaptchaCallback';
+import i18n from '@/i18n';
+
+describe('ReCaptchaCallback.vue', () => {
+  let sandbox = null;
+
+  beforeEach(() => {
+    sandbox = Sinon.createSandbox();
+    sandbox.stub(ReCaptchaCallback, 'mounted').callsFake(() => {});
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('Load ReCaptchaCallback component', () => {
+    const wrapper = shallowMount(ReCaptchaCallback, {
+      i18n,
+      stubs: {
+        'router-link': true,
+      },
+    });
+
+    expect(wrapper.name()).to.equal('ReCaptchaCallback');
+  });
+});
