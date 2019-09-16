1c4e99262a3591bbc412b9718e2a41418621aa35
oliver.bradley <oliver.bradley@forgerock.com>
2019-09-16 14:46:40 -0700

    IAM-85 platform-ui - integrated Jest library for unit and snapshot testing

---
+++ packages/platform-enduser/jest.config.js
@@ -0,0 +1,8 @@
+const baseConfig = require('../../jest.config.base');
+
+process.env.VUE_CLI_BABEL_TARGET_NODE = true;
+process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
+
+module.exports = {
+	...baseConfig,
+};
