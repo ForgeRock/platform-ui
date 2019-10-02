d8cd95a01d744969da0ab9eb0a3b2f5062591601
Brendan Miller <brendan.miller@forgerock.com>
2019-10-02 16:13:44 -0700

    IAM-20 Update Dockerfiles for production deployment.
    Update and rename VUE_APP environment variables.
    Provide .env.production for production builds.
    Add entrypoint script.

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Entrypoint script for all ForgeRock Platform UI applications.
+
+echo "Replacing env vars in JS"
+echo ""
+echo "Setting AM URL as $AM_URL"
+echo "Setting IDM REST URL as $IDM_REST_URL"
+echo "Setting IDM ADMIN URL as $IDM_ADMIN_URL"
+echo "Setting LOGIN URL as $LOGIN_UI_URL"
+echo "Setting ENDUSER URL as $ENDUSER_UI_URL"
+echo "Setting ENDUSER CLIENT ID as $ENDUSER_CLIENT_ID"
+echo "Setting ADMIN CLIENT ID as $ADMIN_CLIENT_ID"
+
+# Replace env vars in JavaScript files
+for file in /usr/share/nginx/html/js/*.js
+do
+  echo "Processing $file ..."
+
+  # Use the existing JS file as template
+  if [ ! -f "/tmp/$(basename $file).tmpl" ]; then
+    cp "$file" "/tmp/$(basename $file).tmpl"
+  fi
+
+  envsubst '$AM_URL $IDM_REST_URL $IDM_ADMIN_URL $LOGIN_UI_URL $ENDUSER_UI_URL $ENDUSER_CLIENT_ID $ADMIN_CLIENT_ID' < "/tmp/$(basename $file).tmpl" > "$file"
+done
+
+echo "Starting Nginx"
+nginx -g 'daemon off;'
+
