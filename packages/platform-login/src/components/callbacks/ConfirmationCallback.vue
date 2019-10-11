adb03148b1a612ce8a12a1a41ac4c855bdaf1035
Huck Elliott <huck.elliott@forgerock.com>
2019-10-11 10:15:26 -0700

    IAM-90 part 2: adds ConfirmationCallback and TextOutputCallback (for Message Node), and support for PollingWaitCallback, HiddenValueCallback, MetadataCallback, SuccessUrl, and FailureUrl

---
+++ packages/platform-login/src/components/callbacks/ConfirmationCallback.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row text-left ml-1 mb-2">
+    <BButton
+      v-for="(option, index) in options"
+      :key="index"
+      class="btn btn btn-block btn-lg btn-primary mt-3"
+      variant="primary"
+      @click="setValue(index)">
+      {{ option }}
+    </BButton>
+  </div>
+</template>
+
+<script>
+import { BButton } from 'bootstrap-vue';
+
+export default {
+  components: {
+    BButton,
+  },
+  props: {
+    callback: {
+      type: Object,
+      required: true,
+    },
+    nextStep: {
+      type: Function,
+      required: true,
+    },
+  },
+  mounted() {
+    this.options = this.callback.getOptions();
+  },
+  data() {
+    return {
+      options: [],
+    };
+  },
+  methods: {
+    setValue(value) {
+      this.callback.setInputValue(value);
+      this.nextStep();
+    },
+  },
+};
+</script>
