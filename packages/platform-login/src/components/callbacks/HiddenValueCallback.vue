76e8892a7410e911a71884024044dc4ec685346e
Huck Elliott <huck.elliott@forgerock.com>
2019-10-11 10:15:26 -0700

    IAM-90 part 2: adds ConfirmationCallback and TextOutputCallback (for Message Node), and support for PollingWaitCallback, HiddenValueCallback, MetadataCallback, SuccessUrl, and FailureUrl

---
+++ packages/platform-login/src/components/callbacks/HiddenValueCallback.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row">
+    <input
+      type="hidden"
+      :name="name"
+      :ref="name"
+      :value="value"
+      @change="onChange()">
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    callback: {
+      type: Object,
+      required: true,
+    },
+    index: {
+      type: Number,
+      default: 0,
+    },
+  },
+  mounted() {
+    this.name = `callback_${this.index}`;
+
+    this.value = this.callback.getInputValue();
+  },
+  data() {
+    return {
+      name: '',
+      value: '',
+    };
+  },
+  methods: {
+    onChange() {
+      this.callback.setInputValue(this.value);
+    },
+  },
+};
+</script>
---
+++ packages/platform-login/src/components/callbacks/TextOutputCallback.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="row ml-1 mb-2"
+    ref="textOutputPanel">
+    <div
+      v-if="messageType === 'INFORMATION'"
+      class="text-center text-muted w-100">
+      {{ message }}
+    </div>
+    <BAlert
+      v-if="messageType === 'WARNING'"
+      variant="warning"
+      class="w-100"
+      show>
+      {{ message }}
+    </BAlert>
+    <BAlert
+      v-if="messageType === 'ERROR'"
+      variant="danger"
+      class="w-100"
+      show>
+      {{ message }}
+    </BAlert>
+  </div>
+</template>
+
+<script>
+import { BAlert } from 'bootstrap-vue';
+
+export default {
+  components: {
+    BAlert,
+  },
+  props: {
+    callback: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    switch (this.callback.getMessageType()) {
+    case 1:
+      this.messageType = 'WARNING';
+      break;
+    case 2:
+      this.messageType = 'ERROR';
+      break;
+    case 4:
+      this.messageType = 'SCRIPT';
+      break;
+    default:
+      this.messageType = 'INFORMATION';
+    }
+
+    if (this.messageType === 'SCRIPT') {
+      const el = document.createElement('script');
+      el.innerHTML = this.message;
+      this.$refs.textOutputPanel.appendChild(el);
+    }
+  },
+  data() {
+    return {
+      message: this.callback.getMessage(),
+      messageType: '',
+    };
+  },
+};
+</script>
