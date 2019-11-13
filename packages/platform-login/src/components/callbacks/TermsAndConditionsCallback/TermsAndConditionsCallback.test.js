2a55e204b71c03150d91e2c5da295e6625101420
Michael Wong <michael.wong@forgerock.com>
2019-11-13 15:43:10 -0800

    IAM-122 Add license header to files

--- packages/platform-login/src/components/callbacks/TermsAndConditionsCallback/TermsAndConditionsCallback.test.js
+++ packages/platform-login/src/components/callbacks/TermsAndConditionsCallback/TermsAndConditionsCallback.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2019 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
 import { shallowMount } from '@vue/test-utils';
 import TermsAndConditionsCallback from '@/components/callbacks/TermsAndConditionsCallback';
 import i18n from '@/i18n';
