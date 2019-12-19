92374e05e746003437cb76f035de175dfbabbb1d
Andrew Hertel <andrew.hertel@forgerock.com>
2019-12-19 14:53:23 -0800

    IAM-150

--- packages/platform-login/src/main.js
+++ packages/platform-login/src/main.js
@@ -8,7 +8,6 @@
 import Vue from 'vue';
 import ToggleButton from 'vue-js-toggle-button';
 import Notifications from 'vue-notification';
-import VeeValidate from 'vee-validate';
 import i18n from './i18n';
 import router from './router';
 import App from './App';
@@ -16,7 +15,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 Vue.config.productionTip = false;
-Vue.use(VeeValidate, { inject: false, fastExit: false });
 
 Vue.use(Notifications);
 Vue.use(ToggleButton);
