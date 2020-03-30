e4f93858a9c35dc1199cde9ec1821e6ca7102a79
oliver.bradley <oliver.bradley@forgerock.com>
2020-03-30 09:21:41 -0700

    IAM-259 Platform UI - create custom form generator

---
+++ packages/platform-shared/src/components/FormGenerator/renderers/RadioDisplay.vue
@@ -0,0 +1,61 @@
+<template>
+  <BFormGroup
+    :label="uiSchema.label">
+    <BFormRadioGroup
+      v-model="value"
+      :options="uiSchema.options"
+      text-field="name"
+      stacked />
+    <small
+      v-if="uiSchema.isHtml"
+      v-html="uiSchema.helpText"
+      class="form-text text-muted">
+      {{ uiSchema.helpText }}
+    </small>
+    <small
+      v-else
+      class="form-text text-muted">
+      {{ uiSchema.helpText }}
+    </small>
+  </BFormGroup>
+</template>
+
+<script>
+import {
+  BFormGroup,
+  BFormRadioGroup,
+} from 'bootstrap-vue';
+
+export default {
+  name: 'RadioDisplay',
+  components: {
+    BFormGroup,
+    BFormRadioGroup,
+  },
+  props: {
+    uiSchema: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    saveModel: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    value: {
+      get() {
+        return this.uiSchema.value;
+      },
+      set(newValue) {
+        this.$emit('update:model', {
+          model: this.saveModel,
+          value: newValue,
+        });
+      },
+    },
+  },
+};
+</script>
