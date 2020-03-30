d586e72273eb7f08f7f9436d04b61ad077e67518
oliver.bradley <oliver.bradley@forgerock.com>
2020-03-30 09:21:41 -0700

    IAM-259 Platform UI - create custom form generator

---
+++ packages/platform-shared/src/components/FormGenerator/renderers/ArrayDisplay.vue
@@ -0,0 +1,49 @@
+<template>
+  <FloatingLabelInput
+    type="multiselect"
+    v-model="value"
+    :disabled="uiSchema.disabled"
+    :select-options="uiSchema.options"
+    :label="uiSchema.label"
+    :help-text="uiSchema.helpText"
+    :is-html="uiSchema.isHtml"
+    :taggable="true"
+    :multiple="true"
+    class="mb-4 pr-1"
+  />
+</template>
+<script>
+import FloatingLabelInput from '@forgerock/platform-shared/src/components/FloatingLabelInput';
+
+export default {
+  name: 'ArrayDisplay',
+  components: {
+    FloatingLabelInput,
+  },
+  props: {
+    uiSchema: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    saveModel: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    value: {
+      get() {
+        return this.uiSchema.value;
+      },
+      set(newValue) {
+        this.$emit('update:model', {
+          model: this.saveModel,
+          value: newValue,
+        });
+      },
+    },
+  },
+};
+</script>
---
+++ packages/platform-shared/src/components/FormGenerator/renderers/NumberDisplay.vue
@@ -0,0 +1,46 @@
+<template>
+  <FloatingLabelInput
+    type="text"
+    v-model.number="value"
+    :disabled="uiSchema.disabled"
+    :label="uiSchema.label"
+    :help-text="uiSchema.helpText"
+    :is-html="uiSchema.isHtml"
+    class="mb-4 px-1" />
+</template>
+
+<script>
+import FloatingLabelInput from '@forgerock/platform-shared/src/components/FloatingLabelInput';
+
+export default {
+  name: 'NumberDisplay',
+  components: {
+    FloatingLabelInput,
+  },
+  props: {
+    uiSchema: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    saveModel: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    value: {
+      get() {
+        return this.uiSchema.value;
+      },
+      set(newValue) {
+        this.$emit('update:model', {
+          model: this.saveModel,
+          value: newValue,
+        });
+      },
+    },
+  },
+};
+</script>
