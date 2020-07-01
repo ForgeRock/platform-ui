e071ccc5a5b75f20ea7b8f1a241f3ce172ae1608
Adam Crockett <adam.crockett@forgerock.com>
2020-07-01 13:59:51 +0100

    bugfix/IAM-476 Added Media mixin to use real media queries (widely supported from IE10), this is much more efficient than watching for window resize and allows further media query types to fire JavaScript.
    bugfix/IAM-476 fixed accessibility issues allowing keyboard users to navigate the platform. changed event for functional prop ensuring that state and method come from a single source, this should reduce complexity and test effort.
    
    bugfix/IAM-476 fixed issue where large mobile shadow is retained when expanding the browser.
    
    bugfix/IAM-476 optimized the animation of sidemenu by using gpu supported css transform property, added will-change to add the animation to a new layer for buttery smooth animation performance with less jank.
    
    bugfix/IAM-476 stylelint whitelist transformY
    
    bugfix/IAM-476 refactored back to events but kept consistent names, TIL props in events out, may bad.
    
    bugfix/IAM-476 allows css to control the js media queries.
    
    bugfix/IAM-476 add link to matchMedia on MDN
    
    bugfix/IAM-476 match media is not implimented in JSDOM so Jest fails.

--- packages/platform-shared/src/components/Layout/Layout.test.js
+++ packages/platform-shared/src/components/Layout/Layout.test.js
@@ -9,6 +9,21 @@ import { shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
 import Layout from './index';
 
+// https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 const store = new Vuex.Store({
   state: {
     UserStore: {},
