3f9d62b4813f2c08ba15b48ccec5d5b56ff79929
Adam Crockett <adam.crockett@forgerock.com>
2020-07-01 13:59:51 +0100

    bugfix/IAM-476 Added Media mixin to use real media queries (widely supported from IE10), this is much more efficient than watching for window resize and allows further media query types to fire JavaScript.
    bugfix/IAM-476 fixed accessibility issues allowing keyboard users to navigate the platform. changed event for functional prop ensuring that state and method come from a single source, this should reduce complexity and test effort.
    
    bugfix/IAM-476 fixed issue where large mobile shadow is retained when expanding the browser.
    
    bugfix/IAM-476 optimized the animation of sidemenu by using gpu supported css transform property, added will-change to add the animation to a new layer for buttery smooth animation performance with less jank.
    
    bugfix/IAM-476 stylelint whitelist transformY
    
    bugfix/IAM-476 refactored back to events but kept consistent names, TIL props in events out, may bad.
    
    bugfix/IAM-476 allows css to control the js media queries.
    
    bugfix/IAM-476 add link to matchMedia on MDN
    
    bugfix/IAM-476 match media is not implimented in JSDOM so Jest fails.

---
+++ packages/platform-shared/src/mixins/Media/index.vue
@@ -0,0 +1,36 @@
+<script>
+import cssCustomProperties from '../../utils/cssCustomProperties';
+
+export default {
+  name: 'Media',
+  data: () => ({
+    /**
+     * @description IE 10 does not support custom properties so a fallback value is provided, elsewhere css variables from bootstrap controls this code.
+     * */
+    alias$: {
+      sm: `screen and (min-width: ${cssCustomProperties.getWithFallback('breakpoint-sm', '576px')})`,
+      'lt-sm': `screen and (max-width: ${cssCustomProperties.getWithFallback('breakpoint-sm', '576px')})`,
+      md: `screen and (min-width: ${cssCustomProperties.getWithFallback('breakpoint-md', '768px')})`,
+      'lt-md': `screen and (max-width: ${cssCustomProperties.getWithFallback('breakpoint-md', '768px')})`,
+      lg: `screen and (min-width: ${cssCustomProperties.getWithFallback('breakpoint-lg', '992px')})`,
+      'lt-lg': `screen and (max-width: ${cssCustomProperties.getWithFallback('breakpoint-lg', '992px')})`,
+      xl: `screen and (min-width: ${cssCustomProperties.getWithFallback('breakpoint-xl', '1200px')})`,
+      'lt-xl': `screen and (max-width: ${cssCustomProperties.getWithFallback('breakpoint-xl', '1200px')})`,
+      dark: '(prefers-color-scheme: dark)',
+      'no-animation': '(prefers-reduced-motion)',
+    },
+  }),
+  methods: {
+    /**
+     * @description takes a media query or an alias name to query media, acceptable queries can be any css media query.
+     * @link https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia
+     * */
+    media(qs) {
+      if (Object.keys(this.alias$).includes(qs)) {
+        return window.matchMedia(this.alias$[qs]);
+      }
+      return window.matchMedia(qs);
+    },
+  },
+};
+</script>
