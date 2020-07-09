36e25e63d75c7f944635f391376bd8245c26dca6
Lindsey Miller <lindsey.miller@forgerock.com>
2020-07-09 16:32:30 -0700

    IAM-431 don't allow non-admins to access the admin at all

---
+++ packages/platform-shared/src/views/Forbidden/index.vue
@@ -0,0 +1,59 @@
+<!-- Copyright 2020 ForgeRock AS. All Rights Reserved
+
+Use of this code requires a commercial software license with ForgeRock AS.
+or with one of its affiliates. All use shall be exclusively subject
+to such license between the licensee and ForgeRock AS. -->
+<template>
+  <BContainer
+    class="h-100 d-flex"
+    fluid>
+    <div
+      style="width: 550px;"
+      class="m-auto align-self-center text-center">
+      <BImg
+        :src="require('@forgerock/platform-shared/src/assets/images/locked.svg')"
+        width="112"
+        height="112"
+        alt="img"
+        class="mb-2" />
+      <p class="lead text-center font-weight-light text-muted mb-5">
+        {{ $t("pages.forbidden.notAuthorized") }}
+      </p>
+      <hr class="fr-accent">
+      <div class="text-center">
+        <BButton
+          variant="link"
+          @click="this.systemLogout">
+          {{ $t('pages.forbidden.signOut') }}
+        </BButton>
+      </div>
+    </div>
+  </BContainer>
+</template>
+
+<script>
+import LoginMixin from '@forgerock/platform-shared/src/mixins/LoginMixin';
+import {
+  BButton,
+  BContainer,
+  BImg,
+} from 'bootstrap-vue';
+
+/**
+ * @description The default 403 page when an unauthorized user tries to access the admin
+ */
+export default {
+  name: 'Forbidden',
+  components: {
+    BButton,
+    BContainer,
+    BImg,
+  },
+  mixins: [LoginMixin],
+
+  beforeRouteLeave(to, from, next) {
+    this.$store.dispatch('setUserId', null);
+    next();
+  },
+};
+</script>
