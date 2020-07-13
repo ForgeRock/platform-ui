959d599c25e7914b96a8b940e98a6a68da594686
Adam Crockett <adam.crockett@forgerock.com>
2020-07-13 15:10:45 +0100

    bugfix/IAM-535 removed all closed source license headers from platform-enduser and added MIT license file.
    bugfix/IAM-535 removed all closed source license headers from platform-enduser and added MIT license file.
    
    bugfix/IAM-535 bugfix/IAM-535 removed all closed source license headers from platform-admin and added MIT license file.
    
    bugfix/IAM-535 lint fix this vue.config.js file.
    
    bugfix/IAM-535 all licenses updated, including files erroneously missing licenses, find-copyright-problems script added to package.json (within the scope of this ticket) - when ran, will show all files that dont have a valid license header.
    
    bugfix/IAM-535 Restore banner and scss comment
    
    restored from master and added file header
    
    Restore preferences test to upstream master but include copyright header
    
    bugfix/IAM-535 files dont exist in upstream master.
    
    bugfix/IAM-535 reverted to upstream master and added copyright header.
    
    bugfix/IAM-535 files not in upstream.

--- packages/platform-enduser/e2e/tests/dashboard.test.e2e.js
+++ packages/platform-enduser/e2e/tests/dashboard.test.e2e.js
@@ -1,10 +1,11 @@
 /**
- * Copyright 2019-2020 ForgeRock AS. All Rights Reserved
+ * @license
+ * Copyright (c) 2020 ForgeRock. All rights reserved.
  *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
  */
+
 context('Dashboard View', () => {
   const userName = 'openidm-admin';
   beforeEach(() => {
