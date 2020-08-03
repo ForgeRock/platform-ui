1dab5294bda1a3de752f70e6774f6eb6513e198f
Cameron Martin <cameron.martin@forgerock.com>
2020-08-03 13:02:21 +0100

    Snapshot console errors.
    This automatically snapshots all console errors and warnings emitted during tests. This makes it hard to add new ones, because adding a new error adds a new snapshot file that will be visible in code reviews.
    
    The alternative is to fail tests when console errors are emitted, but this is more flexible as it allows tests to intentionally log, which is something we found necsesary in IG.
    
    This is essentially a port from IG: https://stash.forgerock.org/projects/OPENIG/repos/openig/commits/8534c1e32fe86c5a21a28f6569bc60bca50bdb5e

---
+++ config/jest/snapshot-errors.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2020 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+
+const path = require('path');
+
+const baseDir = path.dirname(path.dirname(__dirname));
+
+beforeEach(() => {
+    jest.spyOn(console, "error");
+    jest.spyOn(console, "warn");
+});
+
+
+function cleanFilepaths (string) {
+    return string.split(baseDir).join("");
+}
+
+function cleanMockCall (args) {
+    return args.map((arg) => (typeof arg === "string" ? cleanFilepaths(arg) : arg));
+}
+
+function snapshotCalls (fn, snapshotName) {
+    if (fn.mock.calls.length > 0) {
+        expect(fn.mock.calls.map(cleanMockCall)).toMatchSnapshot(snapshotName);
+    }
+}
+
+afterEach(() => {
+    snapshotCalls(console.error, "console-error-snapshot");
+    snapshotCalls(console.warn, "console-warn-snapshot");
+    console.error.mockRestore();
+    console.warn.mockRestore();
+});
