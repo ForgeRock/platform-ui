f6d14d171ec20bffd8c303219d672740c3e80d34
Andrew Hertel <andrew.hertel@forgerock.com>
2020-08-10 16:07:52 -0700

    IAM-650 Bring e2e Up to Date

--- packages/platform-login/e2e/tests/login.test.e2e.js
+++ packages/platform-login/e2e/tests/login.test.e2e.js
@@ -6,9 +6,11 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-context('Login View', () => {
+describe('Login View', () => {
+  const userName = Cypress.env('AM_USERNAME');
+  const password = Cypress.env('AM_PASSWORD');
   beforeEach(() => {
-    cy.visit('#/service/Login');
+    cy.visit(`${Cypress.config().baseUrl}/login/?realm=/#/`);
   });
 
   it('should have logo', () => {
@@ -17,34 +19,42 @@ context('Login View', () => {
 
   it('location should be at login', () => {
     cy.location().should((location) => {
-      expect(location.href).to.eq('http://localhost:8083/#/service/Login');
-      expect(location.host).to.eq('localhost:8083');
-      expect(location.hostname).to.eq('localhost');
-      expect(location.origin).to.eq('http://localhost:8083');
-      expect(location.pathname).to.eq('/');
-      expect(location.port).to.eq('8083');
-      expect(location.protocol).to.eq('http:');
+      expect(location.href).to.eq(`${Cypress.config().baseUrl}/login/?realm=/#/`);
+      expect(location.host).to.eq(Cypress.env('FQDN'));
+      expect(location.hostname).to.eq(Cypress.env('FQDN'));
+      expect(location.origin).to.eq(`${Cypress.config().baseUrl}`);
+      expect(location.pathname).to.eq('/login/');
+      expect(location.port).to.eq('');
+      expect(location.protocol).to.eq('https:');
     });
   });
 
   it('should have localhost login url', () => {
-    cy.url().should('eq', 'http://localhost:8083/#/service/Login');
+    cy.url().should('eq', `${Cypress.config().baseUrl}/login/?realm=/#/`);
   });
 
   it('should fail login with incorrect credentials', () => {
-    cy.get('[placeholder="Username"]').type('amadmin');
-    cy.get('[placeholder="Username"]').should('have.value', 'amadmin');
-    cy.get('[placeholder="Password"]').type('password', { force: true });
-    cy.get('[placeholder="Password"]').should('have.value', 'amadmin');
+    const errorMessage = 'Sorry that isn\'t right. Try again.';
+    cy.get('[placeholder="User Name:"]')
+      .type(userName)
+      .should('have.value', userName);
+    cy.get('[placeholder="Password:"]')
+      .type(userName, { force: true })
+      .should('have.value', userName);
     cy.get('.btn-primary').click();
-    cy.get('.fr-center-card').should('contain', 'Login Failure');
+    cy.get('.fr-center-card').should('contain', errorMessage);
   });
 
   it('should succeed login with valid credentials', () => {
-    cy.get('[placeholder="Username"]').type('amadmin');
-    cy.get('[placeholder="Username"]').should('have.value', 'amadmin');
-    cy.get('[placeholder="Password"]').type('password', { force: true });
-    cy.get('[placeholder="Password"]').should('have.value', 'password');
+    cy.get('[placeholder="User Name:"]')
+      .type(userName)
+      .should('have.value', userName);
+    cy.get('[placeholder="Password:"]')
+      .type(password, { force: true })
+      .should('have.value', password);
     cy.get('.btn-primary').click();
+    cy.location().should((location) => {
+      expect(location.href).to.not.eq(`${Cypress.config().baseUrl}/login/?realm=/#/`);
+    });
   });
 });
