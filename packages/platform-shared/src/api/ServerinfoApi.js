69241e6e746c96a3082a4b6e7c63240de1f6dfa2
Dave Ernsting <dave.ernsting@forgerock.com>
2020-08-27 14:32:34 +0100

    FRAAS-4039 Adds tenant settings view with global settings tab and cookie name display Also moves the log api key view from a dropdown menu item to being found within tenant settings.

--- packages/platform-shared/src/api/ServerinfoApi.js
+++ packages/platform-shared/src/api/ServerinfoApi.js
@@ -19,8 +19,7 @@ const idmInfoApi = {
   * Gets current version information including full version name
   * @returns {Promise}
   */
-// eslint-disable-next-line import/prefer-default-export
-export function getAmServerInfo(realm) {
+export function getAmServerVersionInfo(realm) {
   if (realm && realm !== 'root' && realm !== '/') {
     return generateAmApi({
       path: `realms/root/realms/${realm}`,
@@ -37,7 +36,27 @@ export function getAmServerInfo(realm) {
   );
 }
 
-// eslint-disable-next-line import/prefer-default-export
+/**
+  * Gets server info including iPlanetDirectoryPro cookie name
+  * @returns {Promise}
+  */
+export function getAmServerInfo(realm) {
+  if (realm && realm !== 'root' && realm !== '/') {
+    return generateAmApi({
+      path: `realms/root/realms/${realm}`,
+      apiVersion: 'protocol=2.1,resource=1.0',
+    }).get(
+      'serverinfo/*',
+      { withCredentials: true },
+    );
+  }
+
+  return generateAmApi(amInfoApi).get(
+    'serverinfo/*',
+    { withCredentials: true },
+  );
+}
+
 export function getIdmServerInfo() {
   return generateIdmApi(idmInfoApi).get(
     'info/version',
