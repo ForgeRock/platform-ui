f3ef239d96b57ccbfa6498f619c60753379a76a7
Dave Ernsting <dave.ernsting@forgerock.com>
2020-08-27 14:32:34 +0100

    FRAAS-4039 Adds tenant settings view with global settings tab and cookie name display Also moves the log api key view from a dropdown menu item to being found within tenant settings.

--- packages/platform-shared/src/components/ListGroup/ListGroup.test.js
+++ packages/platform-shared/src/components/ListGroup/ListGroup.test.js
@@ -5,20 +5,37 @@
  * or with one of its affiliates. All use shall be exclusively subject
  * to such license between the licensee and ForgeRock AS.
  */
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import ListGroup from './index';
 
 describe('ListGroup Component', () => {
   let wrapper;
-  beforeEach(() => {
-    wrapper = shallowMount(ListGroup, {
+
+  function mountListGroup(extraProps) {
+    wrapper = mount(ListGroup, {
       mocks: {
         $t: () => {},
       },
+      ...extraProps,
     });
-  });
+  }
 
   it('ListGroup successfully loaded', () => {
+    mountListGroup();
     expect(wrapper.name()).toEqual('ListGroup');
+  });
+
+  it('Adds a bottom margin to the top level card by default', () => {
+    mountListGroup();
+    expect(wrapper.find('.mb-3').exists()).toBe(true);
+  });
+
+  it('Does not add a bottom margin to the top level card when the borderless prop is true', () => {
+    mountListGroup({
+      propsData: {
+        noMargin: true,
+      },
+    });
+    expect(wrapper.find('.mb-3').exists()).toBe(false);
   });
 });
