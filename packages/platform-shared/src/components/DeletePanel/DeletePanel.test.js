e633b227901723fe51859e2980a692acbaae91ea
Dave Ernsting <dave.ernsting@forgerock.com>
2020-09-11 14:44:05 +0100

    IAM-320 refactors EditApplication and am schema code to be more re-usable This has mostly involved splitting some reusable sub-components out from the EditApplication hierarchy to platform shared. The way that EditApplication and the FormGenerator components interacted with the store has also been reworked so that the store objects for schemas from AM aren't so tied to oauth, allowing re-use.

---
+++ packages/platform-shared/src/components/DeletePanel/DeletePanel.test.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2020 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+import { shallowMount } from '@vue/test-utils';
+import DeletePanel from './index';
+
+
+describe('DeletePanel', () => {
+  it('DeletePanel successfully loaded', () => {
+    const wrapper = shallowMount(DeletePanel, {
+      mocks: {
+        $t: () => {},
+      },
+    });
+
+    expect(wrapper.name()).toEqual('DeletePanel');
+  });
+});
