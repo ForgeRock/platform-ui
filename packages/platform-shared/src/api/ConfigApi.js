5fea3d1a8e62dc780fea889311aa7fb1c67506d8
Michael Wong <michael.wong@forgerock.com>
2020-09-17 15:14:14 -0700

    IAM-789 Add display of password policy rules to platform-admin password reset

---
+++ packages/platform-shared/src/api/ConfigApi.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2019-2020 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+import { generateIdmApi } from './BaseApi';
+
+/**
+  * Returns UI configuration file and additionally includes the server configured language
+  *
+  * @returns {Promise}
+  */
+export function getUiConfig() {
+  return generateIdmApi().get('/info/uiconfig');
+}
+
+/**
+ * Retrieves a config file from the config store
+ *
+ * @returns {Promise} API promise with return config file
+ */
+export function getConfig(file) {
+  return generateIdmApi().get(`config/${file}`);
+}
+
+/**
+ * Puts config file into config store
+ *
+ * @returns {Promise} API promise with result from config put
+ */
+export function putConfig(file, config) {
+  return generateIdmApi().put(`config/${file}`, config);
+}
