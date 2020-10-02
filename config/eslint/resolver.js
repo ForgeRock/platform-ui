f248b5ed0b29ab44daf919d36bc8613c0200bb1d
Cameron Martin <cameron.martin@forgerock.com>
2020-10-02 12:35:26 +0100

    IAM-822 Decouple running eslint from build.
    Instead of eslint running as a vue cli plugin, it now runs as a jest runner. This has several advantages:
    
    * All files are now linted. Before no unit test, e2e tests or configuration files were linted.
    * The eslint vscode plugin, and probably other plugins, now correctly resolves imports for rules from [eslint-plugin-import](https://github.com/benmosher/eslint-plugin-import). Previously the module resolver was tied to the webpack build, but now it works independently.
    * Linting can now be run for platform-shared without running an otherwise unnecessary build for this package (this was the original issue).
    * Linting can now be run in watch mode alongside unit tests.

---
+++ config/eslint/resolver.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2020 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+
+/* eslint-disable no-restricted-syntax */
+const path = require('path');
+const fs = require('fs');
+const nodeResolver = require('eslint-import-resolver-node');
+
+exports.interfaceVersion = 2;
+
+const packagesRoot = path.resolve(__dirname, '..', '..', 'packages');
+const packageNames = fs.readdirSync(packagesRoot);
+
+exports.resolve = function resolve(source, file, config) {
+  if (source.startsWith('@/')) {
+    for (const packageName of packageNames) {
+      if (file.startsWith(path.join(packagesRoot, packageName))) {
+        const newSource = path.join(packagesRoot, packageName, 'src', source.substring(1));
+        return nodeResolver.resolve(newSource, file, config);
+      }
+    }
+  }
+
+  return nodeResolver.resolve(source, file, config);
+};
