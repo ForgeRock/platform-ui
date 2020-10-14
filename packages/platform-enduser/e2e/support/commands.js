6c97c9f61d4fa4f71b28f892c2004bcfc337afec
Andrew Hertel <andrew.hertel@forgerock.com>
2020-10-14 15:30:29 -0700

    IAM-786 Theming e2e & e2e Enhancements

--- packages/platform-enduser/e2e/support/commands.js
+++ packages/platform-enduser/e2e/support/commands.js
@@ -1,39 +1,12 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (userName, password = 'Welcome1', loginUrl = `${Cypress.config().baseUrl}/enduser/`) => {
+  indexedDB.deleteDatabase('appAuth');
+  cy.visit(loginUrl);
+  cy.get('[placeholder="User Name:"]').type(userName);
+  cy.get('[placeholder="Password:"]').type(password, { force: true });
+  cy.get('.btn-primary').click();
+  cy.get('h1', { timeout: 20000 });
+});
 
-// https://stackoverflow.com/questions/50750956/how-to-select-nth-item-inside-select-element-in-cypress
-Cypress.Commands.add(
-  'selectNth',
-  { prevSubject: 'element' },
-  (subject, pos, children) => {
-    cy.wrap(subject)
-      .children(children)
-      .eq(pos)
-      .then((e) => {
-        cy.wrap(subject).select(e.val());
-      });
-  },
-);
+Cypress.Commands.add('logout', () => {
+  cy.clearCookies();
+});
