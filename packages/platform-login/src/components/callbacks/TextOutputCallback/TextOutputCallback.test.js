0e23a347814e61704269f3014d8b265eab9d2f17
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2020-11-30 17:40:44 -0600

    IAM-958 - keep hidden value callback synced with dom

--- packages/platform-login/src/components/callbacks/TextOutputCallback/TextOutputCallback.test.js
+++ packages/platform-login/src/components/callbacks/TextOutputCallback/TextOutputCallback.test.js
@@ -12,34 +12,38 @@ import i18n from '@/i18n';
 
 describe('TextOutputCallback.vue', () => {
   let wrapper;
-
-  const mountComponent = ({ messageType } = {}) => {
-    const propsData = {
-      callback: {
-        getMessageType: () => messageType || 4,
-        getMessage: jest.fn(),
-      },
-      index: 5,
-    };
-    wrapper = mount(TextOutputCallback, {
-      i18n,
-      propsData,
-    });
-  };
+  let mountComponent;
 
   beforeEach(() => {
     wrapper = undefined;
+    mountComponent = ({ messageType, message, mockMethods }) => {
+      const step = {
+        getCallbacksOfType: jest.fn(),
+        find: jest.fn(),
+        setInputValue: jest.fn(),
+      };
+      step.getCallbacksOfType.mockReturnValue(step);
+      step.find.mockReturnValue(step);
+
+      const propsData = {
+        callback: {
+          getMessageType: () => messageType || 4,
+          getMessage: () => message || '',
+        },
+        index: 5,
+        step,
+      };
+      wrapper = mount(TextOutputCallback, {
+        i18n,
+        propsData,
+        methods: mockMethods || {},
+      });
+    };
   });
 
   it('Load TextOutputCallback component', () => {
     mountComponent({ messageType: '1' });
     expect(wrapper.name()).toEqual('TextOutputCallback');
-  });
-
-  it('Mounts QRCodeReader on window', () => {
-    mountComponent({ messageType: '1' });
-    expect(window.QRCodeReader).toBeDefined();
-    expect(window.QRCodeReader.createCode).toBeDefined();
   });
 
   it('Sets data messageType base on getMessageType', () => {
@@ -56,14 +60,74 @@ describe('TextOutputCallback.vue', () => {
     expect(wrapper.vm.$data.messageType).toBe('SCRIPT');
   });
 
-  it('It handles scripts by emit event and appending', async () => {
+  it('It handles scripts by emit event and invoking', () => {
+    const mockMethods = {
+      invokeScriptWithHelpers: jest.fn(),
+    };
+    mountComponent({ messageType: '4', mockMethods });
+
+    const emittedFunction = wrapper.emitted()['has-scripts'].pop()[0];
+
+    expect(emittedFunction).toEqual(expect.any(Function));
+
+    emittedFunction();
+
+    expect(mockMethods.invokeScriptWithHelpers).toHaveBeenCalled();
+  });
+
+  it('Mounts QRCodeReader on window', () => {
     mountComponent({ messageType: '4' });
-    wrapper.vm.$refs.textOutputPanel.appendChild = jest.fn();
 
-    expect(wrapper.emitted()['has-scripts'].pop()).toEqual([]);
+    const emittedFunction = wrapper.emitted()['has-scripts'].pop()[0];
+    emittedFunction();
 
-    await wrapper.vm.$nextTick();
+    expect(window.QRCodeReader).toBeDefined();
+    expect(window.QRCodeReader.createCode).toBeDefined();
+  });
 
-    expect(wrapper.vm.$refs.textOutputPanel.appendChild).toHaveBeenCalledWith(expect.any(Element));
+  it('Makes APIs accessible to backend script - API is one way (no returns)', () => {
+    const messageScript = `
+      window.APIs = {
+        loginHelpers
+      };`;
+    const mockMethods = {
+      setHiddenCallback: jest.fn(),
+    };
+    mountComponent({ messageType: '4', message: messageScript, mockMethods });
+
+    const emittedFunction = wrapper.emitted()['has-scripts'].pop()[0];
+    emittedFunction();
+
+    expect(window.APIs).toBeDefined();
+    expect(window.APIs.loginHelpers).toBeDefined();
+
+    expect(window.APIs.loginHelpers.disableNextButton).toBeDefined();
+    expect(window.APIs.loginHelpers.disableNextButton(true)).toBeUndefined();
+    expect(wrapper.emitted()['disable-next-button'].pop()).toEqual([true, 5]);
+
+    expect(window.APIs.loginHelpers.hideNextButton).toBeDefined();
+    expect(window.APIs.loginHelpers.hideNextButton(true)).toBeUndefined();
+    expect(wrapper.emitted()['hide-next-button'].pop()).toEqual([true]);
+
+    expect(window.APIs.loginHelpers.nextStep).toBeDefined();
+    expect(window.APIs.loginHelpers.nextStep('no param')).toBeUndefined();
+    expect(wrapper.emitted()['next-step'].pop()).toEqual([]);
+
+    expect(window.APIs.loginHelpers.nextStepCallback).toBeDefined();
+    expect(window.APIs.loginHelpers.nextStepCallback(() => {})).toBeUndefined();
+    expect(wrapper.emitted()['next-step-callback'].pop()).toEqual([expect.any(Function)]);
+
+    expect(window.APIs.loginHelpers.setHiddenCallback).toBeDefined();
+    expect(window.APIs.loginHelpers.setHiddenCallback('callbackName', 'callbackValue')).toBeUndefined();
+    expect(mockMethods.setHiddenCallback).toHaveBeenCalledWith('callbackName', 'callbackValue');
+  });
+
+  it('Mounts QRCodeReader on window', () => {
+    mountComponent({ messageType: '4' });
+
+    wrapper.vm.setHiddenCallback('callbackName', 'callbackValue');
+
+    expect(wrapper.vm.$props.step.getCallbacksOfType).toHaveBeenCalledWith('HiddenValueCallback');
+    expect(wrapper.vm.$props.step.setInputValue).toHaveBeenCalledWith('callbackValue');
   });
 });
