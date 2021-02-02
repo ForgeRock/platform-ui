e13a1c9c4ae6dc7e6bc6f3490f1534c215882dc1
Andrew Hertel <andrew.hertel@forgerock.com>
2021-02-02 12:15:53 -0800

    FRAAS-5256 Repushing custom domain after freeze

--- packages/platform-enduser/e2e/tests/enduser-theme.test.e2e.js
+++ packages/platform-enduser/e2e/tests/enduser-theme.test.e2e.js
@@ -1,15 +1,15 @@
 /**
- * @license
- * Copyright (c) 2020 ForgeRock. All rights reserved.
+ * Copyright (c) 2020-2021 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+
 import { createIDMTestUser } from '../managedApi.e2e';
 
 describe('Enduser Theming', () => {
   const platformLoginUrl = `${Cypress.config().baseUrl}/platform/`;
-  const locationUrl = `${Cypress.config().baseUrl}/platform/?realm=root#/realm`;
+  const locationUrl = `${Cypress.config().baseUrl}/platform/?realm=root#/realm/theme`;
   let enduserUserName = '';
   const adminUserName = Cypress.env('AM_USERNAME');
   const adminPassword = Cypress.env('AM_PASSWORD');
