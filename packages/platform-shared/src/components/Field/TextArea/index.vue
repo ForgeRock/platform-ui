d3d61d8e7bf309f3e92b294dde81ce33359c1572
Dave Ernsting <dave.ernsting@forgerock.com>
2021-02-03 14:37:43 +0000

    IAM-1039 Adds scripting support to platform-ui
    IAM-1042 Adds script API and route, as well as list page
    
    IAM-1041 Adds script editor
    
    IAM-367 Adds SelectWithActions component and an edit panel for scripted decision nodes.
    The new panel uses the selectWithActions component for script selection
    together with the ScriptEditorModal to allow viewing, editing, and adding
    scripts to be triggered from within the edit panel.
    
    IAM-1046 - script duplication
    
    IAM-1050 adds e2e tests

--- packages/platform-shared/src/components/Field/TextArea/index.vue
+++ packages/platform-shared/src/components/Field/TextArea/index.vue
@@ -1,8 +1,7 @@
-<!-- Copyright 2020 ForgeRock AS. All Rights Reserved
+<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
 
-Use of this code requires a commercial software license with ForgeRock AS.
-or with one of its affiliates. All use shall be exclusively subject
-to such license between the licensee and ForgeRock AS. -->
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
 <template>
   <FrInputLayout
     :id="id"
@@ -69,7 +68,7 @@ export default {
      */
     rows: {
       type: Number,
-      default: 5,
+      default: 3,
     },
     /**
      * specifies the visible width of a text area.
