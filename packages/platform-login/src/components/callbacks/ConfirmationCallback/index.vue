a29f64ca10f28370d8f03ad6e0ad08d02a935628
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2021-02-12 16:05:25 -0600

    IAM-1078 IE redirecting to form url on click

--- packages/platform-login/src/components/callbacks/ConfirmationCallback/index.vue
+++ packages/platform-login/src/components/callbacks/ConfirmationCallback/index.vue
@@ -1,12 +1,9 @@
-<!--
-Copyright (c) 2020 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
-of the MIT license. See the LICENSE file for details.
--->
-
+of the MIT license. See the LICENSE file for details. -->
 <template>
-  <div class="row text-left mb-2">
+  <div>
     <BButton
       v-for="(option, index) in options"
       :key="index"
