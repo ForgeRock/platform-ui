684ba12c3781cb8c1cfd0de141257c98ffa93bdc
Cameron Martin <cameron.martin@forgerock.com>
2021-02-18 15:27:40 +0000

    Add cypress testing library.
    This allows using selectors mentioned in the E2E testing doc, such as `findByRole`, `findByPlaceholder`, etc. See https://testing-library.com/docs/cypress-testing-library/intro/ for more information.
    
    I have converted a single usage for testing purposes. A more thorough refactoring of existing tests will follow.

--- packages/platform-login/e2e/support/commands.js
+++ packages/platform-login/e2e/support/commands.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2021 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +30,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+import '@testing-library/cypress/add-commands';
