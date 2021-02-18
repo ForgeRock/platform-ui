f2291f4607338e18ad056846c97fb6c2fee6e9da
Cameron Martin <cameron.martin@forgerock.com>
2021-02-18 15:27:40 +0000

    Add cypress testing library.
    This allows using selectors mentioned in the E2E testing doc, such as `findByRole`, `findByPlaceholder`, etc. See https://testing-library.com/docs/cypress-testing-library/intro/ for more information.
    
    I have converted a single usage for testing purposes. A more thorough refactoring of existing tests will follow.

--- packages/platform-enduser/e2e/support/commands.js
+++ packages/platform-enduser/e2e/support/commands.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2021 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import '@testing-library/cypress/add-commands';
+
 Cypress.Commands.add('login', (userName, password = 'Welcome1', loginUrl = `${Cypress.config().baseUrl}/enduser/`) => {
   indexedDB.deleteDatabase('appAuth');
   cy.visit(loginUrl);
