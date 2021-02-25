af2805c91676c4e0812c27e15622d2580ba48451
Andrew Hertel <andrew.hertel@forgerock.com>
2021-02-25 14:32:13 -0800

    FRAAS-5968 Remove ForgeRock Enduser References

--- packages/platform-enduser/src/App.vue
+++ packages/platform-enduser/src/App.vue
@@ -1,13 +1,12 @@
-<!--
-Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
-of the MIT license. See the LICENSE file for details.
--->
+of the MIT license. See the LICENSE file for details. -->
 <template>
   <div>
     <FrLayout
       :menu-items="menuItems"
+      :is-enduser="true"
       :version="version">
       <RouterView :key="this.$route.fullPath" />
     </FrLayout>
