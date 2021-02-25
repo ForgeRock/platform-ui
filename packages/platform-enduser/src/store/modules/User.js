6c43c566f181d93d51ca1270f1723b219f33b7ad
Andrew Hertel <andrew.hertel@forgerock.com>
2021-02-25 14:32:13 -0800

    FRAAS-5968 Remove ForgeRock Enduser References

--- packages/platform-enduser/src/store/modules/User.js
+++ packages/platform-enduser/src/store/modules/User.js
@@ -1,9 +1,8 @@
 /**
- * Copyright 2019-2020 ForgeRock AS. All Rights Reserved
+ * Copyright (c) 2019-2021 ForgeRock. All rights reserved.
  *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
  */
 
 /**
@@ -52,7 +51,7 @@ const getters = {
 
     return {
       name: userFullName,
-      company: state.company || 'ForgeRock',
+      company: state.company,
       email: state.email,
       roles: state.roles || [],
     };
