aa45958ff120f423df713d7c8507285ce671a7b7
Jason Browne <jason.browne@forgerock.com>
2021-03-10 14:52:06 -0800

    IAM-1099 Add platform UI profile avatar feature

--- packages/platform-enduser/src/components/profile/profile.test.js
+++ packages/platform-enduser/src/components/profile/profile.test.js
@@ -1,6 +1,5 @@
 /**
- * @license
- * Copyright (c) 2020 ForgeRock. All rights reserved.
+ * Copyright (c) 2020-2021 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -17,9 +16,9 @@ localVue.use(Vuex);
 localVue.use(BootstrapVue);
 
 Profile.components['fr-consent'] = jest.fn();
-Profile.components['fr-edit-personal-info'] = jest.fn();
 Profile.components['fr-account-controls'] = jest.fn();
 Profile.components['fr-account-security'] = jest.fn();
+Profile.components['fr-edit-profile'] = jest.fn();
 
 describe('Profile.vue', () => {
   let wrapper;
@@ -27,7 +26,20 @@ describe('Profile.vue', () => {
     const getUserProfile = jest.fn();
     const store = new Vuex.Store({
       state: {
-        UserStore: {},
+        UserStore: {
+          userId: null,
+          managedResource: null,
+          roles: null,
+          internalUser: false,
+          adminUser: false,
+          profile: {},
+          schema: {},
+          access: [],
+          givenName: '',
+          sn: '',
+          email: '',
+          userName: '',
+        },
         ApplicationStore: {},
       },
     });
@@ -38,6 +50,9 @@ describe('Profile.vue', () => {
       store,
       computed: {
         fullName() {
+          return '';
+        },
+        profileImage() {
           return '';
         },
       },
