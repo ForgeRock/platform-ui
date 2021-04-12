26011048cc373b38401b0a693f39a84832d98de8
Ingrid van den Berg <ingrid.vandenberg@forgerock.com>
2021-04-12 14:45:04 +0100

    IAM-996 - removed extra pr-5 from SchemaStateButton

--- packages/platform-shared/src/components/SchemaStateButton/index.vue
+++ packages/platform-shared/src/components/SchemaStateButton/index.vue
@@ -4,8 +4,7 @@ This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <BDropdown
-    variant="outline-secondary"
-    toggle-class="pr-5">
+    variant="outline-secondary">
     <template
       v-slot:button-content>
       <i
