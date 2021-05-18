90c416bd46af9c8bf0c59ec030dcb40576db13b0
Michael Wong <michael.wong@forgerock.com>
2021-05-18 16:29:10 -0700

    IAM-1062 Refactor navbar and layout to no longer use store

--- packages/platform-enduser/src/App.test.js
+++ packages/platform-enduser/src/App.test.js
@@ -1,10 +1,10 @@
 /**
- * @license
- * Copyright (c) 2020 ForgeRock. All rights reserved.
+ * Copyright (c) 2020-2021 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+
 import BootstrapVue from 'bootstrap-vue';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
@@ -18,10 +18,13 @@ localVue.use(BootstrapVue);
 describe('App.vue', () => {
   const store = new Vuex.Store({
     state: {
-      UserStore: {},
+      UserStore: {
+        userDetails: {},
+      },
       ApplicationStore: {},
     },
     getters: {
+      'UserStore/userDetails': (state) => state.UserStore.userDetails,
       UserStore: (state) => state.UserStore,
       ApplicationStore: (state) => state.ApplicationStore,
     },
