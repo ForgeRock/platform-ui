cbd06709f9781b45de0415c6123f9d0afc9f3453
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2021-07-07 18:04:15 -0500

    IAM-1259 End User WCAG Changes

--- packages/platform-shared/src/components/Field/Switch/index.vue
+++ packages/platform-shared/src/components/Field/Switch/index.vue
@@ -4,7 +4,9 @@ This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <div class="d-flex flex-row-reverse">
-    <label :class="[{'mt-2': size !== 'sm'}, 'mb-0 text-secondary w-100']">
+    <label
+      v-if="!srOnlyLabel"
+      :class="[{'mt-2': size !== 'sm'}, 'mb-0 text-secondary w-100']">
       <span
         :id="`helppopover-${name}`"
         tabindex="0"
@@ -19,7 +21,12 @@ of the MIT license. See the LICENSE file for details. -->
       role="switch"
       switch
       :name="name"
-      :size="size" />
+      :size="size">
+      <span
+        class="sr-only">
+        {{ switchLabel }}
+      </span>
+    </BFormCheckbox>
   </div>
 </template>
 
@@ -43,6 +50,13 @@ export default {
     BFormCheckbox,
   },
   props: {
+    /**
+     * Set label to be screen reader only
+     */
+    srOnlyLabel: {
+      type: Boolean,
+      default: false,
+    },
     /**
      * Size of toggle switch
      */
