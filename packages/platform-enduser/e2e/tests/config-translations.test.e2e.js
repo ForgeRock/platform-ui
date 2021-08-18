e288f071018a8b7730eb3dab3e0d7ddab00a3f02
Michael Wong <michael.wong@forgerock.com>
2021-08-18 13:38:43 -0700

    IAM-1371 Add e2e testing for config translations in login and enduser

---
+++ packages/platform-enduser/e2e/tests/config-translations.test.e2e.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2021 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { random } from 'lodash';
+import { createIDMUser } from '../api/managedApi.e2e';
+import addOverrides, { deleteOverrides } from '../api/localizationApi.e2e';
+
+const enTranslations = {
+  enduser: {
+    sideMenu: {
+      profile: 'Profile Test',
+    },
+    pages: {
+      dashboard: {
+        widgets: {
+          welcome: {
+            welcomeMessage: 'Welcome Message Test',
+          },
+        },
+      },
+    },
+  },
+  shared: {
+    sideMenu: {
+      dashboard: 'Dashboard Test',
+    },
+  },
+};
+
+describe('Enduser config translations', () => {
+  let accessToken;
+  let userName;
+
+  before(() => {
+    // create test user
+    userName = `e2eTestUser${random(Number.MAX_SAFE_INTEGER)}`;
+    createIDMUser({ userName });
+
+    // get admin access token
+    cy.intercept('POST', '/am/oauth2/access_token').as('getAccessToken');
+    cy.login(Cypress.env('AM_USERNAME'), Cypress.env('AM_PASSWORD'), `${Cypress.config().baseUrl}/platform/`);
+
+    cy.wait('@getAccessToken').then(({ response }) => {
+      // add config translation override
+      accessToken = response.body.access_token;
+      addOverrides(accessToken, 'en', enTranslations);
+      cy.logout();
+    });
+  });
+
+  it('should override text based on translations stored in config', () => {
+    cy.login(userName);
+    cy.findByRole('link', { name: 'Profile Test' }).should('exist');
+    cy.findByRole('link', { name: 'Dashboard Test' }).should('exist');
+    cy.findByText('Welcome Message Test').should('exist');
+    cy.logout();
+  });
+
+  // cleanup
+  after(() => {
+    deleteOverrides(accessToken, 'en');
+  });
+});
--- packages/platform-login/e2e/support/commands.js
+++ packages/platform-login/e2e/support/commands.js
@@ -5,30 +5,20 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
 import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('login', () => {
+  const loginUrl = `${Cypress.config().baseUrl}/platform/`;
+  const adminUserName = Cypress.env('AM_USERNAME');
+  const adminPassword = Cypress.env('AM_PASSWORD');
+  indexedDB.deleteDatabase('appAuth');
+  cy.visit(loginUrl);
+  cy.get('[placeholder="User Name:"]').type(adminUserName);
+  cy.get('[placeholder="Password:"]').type(adminPassword, { force: true });
+  cy.get('.btn-primary').click();
+  cy.get('h1', { timeout: 20000 });
+});
+
+Cypress.Commands.add('logout', () => {
+  cy.clearCookies();
+});
