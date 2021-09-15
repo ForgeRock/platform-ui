2e7a7962a6eb48f93bd1d328850b6ae33d586fa0
Michael Wong <michael.wong@forgerock.com>
2021-09-15 15:56:15 -0700

    FRAAS-8133 platform-login prevent render of logo, page header, page description, and callbacks until theme has loaded

--- packages/platform-login/src/App.vue
+++ packages/platform-login/src/App.vue
@@ -27,6 +27,7 @@ of the MIT license. See the LICENSE file for details. -->
           :logo-height="logoHeight"
           :logo-path="logo"
           :key="$route.fullPath"
+          :theme-loading="theme === null"
           @set-theme="setupTheme" />
       </Transition>
     </div>
