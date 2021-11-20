ea12b22a11e8174184cb199afcd90500437c1507
Angel Hernandez <angel.hernandez@forgerock.com>
2021-11-19 17:28:03 -0700

    IAM-1690 removed ghost from 404 page and replaced with 404 text

--- packages/platform-shared/src/views/NotFound/index.vue
+++ packages/platform-shared/src/views/NotFound/index.vue
@@ -11,21 +11,9 @@ to such license between the licensee and ForgeRock AS. -->
     <div class="p-5 my-5 container">
       <div
         class="m-auto align-self-center text-center">
-        <div class="fr-speech-bubble">
-          <p>
-            <strong class="text-muted">
-              {{ ghostMessage }}
-            </strong>
-          </p>
-          <div class="fr-speech-arrow" />
-        </div>
-        <BImg
-          @click="ghostMessage === '404' ? ghostMessage = 'Boo' : ghostMessage = '...'"
-          :src="require('@forgerock/platform-shared/src/assets/images/ghost.svg')"
-          width="112"
-          height="112"
-          alt="img"
-          class="fr-ghost mb-4" />
+        <h1 class="display-1">
+          404
+        </h1>
         <p class="lead text-center">
           {{ $t("pages.notFound.couldNotFind") }}
         </p>
@@ -38,13 +26,6 @@ to such license between the licensee and ForgeRock AS. -->
             </BButton>
           </RouterLink>
         </div>
-        <BImg
-          @click="ghostMessage = '404'"
-          :src="require('@forgerock/platform-shared/src/assets/images/ghost-shadow.svg')"
-          width="112"
-          height="112"
-          alt="img"
-          class="fr-shadow" />
       </div>
     </div>
   </BContainer>
@@ -55,7 +36,6 @@ import BreadcrumbMixin from '@forgerock/platform-shared/src/mixins/BreadcrumbMix
 import {
   BButton,
   BContainer,
-  BImg,
 } from 'bootstrap-vue';
 /**
  * @description The default 404 page when Vue router is unable to locate a route.
@@ -65,7 +45,6 @@ export default {
   components: {
     BButton,
     BContainer,
-    BImg,
   },
   mixins: [
     BreadcrumbMixin,
@@ -92,86 +71,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-@keyframes nudge {
-  0%,
-  100% {
-    transform: translate(0, 0);
-  }
-
-  25% {
-    transform: translate(15px, 5px);
-  }
-
-  50% {
-    transform: translate(20px, 0);
-  }
-
-  80% {
-    transform: translate(10px, 5px);
-  }
-
-  90% {
-    transform: translate(-10px, 0);
-  }
-}
-
-#notFoundContainer {
-  .fr-ghost,
-  .fr-shadow {
-    animation: nudge 5s linear infinite alternate;
-    cursor: pointer;
-  }
-
-  .fr-speech-bubble {
-    animation: nudge 5s linear infinite alternate;
-    font-size: 20px;
-    font-weight: 300;
-    background: $card-bg;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    border-radius: 4px;
-    box-shadow: 4px 4px 0 rgba(0, 0, 0, 0.2);
-    line-height: 1.3;
-    margin: 0 auto 15px;
-    max-width: 110px;
-    padding: 15px;
-    position: relative;
-    right: 70px;
-
-    p {
-      margin-bottom: 10px;
-
-      &:last-of-type {
-        margin-bottom: 0;
-      }
-    }
-
-    .fr-speech-arrow {
-      border-left: 21px solid transparent;
-      border-top: 20px solid rgba(0, 0, 0, 0.2);
-      bottom: -25px;
-      position: absolute;
-      right: 15px;
-
-      &::before {
-        border-left: 23px solid transparent;
-        border-top: 23px solid $card-bg;
-        bottom: 2px;
-        content: '';
-        position: absolute;
-        right: 5px;
-      }
-
-      &::after {
-        border-left: 21px solid transparent;
-        border-top: 23px solid $card-bg;
-        bottom: 4px;
-        content: '';
-        position: absolute;
-        right: 6px;
-      }
-    }
-  }
-}
-</style>
