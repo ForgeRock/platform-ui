079dc45644a327a933d2d5dffa48eadf1cffb311
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2021-12-17 18:19:37 -0600

    IAM-1532 ReCaptchaV3

---
+++ packages/platform-login/src/components/callbacks/ReCaptchaCallback/ReCaptchaV3.vue
@@ -0,0 +1,52 @@
+<!-- Copyright (c) 2022 ForgeRock. All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
+<template>
+  <div />
+</template>
+
+<script>
+/**
+ * @description Selfservice stage for multiple selfservice flows, displays a google captcha
+ *
+ * */
+import Vue from 'vue';
+import NotificationMixin from '@forgerock/platform-shared/src/mixins/NotificationMixin';
+import { VueReCaptcha } from 'vue-recaptcha-v3';
+
+export default {
+  name: 'ReCaptchaV3',
+  mixins: [
+    NotificationMixin,
+  ],
+  props: {
+    callback: {
+      type: Object,
+      required: true,
+    },
+  },
+  created() {
+    const recaptchaSiteKey = this.callback.getSiteKey();
+    Vue.use(VueReCaptcha, {
+      siteKey: recaptchaSiteKey,
+    });
+    this.$emit('next-step-callback', this.runRecaptcha);
+  },
+  methods: {
+    runRecaptcha() {
+      return this.$recaptchaLoaded()
+        .then(() => this.$recaptcha('login')
+          .then((token) => {
+            this.callback.setInputValue(token);
+          }))
+        .catch(() => {
+          this.handleCaptchaError();
+        });
+    },
+    handleCaptchaError() {
+      this.displayNotification('IDMMessages', 'error', this.$t('pages.selfservice.captchaError'));
+    },
+  },
+};
+</script>
