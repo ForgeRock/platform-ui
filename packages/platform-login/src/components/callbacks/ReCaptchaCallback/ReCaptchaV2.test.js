aef12ea9be7804f0e8fe4d016809fffb737cf9dd
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2021-12-17 18:19:37 -0600

    IAM-1532 ReCaptchaV3

---
+++ packages/platform-login/src/components/callbacks/ReCaptchaCallback/ReCaptchaV2.test.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2022 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { shallowMount } from '@vue/test-utils';
+import ReCaptchaCallback from '@/components/callbacks/ReCaptchaCallback/ReCaptchaV2';
+import i18n from '@/i18n';
+
+describe('ReCaptchaV2.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    window.document.head.appendChild = jest.fn();
+    wrapper = shallowMount(ReCaptchaCallback, {
+      i18n,
+      stubs: {
+        'router-link': true,
+      },
+      propsData: {
+        callback: {
+          getSiteKey: jest.fn(() => 'siteKey'),
+          setInputValue: jest.fn(),
+        },
+        index: 5,
+      },
+    });
+  });
+
+  it('Load ReCaptchaV2 component', () => {
+    expect(wrapper.name()).toEqual('ReCaptchaV2');
+  });
+
+  it('Sets data', () => {
+    expect(wrapper.vm.$data.name).toBe('callback_5');
+    expect(wrapper.vm.$props.callback.getSiteKey).toHaveBeenCalled();
+    expect(wrapper.vm.$data.recaptchaSiteKey).toBe('siteKey');
+  });
+
+  it('Appends Script to the dom', () => {
+    expect(window.document.head.appendChild).toHaveBeenCalledWith(expect.any(Element));
+  });
+
+  it('Handles recaptcha callback', () => {
+    wrapper.vm.handleCaptchaCallback('abcd');
+    expect(wrapper.vm.$data.value).toEqual('abcd');
+    expect(wrapper.vm.$props.callback.setInputValue).toHaveBeenCalledWith('abcd');
+  });
+});
