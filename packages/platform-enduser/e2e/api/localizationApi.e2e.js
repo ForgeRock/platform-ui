7436684508dbc88ed94017f6693ed4030c2895c7
Dave Ernsting <dave.ernsting@forgerock.com>
2021-12-22 13:41:22 +0000

    IAM-1809 updates bulk import E2E tests to work against cloud IAM-1810 updates platform admin E2E tests that create users for cloud

--- packages/platform-enduser/e2e/api/localizationApi.e2e.js
+++ packages/platform-enduser/e2e/api/localizationApi.e2e.js
@@ -1,34 +1,34 @@
 /**
- * Copyright (c) 2021 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2022 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
 /**
- * Delete a config translation file
- */
-export function deleteOverrides(accessToken = Cypress.env('ACCESS_TOKEN').access_token, locale) {
-  return cy.request({
-    method: 'DELETE',
-    url: `https://${Cypress.env('FQDN')}/openidm/config/uilocale/${locale}`,
-    headers: {
-      authorization: `Bearer ${accessToken}`,
-    },
-  });
-}
-
-/**
  * Add a config translation file
  */
-export default function (accessToken = Cypress.env('ACCESS_TOKEN').access_token, locale, body) {
+export function addOverrides(locale, body) {
   return cy.request({
     method: 'PUT',
     url: `https://${Cypress.env('FQDN')}/openidm/config/uilocale/${locale}`,
     headers: {
-      authorization: `Bearer ${accessToken}`,
+      authorization: `Bearer ${Cypress.env('ACCESS_TOKEN').access_token}`,
       'content-type': 'application/json',
     },
     body,
+  });
+}
+
+/**
+ * Delete a config translation file
+ */
+export function deleteOverrides(locale) {
+  return cy.request({
+    method: 'DELETE',
+    url: `https://${Cypress.env('FQDN')}/openidm/config/uilocale/${locale}`,
+    headers: {
+      authorization: `Bearer ${Cypress.env('ACCESS_TOKEN').access_token}`,
+    },
   });
 }
--- packages/platform-enduser/e2e/tests/config-translations.test.e2e.js
+++ packages/platform-enduser/e2e/tests/config-translations.test.e2e.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2022 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -7,22 +7,13 @@
 
 import { random } from 'lodash';
 import filterTests from '../../../../e2e/filter_tests';
-import { createIDMUser } from '../api/managedApi.e2e';
-import addOverrides, { deleteOverrides } from '../api/localizationApi.e2e';
+import { createIDMUser, deleteIDMUser } from '../api/managedApi.e2e';
+import { addOverrides, deleteOverrides } from '../api/localizationApi.e2e';
 
 const enTranslations = {
   enduser: {
     sideMenu: {
       profile: 'Profile Test',
-    },
-    pages: {
-      dashboard: {
-        widgets: {
-          welcome: {
-            welcomeMessage: 'Welcome Message Test',
-          },
-        },
-      },
     },
   },
   shared: {
@@ -32,39 +23,36 @@ const enTranslations = {
   },
 };
 
-filterTests(['forgeops'], () => {
+filterTests(['forgeops', 'cloud'], () => {
   describe('Enduser config translations', () => {
-    let accessToken;
     let userName;
+    let userId;
 
-    before(() => {
-      // create test user
-      userName = `e2eTestUser${random(Number.MAX_SAFE_INTEGER)}`;
-      createIDMUser({ userName });
-
-      // get admin access token
-      cy.intercept('POST', '/am/oauth2/access_token').as('getAccessToken');
-      cy.login(Cypress.env('AM_USERNAME'), Cypress.env('AM_PASSWORD'), `${Cypress.config().baseUrl}/platform/`);
-
-      cy.wait('@getAccessToken').then(({ response }) => {
+    beforeEach(() => {
+      // Login as an admin to get an access token
+      cy.loginAsAdmin().then(() => {
         // add config translation override
-        accessToken = response.body.access_token;
-        addOverrides(accessToken, 'en', enTranslations);
-        cy.logout();
+        addOverrides('en', enTranslations);
+
+        // create test user
+        userName = `e2eTestUser${random(Number.MAX_SAFE_INTEGER)}`;
+        createIDMUser({ userName }).then(({ body: { _id: responseUserId } }) => {
+          userId = responseUserId;
+          cy.logout();
+        });
       });
     });
 
     it('should override text based on translations stored in config', () => {
-      cy.login(userName);
+      cy.loginAsEnduser(userName);
       cy.findByRole('link', { name: 'Profile Test' }).should('exist');
       cy.findByRole('link', { name: 'Dashboard Test' }).should('exist');
-      cy.findByText('Welcome Message Test').should('exist');
-      cy.logout();
     });
 
     // cleanup
     after(() => {
-      deleteOverrides(accessToken, 'en');
+      deleteOverrides('en');
+      deleteIDMUser(userId);
     });
   });
 });
