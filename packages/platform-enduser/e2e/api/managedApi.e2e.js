761cd86594cb8280fa97553a6a29503eb1bf11db
Dave Ernsting <dave.ernsting@forgerock.com>
2021-12-22 13:41:22 +0000

    IAM-1809 updates bulk import E2E tests to work against cloud IAM-1810 updates platform admin E2E tests that create users for cloud

--- packages/platform-enduser/e2e/api/managedApi.e2e.js
+++ packages/platform-enduser/e2e/api/managedApi.e2e.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2022 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -7,10 +7,17 @@
 
 import { random } from 'lodash';
 
-export function addRoleMember(roleId, userId, accessToken = Cypress.env('ACCESS_TOKEN').access_token, resourceType = 'internal') {
+export function addRoleMember(roleId, userId, resourceType = 'internal') {
+  const accessToken = Cypress.env('ACCESS_TOKEN').access_token;
+  let requestResourceType;
+  if (resourceType === 'internal') {
+    requestResourceType = 'role';
+  } else {
+    requestResourceType = Cypress.env('IS_FRAAS') ? 'alpha_role' : 'role';
+  }
   return cy.request({
     method: 'PATCH',
-    url: `https://${Cypress.env('FQDN')}/openidm/${resourceType}/role/${roleId}`,
+    url: `https://${Cypress.env('FQDN')}/openidm/${resourceType}/${requestResourceType}/${roleId}`,
     headers: {
       authorization: `Bearer ${accessToken}`,
       'content-type': 'application/json',
@@ -19,41 +26,13 @@ export function addRoleMember(roleId, userId, accessToken = Cypress.env('ACCESS_
       field: `/${resourceType === 'internal' ? 'authzMembers' : 'members'}/-`,
       operation: 'add',
       value: {
-        _ref: `managed/user/${userId}`,
+        _ref: `managed/${Cypress.env('IS_FRAAS') ? 'alpha_user' : 'user'}/${userId}`,
         _refProperties: {},
       },
     }],
   });
 }
 
-/**
- * Creates am identity. Works, but not sure if we should use this
- */
-export function createAMUser() {
-  return cy.request({
-    method: 'POST',
-    url: `https://${Cypress.env('FQDN')}/am/json/realms/root/authenticate`,
-    headers: {
-      'X-OpenAM-Username': Cypress.env('AM_USERNAME'),
-      'X-OpenAM-Password': Cypress.env('AM_PASSWORD'),
-      'content-type': 'application/json',
-      'accept-api-version': 'resource=2.1',
-    },
-  }).then(() => cy.request({
-    method: 'POST',
-    url: `https://${Cypress.env('FQDN')}/am/json/realms/root/users/?_action=create`,
-    body: {
-      username: 'ForgerockDemo',
-      userpassword: 'forgerock',
-      mail: 'forgerockdemo@example.com',
-    },
-    headers: {
-      'content-type': 'application/json',
-      'x-requested-with': 'XMLHttpRequest',
-      'accept-api-version': 'protocol=2.1,resource=3.0',
-    },
-  }));
-}
 export function createIDMResource(resourceType = 'managed', resourceName, body, accessToken = Cypress.env('ACCESS_TOKEN').access_token) {
   return cy.request({
     method: 'POST',
@@ -70,36 +49,34 @@ export function createIDMResource(resourceType = 'managed', resourceName, body, 
  * Use idm provisioning token to create super-user
  * Creates a user with
  * username: 'e2eTestUser<RandomNumber>',
- * password: 'Welcome1',
+ * password: 'Welcome1!',
  * First Name: 'First'
  * Last Name: 'Last'
  */
 export function createIDMUser(userBody) {
   const body = {
     userName: `e2eTestUser${random(Number.MAX_SAFE_INTEGER)}`,
-    password: 'Welcome1',
+    password: 'Welcome1!',
     givenName: 'First',
     sn: 'Last',
     mail: 'forgerockdemo@example.com',
     ...userBody,
   };
-  return createIDMResource('managed', 'user', body);
+  return createIDMResource('managed', Cypress.env('IS_FRAAS') ? 'alpha_user' : 'user', body);
 }
 
-/**
- * Delete user, but does not currently work as idm provisioning is not allowed to perform delete
- * @param {*} userId
- */
-export function deleteIDMUser(userId) {
+export function deleteIDMResource(resourceType = 'managed', resourceName, id, accessToken = Cypress.env('ACCESS_TOKEN').access_token) {
   return cy.request({
     method: 'DELETE',
-    url: `https://${Cypress.env('FQDN')}/openidm/managed/user/${userId}`,
+    url: `https://${Cypress.env('FQDN')}/openidm/${resourceType}/${resourceName}/${id}`,
     headers: {
-      authorization: `Bearer ${Cypress.env('ACCESS_TOKEN').access_token}`,
-      'content-type': 'application/json',
-      referer: `https://${Cypress.env('FQDN')}/platform/appAuthHelperRedirect.html`,
+      authorization: `Bearer ${accessToken}`,
     },
-  }).then(() => {});
+  });
+}
+
+export function deleteIDMUser(id) {
+  return deleteIDMResource('managed', Cypress.env('IS_FRAAS') ? 'alpha_user' : 'user', id);
 }
 
 /**
@@ -107,18 +84,10 @@ export function deleteIDMUser(userId) {
  * @param {*} userId
  */
 export function assignUserDashboard(userId, dashboards) {
+  const userRealm = Cypress.env('IS_FRAAS') ? 'alpha' : 'root';
   return cy.request({
-    method: 'POST',
-    url: `https://${Cypress.env('FQDN')}/am/json/realms/root/authenticate`,
-    headers: {
-      'X-OpenAM-Username': Cypress.env('AM_USERNAME'),
-      'X-OpenAM-Password': Cypress.env('AM_PASSWORD'),
-      'content-type': 'application/json',
-      'accept-api-version': 'resource=2.1',
-    },
-  }).then(() => cy.request({
     method: 'PUT',
-    url: `https://${Cypress.env('FQDN')}/am/json/realms/root/users/${userId}/services/dashboard`,
+    url: `https://${Cypress.env('FQDN')}/am/json/realms/${userRealm}/users/${userId}/services/dashboard`,
     body: {
       assignedDashboard: dashboards,
     },
@@ -127,5 +96,5 @@ export function assignUserDashboard(userId, dashboards) {
       'x-requested-with': 'XMLHttpRequest',
       'accept-api-version': 'protocol=1.0,resource=1.0',
     },
-  }));
+  });
 }
