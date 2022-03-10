4d3c09653edb526ad1b3f6fa5efabff92a1ac9c6
Dave Ernsting <dave.ernsting@forgerock.com>
2022-03-10 15:57:20 +0000

    IAM-1958 Improves responsiveness of the tree list UI This has been achieved by refactoring the tree list to use less slow DOM manipulations when switching categories and searching

--- packages/platform-shared/src/components/Accordion/index.vue
+++ packages/platform-shared/src/components/Accordion/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2022 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -19,8 +19,9 @@ of the MIT license. See the LICENSE file for details. -->
           @property {object} data item data
         -->
         <div
-          class="border-top"
+          :class="`border-top ${data.accordionItemClass ? data.accordionItemClass : ''}`"
           :key="key"
+          data-testid="accordion-item-wrapper"
           no-body
           @click="$emit('section-expanded', {key, data})">
           <BCardHeader
