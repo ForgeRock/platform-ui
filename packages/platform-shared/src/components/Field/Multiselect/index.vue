3c7f2737c8bef57a66131acac8814db78e81ba5a
Michael Wong <michael.wong@forgerock.com>
2022-05-18 11:32:02 -0700

    IAM-2051 Turn off autocomplete for Select and Multiselect field components

--- packages/platform-shared/src/components/Field/Multiselect/index.vue
+++ packages/platform-shared/src/components/Field/Multiselect/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2022 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -149,6 +149,10 @@ export default {
     };
   },
   mounted() {
+    if (this.defaultSearchable) {
+      this.$refs.vms.$refs.search.setAttribute('autocomplete', 'off');
+    }
+
     if (this.autofocus) {
       this.openHandler();
     }
