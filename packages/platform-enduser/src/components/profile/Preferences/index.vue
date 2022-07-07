49db1b4d61a6dc988449b84a7b721c1b400fa1a1
Sebastian Velasquez <sebastian.velasquez@forgerock.com>
2022-07-07 17:35:44 +0000

    IAM-2285 using classes for headings to improve accesibility

--- packages/platform-enduser/src/components/profile/Preferences/index.vue
+++ packages/platform-enduser/src/components/profile/Preferences/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2022 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -7,7 +7,9 @@ of the MIT license. See the LICENSE file for details. -->
     class="account-card"
     no-body>
     <BCardHeader class="p-4">
-      <h4>{{ $t('pages.profile.preferences.title') }}</h4>
+      <h1 class="h4">
+        {{ $t('pages.profile.preferences.title') }}
+      </h1>
       <p class="m-0">
         {{ $t('pages.profile.preferences.subtitle') }}
       </p>
