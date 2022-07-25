d0d64694daa0ce05d5a001a04961c886fee442ef
Andrés Castillo <andres.castillo@forgerock.com>
2022-07-25 07:55:05 -0500

    IAM-2142 enhanced unit tests for ButtonWithAligment component

--- packages/platform-shared/src/components/ButtonWithAlignment/ButtonWithAlignment.test.js
+++ packages/platform-shared/src/components/ButtonWithAlignment/ButtonWithAlignment.test.js
@@ -14,8 +14,39 @@ describe('ButtonWithAlignment', () => {
       mocks: {
         $t: () => {},
       },
+      slots: {
+        default: 'button content',
+      },
     });
 
     expect(wrapper.name()).toEqual('ButtonWithAlignment');
+    expect(wrapper.html()).toContain('button content');
+    // default props
+    expect(wrapper.props().buttonPosition).toBe('button-full-width');
+    expect(wrapper.props().disabled).toBe(false);
+    expect(wrapper.props().type).toBe('button');
+    expect(wrapper.props().variant).toBe('primary');
+    // defult button position class exists
+    expect(wrapper.find('.flex-column').exists()).toBe(true);
+  });
+
+  it('ButtonWithAlignment button position change and render the correct class', () => {
+    const wrapper = shallowMount(ButtonWithAlignment, {
+      mocks: {
+        $t: () => {},
+      },
+    });
+
+    // defult button position class exists
+    expect(wrapper.find('.flex-column').exists()).toBe(true);
+
+    wrapper.setProps({ buttonPosition: 'button-center' });
+    expect(wrapper.find('.justify-content-center').exists()).toBe(true);
+
+    wrapper.setProps({ buttonPosition: 'button-left' });
+    expect(wrapper.find('.justify-content-start').exists()).toBe(true);
+
+    wrapper.setProps({ buttonPosition: 'button-right' });
+    expect(wrapper.find('.justify-content-end').exists()).toBe(true);
   });
 });
