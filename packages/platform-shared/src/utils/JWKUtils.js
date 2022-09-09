3fa92b8dede344d96dbb111261a9cb355f1e2bad
jake.harrison <jake.harrison@forgerock.com>
2022-09-09 13:24:20 +0100

    IAM-2139: can now add/edit/delete service accounts

--- packages/platform-shared/src/utils/JWKUtils.js
+++ packages/platform-shared/src/utils/JWKUtils.js
@@ -5,16 +5,52 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import * as jose from 'jose';
+import * as jose from 'jose'; // TODO: perhaps use https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API as alternative to jose
 
 /**
  * Generates a JSON web key using the RS256 specification
  * @returns the generated Public key
  */
-async function generatePublicKey() {
-  const key = await jose.generateKeyPair('RS256');
-  const publicKey = await jose.exportJWK(key.publicKey);
-  return publicKey;
+async function generateKeyPair() {
+  const key = await jose.generateKeyPair('RS256', { extractable: true, modulusLength: 4096 });
+  return key;
 }
 
-export default generatePublicKey;
+/**
+ * Takes the given CryptoKey public key and exports as a JWK
+ * @param {CryptoKey} publicKey
+ * @returns the public key JWK
+ */
+async function getPublicKey(publicKey) {
+  return jose.exportJWK(publicKey);
+}
+
+/**
+ * Takes the given CryptoKey private key and exports as a JWK
+ * @param {CryptoKey} privateKey
+ * @returns the private key JWK
+ */
+async function getPrivateKey(privateKey) {
+  return jose.exportJWK(privateKey);
+}
+
+/**
+ * Builds a JWKS out of the given json web keys
+ * @param {List} jwkList list of json web keys that we want to build a set of
+ * @returns json web key set containing the given json web keys
+ */
+function buildJwkSet(jwkList) {
+  if (!jwkList) throw Error('Something went wrong. Please provide a valid list of json web keys.');
+  return {
+    keys: [
+      ...jwkList,
+    ],
+  };
+}
+
+export {
+  generateKeyPair,
+  getPublicKey,
+  getPrivateKey,
+  buildJwkSet,
+};
