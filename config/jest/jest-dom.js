f1930d0b5ea5e000b8f231eb2b5a3392242f4e4c
Ingrid van den Berg <ingrid.vandenberg@forgerock.com>
2022-09-27 15:29:37 +0100

    IAM-2585 add test-cases for promotions-tabbed-report

--- config/jest/jest-dom.js
+++ config/jest/jest-dom.js
@@ -9,6 +9,9 @@
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
+// Note: this function has been added, as this browser-window function is not available to our unit testing environment and causes tests that call it, to fail.
+window.URL.createObjectURL = jest.fn();
+
 // Note: for use with Jose as there is no TextEncoder in JSDOM as yet - there is an open ticket to add this: https://github.com/jsdom/jsdom/issues/2524
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
