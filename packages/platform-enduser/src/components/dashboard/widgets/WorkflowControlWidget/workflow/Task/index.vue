c02d5cf954c4c28d92d1b14481f1a21458d9f337
Huck Elliott <huck.elliott@forgerock.com>
2022-10-04 08:58:32 -0700

    IAM-2797 wraps all instances of both 'process.env.VUE_APP_IDM_URL' and 'process.env.VUE_APP_AM_URL' in getFQDN() which fixes relative url issue

--- packages/platform-enduser/src/components/dashboard/widgets/WorkflowControlWidget/workflow/Task/index.vue
+++ packages/platform-enduser/src/components/dashboard/widgets/WorkflowControlWidget/workflow/Task/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2022 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -32,6 +32,7 @@ of the MIT license. See the LICENSE file for details. -->
 <script>
 /* eslint-disable no-underscore-dangle */
 import styles from '@forgerock/platform-shared/src/scss/main.scss';
+import getFQDN from '@forgerock/platform-shared/src/utils/getFQDN';
 import axios from 'axios';
 import { ClipLoader } from 'vue-spinner/dist/vue-spinner.min';
 import GenericTask from '../GenericTask';
@@ -93,7 +94,7 @@ export default {
     shown(val) {
       if (val && this.processDefinition === null) {
         const workflowInstance = axios.create({
-          baseURL: process.env.VUE_APP_IDM_URL,
+          baseURL: getFQDN(process.env.VUE_APP_IDM_URL),
           timeout: 15000,
           headers: {},
         });
