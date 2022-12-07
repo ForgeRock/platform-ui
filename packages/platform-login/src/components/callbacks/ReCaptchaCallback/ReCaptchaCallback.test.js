aeb12b34453391e70c653869d40004f94d20822c
Chuck Masucci <chuck.masucci@forgerock.com>
2022-12-06 16:37:06 -0800

    IAM-1939 Added hCaptcha support to Journey node

--- packages/platform-login/src/components/callbacks/ReCaptchaCallback/ReCaptchaCallback.test.js
+++ packages/platform-login/src/components/callbacks/ReCaptchaCallback/ReCaptchaCallback.test.js
@@ -10,7 +10,7 @@ import ReCaptchaCallback from '@/components/callbacks/ReCaptchaCallback';
 import i18n from '@/i18n';
 
 describe('ReCaptchaCallback.vue', () => {
-  const createWrapper = (isV3) => shallowMount(ReCaptchaCallback, {
+  const createWrapper = (apiUrl, isV3) => shallowMount(ReCaptchaCallback, {
     i18n,
     stubs: {
       'router-link': true,
@@ -19,24 +19,33 @@ describe('ReCaptchaCallback.vue', () => {
     },
     propsData: {
       callback: {
-        getOutputByName: jest.fn(() => isV3),
+        getOutputByName: jest.fn((name) => {
+          switch (name) {
+            case 'captchaApiUri':
+              return apiUrl;
+            case 'reCaptchaV3':
+              return isV3;
+            default:
+              return undefined;
+          }
+        }),
       },
     },
   });
 
   it('Load ReCaptchaCallback component', () => {
-    const wrapper = createWrapper(false);
+    const wrapper = createWrapper('https://www.google.com', false);
     expect(wrapper.name()).toEqual('ReCaptchaCallback');
   });
 
   it('Sets data for ReCaptchaV3', () => {
-    const wrapper = createWrapper(true);
+    const wrapper = createWrapper('https://www.google.com/recaptcha/api.js', true);
     expect(wrapper.vm.$props.callback.getOutputByName).toHaveBeenCalled();
     expect(wrapper.vm.$data.reCaptchaComponentVersion).toBe('ReCaptchaV3');
   });
 
   it('Sets data for ReCaptchaV2', () => {
-    const wrapper = createWrapper(false);
+    const wrapper = createWrapper('https://www.google.com/recaptcha/api.js', false);
     expect(wrapper.vm.$props.callback.getOutputByName).toHaveBeenCalled();
     expect(wrapper.vm.$data.reCaptchaComponentVersion).toBe('ReCaptchaV2');
   });
