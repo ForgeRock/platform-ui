218409f85f2931ce325af21c0c4fb6927cc137f0
Andrew Hertel <andrew.hertel@forgerock.com>
2023-01-06 15:35:31 -0800

    FRAAS-7542 Toggle to disable hosted journey pages

--- packages/platform-login/src/router.js
+++ packages/platform-login/src/router.js
@@ -1,6 +1,5 @@
 /**
- * @license
- * Copyright (c) 2020 ForgeRock. All rights reserved.
+ * Copyright (c) 2020-2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -26,6 +25,11 @@ export default new Router({
       path: '/service/:tree',
       name: 'service',
       component: () => import('@/views/Login'),
+    },
+    {
+      path: '/forbidden',
+      name: 'Forbidden',
+      component: () => import(/* webpackChunkName: "forbidden" */ '@/views/Forbidden'),
     },
     {
       // send any bad routes to default login
--- packages/platform-login/src/views/Forbidden/index.vue
+++ packages/platform-login/src/views/Forbidden/index.vue
@@ -5,17 +5,19 @@ of the MIT license. See the LICENSE file for details. -->
 <template>
   <BContainer>
     <div style="margin-top: 40%;">
-      <FrForbidden :show-dashboard="false" />
+      <FrForbidden
+        :show-dashboard="false"
+        :show-logout="false" />
     </div>
   </BContainer>
 </template>
+
 <script>
 /**
  * @description Card container that will stay horizontally and vertically center on the screen (example can be found on the default login page)
- *
- * */
-import FrForbidden from '@forgerock/platform-shared/src/views/Forbidden';
+ */
 import { BContainer } from 'bootstrap-vue';
+import FrForbidden from '@forgerock/platform-shared/src/views/Forbidden';
 
 export default {
   name: 'ForbiddenWrapper',
