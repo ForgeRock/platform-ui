e10ff088a65f06163e54d38738afef66825bb285
Josh Newton <josh.newton@forgerock.com>
2023-01-25 17:37:46 +0000

    IAM-3492 - Be more specific when filtering managed objects from privileges list

--- packages/platform-shared/src/components/resource/CreateResource/CustomStep/CustomStep.test.js
+++ packages/platform-shared/src/components/resource/CreateResource/CustomStep/CustomStep.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -62,6 +62,9 @@ describe('CustomStep.vue', () => {
             realm: 'test',
             UserStore: {
               adminUser: true,
+            },
+            SharedStore: {
+              workforceEnabled: false,
             },
           },
         },
