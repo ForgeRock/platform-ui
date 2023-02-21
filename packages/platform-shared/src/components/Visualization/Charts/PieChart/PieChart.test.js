a14e8f44b6a509c753286872575de6a1a52d464e
Michael Wong <michael.wong@forgerock.com>
2023-02-21 13:52:55 -0800

    IGA-1433 Adds campaign overview

--- packages/platform-shared/src/components/Visualization/Charts/PieChart/PieChart.test.js
+++ packages/platform-shared/src/components/Visualization/Charts/PieChart/PieChart.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 ForgeRock. All rights reserved.
+ * Copyright (c) 2022-2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -9,8 +9,23 @@ import { shallowMount } from '@vue/test-utils';
 import PieChart from './index';
 
 describe('PieChart', () => {
+  window.event = {
+    pageY: 10,
+    pageX: 10,
+  };
+
   const wrapper = shallowMount(PieChart, {
-    mocks: { $t: () => {} },
+    mocks: {
+      $t: () => {},
+      $refs: {
+        d3chart: {
+          chart: {
+            gcenter: {},
+            tooltip: {},
+          },
+        },
+      },
+    },
     propsData: {
       data: [
         {
@@ -30,6 +45,7 @@ describe('PieChart', () => {
         },
       ],
     },
+    stubs: ['D3PieChart'],
   });
 
   it('PieChart successfully loaded', () => {
@@ -37,14 +53,14 @@ describe('PieChart', () => {
   });
 
   it('chart exists and contains expected attributes', () => {
-    const chart = wrapper.find('#chart');
+    const chart = wrapper.find('[data-test-id="chart"]');
     expect(chart.exists()).toBe(true);
     expect(chart.attributes('config')).toBeTruthy();
     expect(chart.attributes('datum')).toBeTruthy();
   });
 
   it('all items are rendered properly', () => {
-    const list = wrapper.find('#list');
+    const list = wrapper.find('[data-test-id="list"]');
     expect(list.exists()).toBe(true);
     const items = wrapper.findAll('li');
     expect(items.length).toBe(3);
@@ -56,5 +72,65 @@ describe('PieChart', () => {
       expect(bullet.attributes('style')).toBe(`background-color: ${wrapper.vm.$props.data[index].color};`);
       expect(itemSpan.text()).toBe(wrapper.vm.$props.data[index].label);
     });
+  });
+
+  it('adds tooltips', async () => {
+    const callbacks = {
+      mouseover: null,
+      mouseout: null,
+      mousemove: null,
+    };
+    let html = '';
+    const gcenter = {};
+    const tooltip = {};
+
+    gcenter.selectAll = jest.fn().mockReturnValue(gcenter);
+    gcenter.on = jest.fn().mockImplementation((type, callback) => {
+      callbacks[type] = () => { callback({ data: { label: 'testlabel', value: 713 } }); };
+      return gcenter;
+    });
+
+    tooltip.classed = jest.fn().mockReturnValue(tooltip);
+    tooltip.getBoundingClientRect = jest.fn().mockReturnValue({ width: 10, height: 10 });
+    tooltip.html = jest.fn().mockImplementation((cb) => {
+      html = cb();
+      return tooltip;
+    });
+    tooltip.node = jest.fn().mockReturnValue(tooltip);
+    tooltip.style = jest.fn().mockReturnValue(tooltip);
+
+    wrapper.vm.$refs.d3chart = {
+      chart: {
+        gcenter,
+        tooltip,
+      },
+    };
+
+    wrapper.vm.addTooltips();
+    await wrapper.vm.$nextTick();
+    expect(gcenter.selectAll).toHaveBeenCalledWith('.chart__slice-group');
+    expect(gcenter.on).toHaveBeenCalledTimes(3);
+
+    callbacks.mouseover();
+    expect(tooltip.html).toHaveBeenCalled();
+    expect(tooltip.classed).toBeCalledWith('active', true);
+    expect(html).toEqual(expect.stringContaining('testlabel'));
+    expect(html).toEqual(expect.stringContaining('713'));
+
+    callbacks.mousemove();
+    expect(tooltip.node).toHaveBeenCalled();
+    expect(tooltip.getBoundingClientRect).toHaveBeenCalled();
+    expect(tooltip.style).toHaveBeenCalledTimes(3);
+
+    callbacks.mouseout();
+    expect(tooltip.classed).toBeCalledWith('active', true);
+  });
+
+  it('forceRerender method should increment keyComponent value', () => {
+    expect(wrapper.vm.componentKey).toBe(0);
+
+    wrapper.vm.forceRerender();
+
+    expect(wrapper.vm.componentKey).toBe(1);
   });
 });
