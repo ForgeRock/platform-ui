0b70a0b03f71c4f68e6ce5507c8012621dc012f6
Michael Wong <michael.wong@forgerock.com>
2023-02-22 14:17:27 -0800

    IGA-1433 Adds campaign view

--- packages/platform-shared/src/components/PageHeader/index.vue
+++ packages/platform-shared/src/components/PageHeader/index.vue
@@ -1,9 +1,15 @@
-<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <div>
+    <h5
+      v-if="topText"
+      class="text-muted mb-2"
+    >
+      {{ topText }}
+    </h5>
     <h1>
       {{ title }}
     </h1>
@@ -27,6 +33,13 @@ export default {
      * Main text displayed in large font.
      */
     title: {
+      type: String,
+      default: '',
+    },
+    /*
+     * Muted text to be shown above the title
+     */
+    topText: {
       type: String,
       default: '',
     },
