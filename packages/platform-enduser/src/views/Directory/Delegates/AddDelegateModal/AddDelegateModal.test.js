6701905f769e6b4ff0c7f10e0c13cba6ff3cf338
Sebastian Guevara <info@sebasg.dev>
2023-03-06 16:30:59 -0500

    IAM-3628 - Validate endDate on Add Delegates Modal

--- packages/platform-enduser/src/views/Directory/Delegates/AddDelegateModal/AddDelegateModal.test.js
+++ packages/platform-enduser/src/views/Directory/Delegates/AddDelegateModal/AddDelegateModal.test.js
@@ -71,14 +71,17 @@ describe('AddDelegateModal', () => {
     expect(errorSpy).toHaveBeenCalled();
   });
 
-  it('can set an end date', async () => {
+  it('should allow only date in the future when setting end-date', async () => {
     const addTaskProxy = jest.spyOn(GovernanceEnduserApi, 'addTaskProxy');
 
     const enableTime = findByTestId(wrapper, 'enable-time-constraint');
     await enableTime.setChecked(true);
 
+    const startDate = findByTestId(wrapper, 'start-date');
+    startDate.vm.$emit('input', '2023-02-01');
+
     const endDate = findByTestId(wrapper, 'end-date');
-    endDate.vm.$emit('input', '2023-02-01');
+    endDate.vm.$emit('input', '2023-02-02');
 
     const saveButton = findByTestId(wrapper, 'save-button');
     await saveButton.trigger('click');
@@ -86,28 +89,131 @@ describe('AddDelegateModal', () => {
     expect(addTaskProxy).toBeCalledWith(
       'testId',
       [''],
-      null,
       '2023-02-01',
+      '2023-02-02',
     );
   });
 
-  it('can set a start date', async () => {
-    const addTaskProxy = jest.spyOn(GovernanceEnduserApi, 'addTaskProxy');
+  describe('isValidEndDate', () => {
+    it('should return false when startdDate and endDate are empty', async () => {
+      await wrapper.setData({
+        startDate: '',
+        endDate: '',
+      });
 
-    const enableTime = findByTestId(wrapper, 'enable-time-constraint');
-    await enableTime.setChecked(true);
+      expect(wrapper.vm.isValidEndDate).toEqual(false);
+    });
 
-    const endDate = findByTestId(wrapper, 'start-date');
-    endDate.vm.$emit('input', '2023-02-01');
+    it('should return false when endDate is empty', async () => {
+      await wrapper.setData({
+        startDate: '01/02/2023',
+        endDate: '',
+      });
 
-    const saveButton = findByTestId(wrapper, 'save-button');
-    await saveButton.trigger('click');
+      expect(wrapper.vm.isValidEndDate).toEqual(false);
+    });
 
-    expect(addTaskProxy).toBeCalledWith(
-      'testId',
-      [''],
-      '2023-02-01',
-      null,
-    );
+    it('should return false when endDate is less than the startDate', async () => {
+      await wrapper.setData({
+        startDate: '01/02/2023',
+        endDate: '01/01/2023',
+      });
+
+      expect(wrapper.vm.isValidEndDate).toEqual(false);
+    });
+
+    it('should return false when endDate is equal than the startDate', async () => {
+      await wrapper.setData({
+        startDate: '01/02/2023',
+        endDate: '01/02/2023',
+      });
+
+      expect(wrapper.vm.isValidEndDate).toEqual(false);
+    });
+
+    it('should return true when endDate is greater than the startDate', async () => {
+      await wrapper.setData({
+        startDate: '01/02/2023',
+        endDate: '01/03/2023',
+      });
+
+      expect(wrapper.vm.isValidEndDate).toEqual(true);
+    });
+  });
+
+  describe('showEndDateError', () => {
+    it('should return false if startDate and endDate are empty and isValidEndDate is not valid', async () => {
+      jest.spyOn(wrapper.vm, 'isValidEndDate', 'get').mockImplementation(() => false);
+
+      await wrapper.setData({
+        startDate: '',
+        endDate: '',
+      });
+
+      expect(wrapper.vm.showEndDateError).toEqual(false);
+    });
+
+    it('should return false if endDate is empty and isValidEndDate is not valid', async () => {
+      jest.spyOn(wrapper.vm, 'isValidEndDate', 'get').mockImplementation(() => false);
+
+      await wrapper.setData({
+        startDate: '01/02/2023',
+        endDate: '',
+      });
+
+      expect(wrapper.vm.showEndDateError).toEqual(false);
+    });
+
+    it('should return false if startDate and endDate are not empty and isValidEndDate is valid', async () => {
+      jest.spyOn(wrapper.vm, 'isValidEndDate', 'get').mockImplementation(() => true);
+
+      await wrapper.setData({
+        startDate: '01/02/2023',
+        endDate: '01/03/2023',
+      });
+
+      expect(wrapper.vm.showEndDateError).toEqual(false);
+    });
+
+    it('should return true if startDate and endDate are not empty and isValidEndDate is not valid', async () => {
+      jest.spyOn(wrapper.vm, 'isValidEndDate', 'get').mockImplementation(() => false);
+
+      await wrapper.setData({
+        startDate: '01/02/2023',
+        endDate: '01/01/2023',
+      });
+
+      expect(wrapper.vm.showEndDateError).toEqual(true);
+    });
+  });
+
+  describe('canUserSave', () => {
+    it('should return true if timeConstraint is not enabled', async () => {
+      await wrapper.setData({
+        enableTimeConstraint: false,
+      });
+
+      expect(wrapper.vm.canUserSave).toEqual(true);
+    });
+
+    it('should return false when timeConstraint is enabled and isValidEndDate is not valid', async () => {
+      jest.spyOn(wrapper.vm, 'isValidEndDate', 'get').mockImplementation(() => false);
+
+      await wrapper.setData({
+        enableTimeConstraint: true,
+      });
+
+      expect(wrapper.vm.canUserSave).toEqual(false);
+    });
+
+    it('should return true when timeConstraint is enabled and isValidEndDate is valid', async () => {
+      jest.spyOn(wrapper.vm, 'isValidEndDate', 'get').mockImplementation(() => true);
+
+      await wrapper.setData({
+        enableTimeConstraint: true,
+      });
+
+      expect(wrapper.vm.canUserSave).toEqual(true);
+    });
   });
 });
--- packages/platform-shared/src/components/Datepicker/index.vue
+++ packages/platform-shared/src/components/Datepicker/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2020-2022 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -81,6 +81,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.fr-field-error {
+      border: 1px solid $red !important;
+}
 // floating label support
 .input-has-value {
   ::v-deep label {
