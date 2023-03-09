485acf3254350f62b142332f33d67bb83963ee8c
Dave Ernsting <dave.ernsting@forgerock.com>
2023-03-09 11:23:40 +0000

    IAM-1812 adds cloud E2E tesing via EMA

--- e2e/passwords.sh
+++ e2e/passwords.sh
@@ -4,9 +4,7 @@ set -euxo pipefail
 kubernetes_namespace=${KUBERNETES_NAMESPACE:-default}
 
 export CYPRESS_AM_USERNAME="amadmin"
-export CYPRESS_IDM_USERNAME="amadmin"
 export CYPRESS_AM_PASSWORD=$(kubectl --namespace $kubernetes_namespace get secret am-env-secrets -o jsonpath='{.data.AM_PASSWORDS_AMADMIN_CLEAR}' | base64 --d)
-export CYPRESS_IDM_PASSWORD=$(kubectl --namespace $kubernetes_namespace get secret idm-env-secrets -o jsonpath='{.data.OPENIDM_ADMIN_PASSWORD}' | base64 --d)
 export CYPRESS_ACCESS_TOKEN=`curl -u idm-provisioning:$(kubectl --namespace $kubernetes_namespace get secret amster-env-secrets -o jsonpath='{.data.IDM_PROVISIONING_CLIENT_SECRET}' | base64 --d) --data 'grant_type=client_credentials&scope=fr:idm:*' -k "https://$(kubectl --namespace $kubernetes_namespace get configmaps platform-config -o jsonpath='{.data.FQDN}')/am/oauth2/access_token"`
 export CYPRESS_FQDN=`kubectl --namespace $kubernetes_namespace get configmaps platform-config -o jsonpath='{.data.FQDN}'`
 export CYPRESS_BASE_URL="https://$CYPRESS_FQDN"
