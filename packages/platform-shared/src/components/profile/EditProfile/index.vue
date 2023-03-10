a52f75c3ab6275cab92ae60390d4eb3f9d0cbe11
jake.harrison <jake.harrison@forgerock.com>
2023-03-10 14:57:44 +0000

    IAM-3679: added aria-label to edit profile picture button

--- packages/platform-shared/src/components/profile/EditProfile/index.vue
+++ packages/platform-shared/src/components/profile/EditProfile/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2021-2022 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2021-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -20,7 +20,8 @@ of the MIT license. See the LICENSE file for details. -->
             @click="$root.$emit('bv::show::modal', 'frProfileImageModal')"
             class="fr-edit-icon"
             size="2em"
-            variant="dark">
+            variant="dark"
+            aria-label="Update profile picture">
             <FrIcon
               class="md-18"
               name="camera_alt"
