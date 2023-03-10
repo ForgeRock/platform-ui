7ff314e56f1599919cfd6a9b9490cd5927300de4
jake.harrison <jake.harrison@forgerock.com>
2023-03-10 13:01:16 +0000

    IAM-3666: added empty alt attribute to qrcode

---
+++ packages/platform-shared/src/utils/stringDomNodeUtils.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2023 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+/* eslint-disable no-use-before-define */
+
+/**
+ * Appends attributes to a given dom node string
+ * Does not handle duplicate attributes
+ * @param {String} domNodeString the dom node that you want to add attributes to
+ * @param {Map} attributesMap a map of attributes to add to the dom node
+ * @returns a string with the attributes appended to the dom node
+ */
+function addAttributesToDomNodeString(domNodeString, attributesMap) {
+  if (!domNodeString || !attributesMap) throw new Error('Please provide both parameters.');
+  if (typeof domNodeString !== 'string') throw new Error('Incorrect parameter type. Please provide a String for `domNodeString`');
+  if (!(attributesMap instanceof Map)) throw new Error('Incorrect parameter type. Please provide a Map for `attributesMap`');
+
+  const endFirstTagLoc = getLocToPlaceAttribute(domNodeString);
+
+  let attributes = '';
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [attr, val] of attributesMap) {
+    if (typeof attr !== 'string') break;
+    attributes += `${attr}="${val}" `;
+  }
+
+  const domNodeStringLength = domNodeString.length;
+  return `${domNodeString.substring(0, endFirstTagLoc)} ${attributes.trim()}${domNodeString.substring(endFirstTagLoc, domNodeStringLength)}`;
+}
+
+function getLocToPlaceAttribute(domNodeString) {
+  const endTagCount = domNodeString.split('>').length - 1;
+  const isSelfClosingElement = endTagCount === 1;
+
+  let loc;
+  if (isSelfClosingElement) {
+    loc = domNodeString.length - 2;
+  } else {
+    loc = domNodeString.indexOf('>');
+    if (loc === -1) throw new Error('Non valid html string detected. Please provide a string representing valid html.');
+  }
+
+  return loc;
+}
+
+export default addAttributesToDomNodeString;
---
+++ packages/platform-shared/src/utils/stringDomNodeUtils.test.js
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2023 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import addAttributesToDomNodeString from './stringDomNodeUtils';
+
+/* eslint-disable indent */
+describe('stringDomNodeUtils', () => {
+  describe('addAttributesToDomNodeString', () => {
+    describe('should throw error', () => {
+      describe('when domNodeString is falsy', () => {
+        it.each`
+        name                    | domNodeString
+        ${'given false'}        | ${false}
+        ${'given zero'}         | ${0}
+        ${'given empty string'} | ${''}
+        ${'given null'}         | ${null}
+        ${'given undefined'}    | ${undefined}
+        ${'given NaN'}          | ${NaN}
+        `('$name', ({ domNodeString }) => {
+          expect(() => addAttributesToDomNodeString(domNodeString)).toThrowError('Please provide both parameters.');
+        });
+      });
+
+      describe('when attributesMap is falsy', () => {
+        const givenNodeString = '<p>given node string</p>';
+        it.each`
+        name                    | attributesMap
+        ${'given false'}        | ${false}
+        ${'given zero'}         | ${0}
+        ${'given empty string'} | ${''}
+        ${'given null'}         | ${null}
+        ${'given undefined'}    | ${undefined}
+        ${'given NaN'}          | ${NaN}
+        `('$name', ({ attributesMap }) => {
+          expect(() => addAttributesToDomNodeString(givenNodeString, attributesMap)).toThrowError('Please provide both parameters.');
+        });
+      });
+
+      it('when domNodeString is not a string', () => {
+        expect(() => addAttributesToDomNodeString(786, new Map())).toThrowError('Incorrect parameter type. Please provide a String for `domNodeString`');
+      });
+
+      it('when attributesMap is not a Map', () => {
+        expect(() => addAttributesToDomNodeString('<p>stub-p</p>', { alt: '' })).toThrowError('Incorrect parameter type. Please provide a Map for `attributesMap`');
+      });
+
+      it('when there is no closing tag', () => {
+        expect(() => addAttributesToDomNodeString('<p', new Map())).toThrowError('Non valid html string detected. Please provide a string representing valid html.');
+      });
+    });
+
+    describe('should ignore attribute', () => {
+      it('when attributesMap contains a key that is not a string', () => {
+        const stubNode = '<p>stub-p</p>';
+        const attributesMap = new Map();
+        attributesMap.set('class', 'bold');
+        attributesMap.set(7, 7);
+
+        const returnedVal = addAttributesToDomNodeString(stubNode, attributesMap);
+        expect(returnedVal).toBe('<p class="bold">stub-p</p>');
+      });
+    });
+
+    describe('should add attribute to first dom node', () => {
+      it('when one attribute', () => {
+        const domNode = '<div>stub-div</div>';
+        const returnedVal = addAttributesToDomNodeString(domNode, new Map().set('class', 'bold'));
+        expect(returnedVal).toBe('<div class="bold">stub-div</div>');
+      });
+
+      it('when multiple attributes', () => {
+        const domNode = '<a>hyperlink</a>';
+
+        const attributes = new Map();
+        attributes.set('class', 'bold');
+        attributes.set('id', 'stub-id');
+        attributes.set('href', 'http://forgerock.com');
+
+        const returnedVal = addAttributesToDomNodeString(domNode, attributes);
+        expect(returnedVal).toBe('<a class="bold" id="stub-id" href="http://forgerock.com">hyperlink</a>');
+      });
+
+      it('when node with children', () => {
+        const domNode = '<div><span>child-1</span></div>';
+        const returnedVal = addAttributesToDomNodeString(domNode, new Map().set('class', 'bold'));
+        expect(returnedVal).toBe('<div class="bold"><span>child-1</span></div>');
+      });
+
+      it('when self closing tag', () => {
+        const domNode = '<input name="stub-input"/>';
+
+        const attributes = new Map();
+        attributes.set('class', 'bold');
+
+        const returnedVal = addAttributesToDomNodeString(domNode, attributes);
+        expect(returnedVal).toBe('<input name="stub-input" class="bold"/>');
+      });
+    });
+  });
+});
