0ef908ef58d3bc637bf258b72e2bbe7945284869
Chuck Masucci <chuck.masucci@forgerock.com>
2023-03-13 15:23:03 -0700

    IAM-3043 Disable next button until captcha is verified

--- packages/platform-login/src/components/callbacks/ReCaptchaCallback/HCaptchaV1.vue
+++ packages/platform-login/src/components/callbacks/ReCaptchaCallback/HCaptchaV1.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2022 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2022-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -31,9 +31,13 @@ export default {
       required: true,
     },
   },
+  created() {
+    this.$emit('disable-next-button', true);
+  },
   methods: {
     handleCaptchaVerify(token) {
       this.callback.setInputValue(token);
+      this.$emit('disable-next-button', false);
     },
     handleCaptchaError() {
       this.showErrorMessage('error', this.$t('pages.selfservice.captchaError'));
