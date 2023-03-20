b7c7d8209c698bf6340b04b62f02f5207b3e13dc
Michael Wong <michael.wong@forgerock.com>
2023-03-20 09:22:39 -0700

    IAM-3758 add no data component to access review list, template list, campaign list, cert task list, and delegate list

--- packages/platform-shared/src/components/NoData/NoData.test.js
+++ packages/platform-shared/src/components/NoData/NoData.test.js
@@ -1,10 +1,10 @@
 /**
- * Copyright 2019 ForgeRock AS. All Rights Reserved
+ * Copyright (c) 2019-2023 ForgeRock. All rights reserved.
  *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
  */
+
 import { shallowMount } from '@vue/test-utils';
 import NoData from './index';
 
