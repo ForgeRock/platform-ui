ce75f36f76e4aaeab29b831b29fbfa5a3d4c121b
Sebastian Guevara <info@sebasg.dev>
2023-03-24 10:47:42 -0500

    IAM-3628 - Validate endDate on Add Delegates Modal

--- packages/platform-shared/src/components/Datepicker/index.vue
+++ packages/platform-shared/src/components/Datepicker/index.vue
@@ -3,47 +3,57 @@
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
-  <BFormDatepicker
-    v-bind="$attrs"
-    v-on="$listeners"
-    hide-header
-    label-help=""
-    menu-class="shadow-lg"
-    :class="{'input-has-value': value}"
-    :label-selected="placeholder"
-    :placeholder="placeholder"
-    :value="value">
-    <template #button-content>
-      <FrIcon
-        name="calendar_today"
-      />
-    </template>
-    <template #nav-prev-year>
-      <FrIcon
-        name="first_page"
-      />
-    </template>
-    <template #nav-prev-month>
-      <FrIcon
-        name="chevron_left"
-      />
-    </template>
-    <template #nav-this-month>
-      <FrIcon
-        name="calendar_today"
-      />
-    </template>
-    <template #nav-next-month>
-      <FrIcon
-        name="chevron_right"
-      />
-    </template>
-    <template #nav-next-year>
-      <FrIcon
-        name="last_page"
-      />
-    </template>
-  </BFormDatepicker>
+  <ValidationProvider
+    mode="aggressive"
+    :rules="validation"
+    :immediate="validationImmediate"
+    v-slot="{ errors }">
+    <BFormDatepicker
+      data-testid="datepicker"
+      v-bind="$attrs"
+      v-on="$listeners"
+      hide-header
+      label-help=""
+      menu-class="shadow-lg"
+      :class="{'input-has-value': value, 'fr-field-error': errors.length > 0}"
+      :label-selected="placeholder"
+      :placeholder="placeholder"
+      :value="value">
+      <template #button-content>
+        <FrIcon
+          name="calendar_today"
+        />
+      </template>
+      <template #nav-prev-year>
+        <FrIcon
+          name="first_page"
+        />
+      </template>
+      <template #nav-prev-month>
+        <FrIcon
+          name="chevron_left"
+        />
+      </template>
+      <template #nav-this-month>
+        <FrIcon
+          name="calendar_today"
+        />
+      </template>
+      <template #nav-next-month>
+        <FrIcon
+          name="chevron_right"
+        />
+      </template>
+      <template #nav-next-year>
+        <FrIcon
+          name="last_page"
+        />
+      </template>
+    </BFormDatepicker>
+    <FrValidationError
+      class="error-messages flex-grow-1"
+      :validator-errors="errors" />
+  </ValidationProvider>
 </template>
 
 <script>
@@ -52,6 +62,9 @@ import {
 } from 'bootstrap-vue';
 
 import FrIcon from '@forgerock/platform-shared/src/components/Icon';
+import FrValidationError from '@forgerock/platform-shared/src/components/ValidationErrorList';
+import { ValidationProvider } from 'vee-validate';
+
 /**
  * Bootstrap datepicker with custom icons, default values set.
  */
@@ -60,6 +73,8 @@ export default {
   components: {
     BFormDatepicker,
     FrIcon,
+    FrValidationError,
+    ValidationProvider,
   },
   props: {
     /**
@@ -76,13 +91,26 @@ export default {
       type: String,
       default: '',
     },
+    /* Vee-validate validation types to check against
+    */
+    validation: {
+      type: [String, Object],
+      default: '',
+    },
+    /**
+     * Whether error validation should happen when this component renders
+     */
+    validationImmediate: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .fr-field-error {
-      border: 1px solid $red !important;
+  border: 1px solid $red !important;
 }
 // floating label support
 .input-has-value {
