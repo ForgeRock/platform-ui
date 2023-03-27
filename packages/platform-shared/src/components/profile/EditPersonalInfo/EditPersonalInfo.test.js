d3a437708514011d60c44398271b6641dd64bd45
jake.harrison <jake.harrison@forgerock.com>
2023-03-27 14:58:43 +0100

    IAM-3681: fieldset now wraps all the edit personal info fields

--- packages/platform-shared/src/components/profile/EditPersonalInfo/EditPersonalInfo.test.js
+++ packages/platform-shared/src/components/profile/EditPersonalInfo/EditPersonalInfo.test.js
@@ -1,125 +1,174 @@
 /**
- * Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+ * Copyright (c) 2020-2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import BootstrapVue from 'bootstrap-vue';
-import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { createWrapper, mount, shallowMount } from '@vue/test-utils';
 import { first } from 'lodash';
 import i18n from '@/i18n';
 import EditPersonalInfo from './index';
+import { findByTestId } from '../../../utils/testHelpers';
 
-const localVue = createLocalVue();
-localVue.use(BootstrapVue);
-
-describe('EditPersonalInfo.vue', () => {
-  let wrapper;
-  const store = {
-    state: {
-      UserStore: {
-        userId: '1234',
-        managedResource: {},
-        internalUser: true,
+describe('EditPersonalInfo', () => {
+  const defaultProps = {
+    schema: {
+      order: ['givenName', 'sn'],
+      properties: {
+        givenName: {
+          description: 'First Name',
+          isPersonal: true,
+          searchable: true,
+          title: 'First Name',
+          type: 'string',
+          usageDescription: '',
+          userEditable: true,
+          viewable: true,
+        },
+        sn: {
+          description: 'Last Name',
+          isPersonal: true,
+          searchable: true,
+          title: 'Last Name',
+          type: 'string',
+          usageDescription: '',
+          userEditable: true,
+          viewable: true,
+        },
       },
+      required: ['givenName'],
     },
+    profile: { first: 'test', second: 'second' },
+    isTesting: true,
   };
-  beforeEach(() => {
-    wrapper = shallowMount(EditPersonalInfo, {
-      localVue,
+
+  function setup(props) {
+    return mount(EditPersonalInfo, {
       i18n,
       mocks: {
-        $store: store,
-      },
-      propsData: {
-        schema: {
-          order: ['test'],
-          properties: {
-            test: {
-              viewable: true,
-              type: 'string',
-              title: 'test title',
-              userEditable: true,
+        $store: {
+          state: {
+            UserStore: {
+              userId: 'stub-id',
+              managedResource: {},
+              internalUser: false,
             },
           },
-          required: [],
         },
-        profile: { test: 'test' },
+      },
+      propsData: {
+        ...defaultProps,
+        ...props,
       },
     });
+  }
 
-    jest.spyOn(wrapper.vm, 'saveForm');
+  describe('@renders', () => {
+    it('renders header', () => {
+      const wrapper = setup();
+      const title = findByTestId(wrapper, 'title-edit-personal-info');
+      expect(title.text()).toBe('Edit personal info');
+
+      const closeModalButton = findByTestId(wrapper, 'btn-edit-personal-info-close');
+      expect(closeModalButton.exists()).toBeTruthy();
+    });
+
+    describe('given no fields', () => {
+      it('renders no fields text', () => {
+        const wrapper = setup({ schema: {} });
+        // Note: must trigger the 'show' event as when we mount the modal manually it doesn't trigger bootstrap @shown event
+        wrapper.vm.$root.$emit('bv::show::modal', 'userDetailsModal');
+
+        const noFieldsHeader = findByTestId(wrapper, 'edit-personal-info-no-fields');
+        expect(noFieldsHeader.text()).toBe('No profile fields available');
+      });
+    });
+
+    describe('given form fields', () => {
+      it('should render fields', () => {
+        const wrapper = setup();
+        // Note: must trigger the 'show' event as when we mount the modal manually it doesn't trigger bootstrap @shown event
+        wrapper.vm.$root.$emit('bv::show::modal', 'userDetailsModal');
+
+        const firstField = findByTestId(wrapper, 'input-edit-personal-info-0');
+        expect(firstField.exists()).toBeTruthy();
+
+        const secondField = findByTestId(wrapper, 'input-edit-personal-info-1');
+        expect(secondField.exists()).toBeTruthy();
+      });
+    });
   });
 
-  afterEach(() => {
-    wrapper = null;
+  describe('@actions', () => {
+    it('should hide modal', async () => {
+      const wrapper = setup();
+      const closeButton = findByTestId(wrapper, 'btn-edit-personal-info-close');
+      const rootWrapper = createWrapper(wrapper.vm.$root);
+
+      expect(rootWrapper.emitted('bv::hide::modal')).toBeFalsy();
+      await closeButton.trigger('click');
+      expect(rootWrapper.emitted('bv::hide::modal')).toBeTruthy();
+    });
+
+    describe('when form is valid', () => {
+      it('should save form', async () => {
+        const wrapper = setup();
+        jest.spyOn(wrapper.vm, 'getRequestService').mockImplementation(() => ({
+          post: () => Promise.resolve({
+            data: {
+              failedPolicyRequirements: {},
+            },
+          }),
+        }));
+        // Note: must trigger the 'show' event as when we mount the modal manually it doesn't trigger bootstrap @shown event
+        wrapper.vm.$root.$emit('bv::show::modal', 'userDetailsModal');
+
+        const saveButton = findByTestId(wrapper, 'btn-edit-personal-info-save');
+        await saveButton.trigger('click');
+      });
+    });
   });
 
-  it('EditPersonalInfo modal loaded', () => {
-    expect(wrapper.name()).toBe('EditPersonalInfo');
-    expect(wrapper.isVisible()).toBe(true);
-  });
+  describe('@unit', () => {
+    describe('generateFormFields', () => {
+      it('should create the proper fields based on schema', () => {
+        const wrapper = shallowMount(EditPersonalInfo, {
+          i18n,
+          propsData: {
+            ...defaultProps,
+          },
+        });
+        const formFields = wrapper.vm.generateFormFields();
+        const firstFormField = first(formFields);
 
-  it('renders a title', () => {
-    expect(wrapper.vm.title).toBe('Edit personal info');
-  });
+        expect(formFields.length).toBe(2);
+        expect(typeof formFields).toBe('object');
+        expect(typeof firstFormField).toBe('object');
 
-  it('creates patches array correctly', () => {
-    const original = [{
-      name: 'description',
-      value: null,
-    }, {
-      name: 'telephoneNumber',
-      value: '123-456-7890',
-    }, {
-      name: 'city',
-      value: 'Portland',
-    }, {
-      name: 'postalCode',
-      value: null,
-    }];
+        expect(firstFormField).toMatchObject({
+          name: 'givenName',
+          title: 'First Name',
+          value: null,
+          type: 'string',
+          description: 'First Name',
+          items: undefined,
+          format: undefined,
+          validation: 'required',
+        });
 
-    const newForm = [{
-      name: 'description',
-      value: 'new description',
-    }, {
-      name: 'telephoneNumber',
-      value: '',
-    }, {
-      name: 'city',
-      value: 'Vancouver',
-    }, {
-      name: 'postalCode',
-      value: null,
-    }];
-
-    const patches = wrapper.vm.generateUpdatePatch(original, newForm);
-
-    expect(patches.length).toBe(3);
-    expect(patches[0].operation).toBe('replace');
-    expect(patches[0].field).toBe('/description');
-    expect(patches[0].value).toBe('new description');
-    expect(patches[1].operation).toBe('remove');
-    expect(patches[1].field).toBe('/telephoneNumber');
-    expect(patches[2].operation).toBe('replace');
-    expect(patches[2].field).toBe('/city');
-    expect(patches[2].value).toBe('Vancouver');
-  });
-
-  describe('#generateFormFields', () => {
-    it('should create the proper fields based on schema', () => {
-      const formFields = wrapper.vm.generateFormFields();
-      const firstFormField = first(formFields);
-
-      expect(typeof formFields).toBe('object');
-      expect(formFields.length).toBe(1);
-      expect(typeof firstFormField).toBe('object');
-      expect(firstFormField).toMatchObject({ name: 'test' });
-      expect(firstFormField).toMatchObject({ title: 'test title (optional)' });
-      expect(firstFormField).toMatchObject({ value: 'test' });
-      expect(firstFormField).toMatchObject({ type: 'string' });
-      expect(firstFormField).toMatchObject({ validation: '' });
+        const secondFormField = formFields[1];
+        expect(secondFormField).toMatchObject({
+          name: 'sn',
+          title: 'Last Name (optional)',
+          value: null,
+          type: 'string',
+          description: 'Last Name',
+          items: undefined,
+          format: undefined,
+          validation: '',
+        });
+      });
     });
   });
 });
