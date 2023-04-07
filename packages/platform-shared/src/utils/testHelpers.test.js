1c3dfc88b51bacdd27fe15c68a55e6f073ea7ee4
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2023-04-07 17:14:08 -0500

    IAM-2835 entitlements

--- packages/platform-shared/src/utils/testHelpers.test.js
+++ packages/platform-shared/src/utils/testHelpers.test.js
@@ -1,14 +1,15 @@
 /**
- * Copyright (c) 2022 ForgeRock. All rights reserved.
+ * Copyright (c) 2022-2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
 /* eslint-disable indent */
-import { mount } from '@vue/test-utils';
-import { findByRole, findByTestId } from './testHelpers';
+import { mount, createLocalVue } from '@vue/test-utils';
+import { findByRole, findByTestId, findAllByTestId } from './testHelpers';
 import FrCheckbox from '../components/Field/Checkbox';
+import FrAccordion from '../components/Accordion';
 
 describe('testHelpers', () => {
   describe('findByTestId', () => {
@@ -61,6 +62,76 @@ describe('testHelpers', () => {
     });
   });
 
+  describe('testHelpers', () => {
+    describe('findAllByTestId', () => {
+      describe('should throw error', () => {
+        describe('when wrapper is falsy', () => {
+          it.each`
+            name                    | invalidWrapper
+            ${'given false'}        | ${false}
+            ${'given zero'}         | ${0}
+            ${'given empty string'} | ${''}
+            ${'given null'}         | ${null}
+            ${'given undefined'}    | ${undefined}
+            `('$name', ({ invalidWrapper }) => {
+            expect(() => findAllByTestId(invalidWrapper, 'given_test_id')).toThrow(Error('Please provide a wrapper'));
+          });
+        });
+
+        describe('when testId is not a string', () => {
+          const localVue = createLocalVue();
+          const stubWrapper = mount(FrAccordion, {
+            localVue,
+            directives: {
+              'b-toggle': jest.mock(),
+            },
+            propsData: {
+              accordionGroup: 'default',
+              items: [
+                { open$: false },
+                { open$: false },
+                { open$: false },
+              ],
+            },
+          });
+          it.each`
+            name                 | testid
+            ${'given false'}     | ${false}
+            ${'given zero'}      | ${0}
+            ${'given null'}      | ${null}
+            ${'given NaN'}       | ${NaN}
+            ${'given undefined'} | ${undefined}
+            `('$name', ({ testid }) => {
+            expect(() => findAllByTestId(stubWrapper, testid)).toThrow(Error('Please provide a valid data-testid'));
+          });
+        });
+      });
+
+      describe('should return element', () => {
+        it('when given valid testId', () => {
+          const localVue = createLocalVue();
+          const wrapper = mount(FrAccordion, {
+            localVue,
+            directives: {
+              'b-toggle': jest.mock(),
+            },
+            propsData: {
+              accordionGroup: 'default',
+              items: [
+                { open$: false },
+                { open$: false },
+                { open$: false },
+              ],
+            },
+          });
+
+          const accordionItem = findAllByTestId(wrapper, 'accordion-item-wrapper');
+          expect(accordionItem.length).toBe(3);
+        });
+      });
+    });
+  });
+
   describe('findByRole', () => {
     describe('should throw error', () => {
       describe('when wrapper is falsy', () => {
@@ -103,9 +174,9 @@ describe('testHelpers', () => {
           },
         });
 
-        const checkboxInpur = findByRole(wrapper, 'checkbox');
-        expect(checkboxInpur.exists()).toBe(true);
-        expect(checkboxInpur.element.name).toBe('stub-name');
+        const checkboxInput = findByRole(wrapper, 'checkbox');
+        expect(checkboxInput.exists()).toBe(true);
+        expect(checkboxInput.element.name).toBe('stub-name');
       });
     });
   });
