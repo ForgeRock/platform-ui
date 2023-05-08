fdfcda21e3693d4a89ef731b154284e861f558da
Wenxuan He <wenxuan.he@forgerock.com>
2023-05-08 13:08:17 -0500

    IAM-3796 Direct Report Detail Page

---
+++ packages/platform-enduser/src/views/MyAccessReview/Roles/index.vue
@@ -0,0 +1,37 @@
+<!-- Copyright (c) 2023 ForgeRock. All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
+<template>
+  <FrMyAccessReviewTable
+    default-sort="role.name"
+    :fields="fields"
+    grant-type="role" />
+</template>
+
+<script>
+
+import FrMyAccessReviewTable from '../MyAccessReviewTable';
+
+export default {
+  name: 'Roles',
+  components: {
+    FrMyAccessReviewTable,
+  },
+  data() {
+    return {
+      fields: [
+        {
+          key: 'roleName',
+          label: this.$t('common.name'),
+          sortable: true,
+        },
+        {
+          key: 'timeConstraint',
+          label: this.$t('pages.myAccess.timeConstraint'),
+        },
+      ],
+    };
+  },
+};
+</script>
