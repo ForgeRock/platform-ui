293a7946f9900d6348ee15233e0f71c9103a05df
Manuel Escobar <manuel.escobar@forgerock.com>
2023-05-24 15:01:28 -0500

    IAM-4132 Update access review user details modal

---
+++ packages/platform-shared/src/views/Governance/CertificationTask/CertificationTaskList/CertificationTaskUserModal/RolesTab/RolesTab.test.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2023 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+/* eslint-disable indent */
+
+import { shallowMount } from '@vue/test-utils';
+import { findByTestId } from '@forgerock/platform-shared/src/utils/testHelpers';
+import FrNoData from '@forgerock/platform-shared/src/components/NoData';
+import RolesTab from './index';
+
+describe('RolesTab', () => {
+  function mountComponent(roles) {
+    return shallowMount(RolesTab, {
+      mocks: {
+        $t: (t) => t,
+      },
+      propsData: {
+        roles,
+      },
+    });
+  }
+
+  describe('component loaded', () => {
+    let wrapper;
+
+    it('Shows FrNoData component when have no roles', () => {
+      wrapper = mountComponent({ result: [] });
+      const rolesTable = findByTestId(wrapper, 'roles-table');
+      expect(rolesTable.exists()).toBeFalsy();
+      expect(wrapper.find(FrNoData).exists()).toBe(true);
+      expect(wrapper.find(FrNoData).props('subtitle')).toBe('governance.certificationTask.lineItemDetailsModal.rolesTab.noItems');
+    });
+
+    it('Shows roles table and not FrNoData when component have roles information', () => {
+      wrapper = mountComponent({
+        result: [
+          {
+            campaignId: 'test',
+            id: 'test-id',
+            item: { type: 'roleMembership' },
+            role: { id: 'test-id-role', name: 'test' },
+            user: { id: 'test' },
+          },
+        ],
+      });
+
+      expect(wrapper.find(FrNoData).exists()).toBe(false);
+      const rolesTable = findByTestId(wrapper, 'roles-table');
+      expect(rolesTable.exists()).toBeTruthy();
+    });
+  });
+});
---
+++ packages/platform-shared/src/views/Governance/CertificationTask/CertificationTaskList/CertificationTaskUserModal/RolesTab/index.vue
@@ -0,0 +1,88 @@
+<!-- Copyright (c) 2023 ForgeRock. All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
+<template>
+  <div>
+    <template v-if="totalRows">
+      <BTable
+        :fields="rolesFields"
+        :items="roles.result"
+        :per-page="pageSize"
+        :current-page="paginationPage"
+        data-testid="roles-table"
+        show-empty
+        responsive>
+        <template #cell(name)="{ item }">
+          <p>{{ nameValue(item.role) }}</p>
+        </template>
+      </BTable>
+      <BPagination
+        v-model="paginationPage"
+        class="py-3 justify-content-center pagination-material-buttons"
+        :per-page="pageSize"
+        :total-rows="totalRows" />
+    </template>
+    <FrNoData
+      v-else
+      :card="false"
+      class="mb-4"
+      icon="inbox"
+      :subtitle="$t('governance.certificationTask.lineItemDetailsModal.rolesTab.noItems')" />
+  </div>
+</template>
+
+<script>
+import { blankValueIndicator } from '@forgerock/platform-shared/src/utils/governance/constants';
+import { get } from 'lodash';
+import { BPagination, BTable } from 'bootstrap-vue';
+import FrNoData from '@forgerock/platform-shared/src/components/NoData';
+
+/**
+ * Tab that allows to visualize the roles of the selected user
+ */
+export default {
+  name: 'RolesTab',
+  components: {
+    BPagination,
+    BTable,
+    FrNoData,
+  },
+  props: {
+    roles: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      blankValueIndicator,
+      paginationPage: 1,
+      pageSize: 5,
+      rolesFields: [
+        {
+          key: 'name',
+          label: this.$t('common.name'),
+          class: 'text-truncate',
+          show: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    /**
+     * Return name value for list item
+     * @param {Object} roleObject - information object returned from API
+     * @returns {String} name value
+     */
+    nameValue(roleObject) {
+      return get(roleObject, 'name', this.blankValueIndicator);
+    },
+  },
+  computed: {
+    totalRows() {
+      return this.roles.result.length;
+    },
+  },
+};
+</script>
