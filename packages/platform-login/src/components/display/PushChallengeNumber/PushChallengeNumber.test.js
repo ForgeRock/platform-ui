365780b0c05b3e91f447279ea0b9e38285801aee
Ingrid van den Berg <ingrid.vandenberg@forgerock.com>
2023-05-30 12:01:30 +0100

    IAM-3962 improve push challenge view

---
+++ packages/platform-login/src/components/display/PushChallengeNumber/PushChallengeNumber.test.js
@@ -0,0 +1,53 @@
+/**
+ * @license
+ * Copyright (c) 2023 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+import { mount } from '@vue/test-utils';
+import i18n from '@/i18n';
+import PushChallengeNumber from './index';
+
+describe('PushChallengeNumber', () => {
+  it('renders push-challenge number', () => {
+    const wrapper = mount(PushChallengeNumber, {
+      i18n,
+      propsData: {
+        callback: {
+          getOutputValue: () => 45,
+        },
+        pushMessage: 'Tap the number [55] on the Push Notification to continue.',
+      },
+    });
+    expect(wrapper.text()).toContain('45');
+  });
+
+  it('renders push-challenge with default message', () => {
+    const defaultAmMessage = 'Tap the number [55] on the Push Notification to continue.';
+    const defaultIDCloudMessage = 'Open your Authenticator app and tap the number shown to sign-in.';
+    const wrapper = mount(PushChallengeNumber, {
+      i18n,
+      propsData: {
+        callback: {
+          getOutputValue: () => 45,
+        },
+        pushMessage: defaultAmMessage,
+      },
+    });
+    expect(wrapper.text()).toContain(defaultIDCloudMessage);
+  });
+
+  it('renders push-challenge override message defined by admin', () => {
+    const wrapper = mount(PushChallengeNumber, {
+      i18n,
+      propsData: {
+        callback: {
+          getOutputValue: () => 45,
+        },
+        pushMessage: 'Tap your forehead this many times',
+      },
+    });
+    expect(wrapper.text()).toContain('Tap your forehead this many times');
+  });
+});
---
+++ packages/platform-login/src/components/display/PushChallengeNumber/index.vue
@@ -0,0 +1,56 @@
+<!-- Copyright (c) 2023 ForgeRock. All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
+<template>
+  <div
+    class="d-flex flex-fill flex-column justify-content-center">
+    <p
+      class="mb-0">
+      {{ pushChallengeMessage }}
+    </p>
+    <h2 class="h1 mb-2 mt-4 p-3">
+      {{ pushChallengeNumber }}
+    </h2>
+  </div>
+</template>
+<script>
+
+/**
+ * @description A view to display push challenge number and message content for authorisation.
+ */
+export default {
+  name: 'PushChallengeNumber',
+  props: {
+    // HiddenValueCallback object that contains the push challenge number.
+    callback: {
+      type: Object,
+      required: true,
+    },
+    // Contains push challenge message
+    pushMessage: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    setPushChallengeMessage() {
+      // If the push-challenge message has not been overriden by admin, replace the default AM message for the default Platform message
+      const defaultString = this.$t('login.pushChallenge.message');
+      const startOfString = 'Tap the number [';
+      const endOfString = '] on the Push Notification to continue.';
+
+      if (this.pushMessage.startsWith(startOfString) && this.pushMessage.endsWith(endOfString)) {
+        return defaultString;
+      }
+      return this.pushMessage;
+    },
+  },
+  data() {
+    return {
+      pushChallengeNumber: this.callback.getOutputValue(),
+      pushChallengeMessage: this.setPushChallengeMessage(),
+    };
+  },
+};
+</script>
