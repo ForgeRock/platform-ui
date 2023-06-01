f666994248db124ac69129ea30b4f83f56f57104
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2023-06-01 11:14:58 -0500

    creating

--- packages/platform-shared/src/views/AutoAccess/Shared/Field/Select/index.vue
+++ packages/platform-shared/src/views/AutoAccess/Shared/Field/Select/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2022 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2022-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -58,7 +58,8 @@ import {
   has,
   map,
 } from 'lodash';
-import VueMultiSelect from 'vue-multiselect';
+// import vue-multiselect from src because dist min/uglified package gets removed in build
+import VueMultiSelect from '../../../../../../../../node_modules/vue-multiselect/src/index';
 import InputLayout from '../Wrapper/InputLayout';
 import InputMixin from '../Wrapper/InputMixin';
 
