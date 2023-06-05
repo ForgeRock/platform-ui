dd2ee1ed45761d0883a310975d83200a4fbead6f
Andrew Hertel <andrew.hertel@forgerock.com>
2023-06-05 16:34:19 -0700

    IAM-2361 Save mappings to individual files

--- packages/platform-shared/src/api/MappingApi.js
+++ packages/platform-shared/src/api/MappingApi.js
@@ -12,18 +12,40 @@ export function getMappingDetails() {
   return generateIdmApi().get('/endpoint/mappingDetails');
 }
 
-export function getMappings(params) {
+/**
+ * Pull full sync.json (not from memory)
+ * @returns results of entire sync.json
+ */
+export function getSyncFile() {
+  return generateIdmApi().get('config/sync');
+}
+
+/**
+ * Pull sync mappings from IDM memory
+ */
+export function getSyncMappings(params) {
   const url = `sync/mappings${encodeQueryString(params)}`;
 
   return generateIdmApi().get(url);
 }
 
-export function getConfigSyncMappings() {
-  return generateIdmApi().get('config/sync');
+/**
+ * Pulls the specified mapping from individual mapping file. Returns 404 if not found.
+ * @param {String} mappingName unique name of mapping
+ */
+export function getConfigMapping(mappingName) {
+  const url = `config/mapping/${mappingName}`;
+
+  return generateIdmApi().get(url);
 }
 
-export function getMapping(mappingId) {
-  const url = `sync/mappings/${mappingId}`;
+/**
+ * Pulls the specified mapping from individual mapping file. If one does not exist,
+ * searches through sync.json for that mapping.
+ * @param {String} mappingName unique name of mapping
+ */
+export function getSyncMapping(mappingName) {
+  const url = `sync/mappings/${mappingName}`;
 
   return generateIdmApi().get(url);
 }
@@ -32,26 +54,30 @@ export function previewMapping(payload) {
   return generateIdmApi().post('script?_action=eval', payload);
 }
 
-export function putMappings(payload) {
+/**
+ * Saves mapping to individual config .json file
+ * @param {String} mappingName Name of mapping to save
+ * @param {Object} payload Mapping details
+ * @returns Promise with mapping details after save or failure results
+ */
+export function putConfigMapping(mappingName, payload) {
+  return generateIdmApi().put(`config/mapping/${mappingName}?waitForCompletion=true`, payload);
+}
+
+export function putSyncMappings(payload) {
   return generateIdmApi().put('config/sync?waitForCompletion=true', payload);
+}
+
+/**
+ * Removes indivdual mapping .json
+ * @param {String} mappingName unique name of mapping
+ * @returns Promise with results of delete
+ */
+export function deleteConfigMapping(mappingName) {
+  return generateIdmApi().delete(`config/mapping/${mappingName}?waitForCompletion=true`);
 }
 
 export function getTargetPreview(resourceId, mapping) {
   const resourceUrl = `sync?_action=getTargetPreview&resourceId=${resourceId}&mapping=${mapping}`;
   return generateIdmApi().post(resourceUrl);
-}
-
-export function buildQueryFilterMappingString(mappingNames) {
-  if (!mappingNames || !mappingNames.length) {
-    return true;
-  }
-
-  let queryFilter = '';
-  mappingNames.forEach((mappingName, index) => {
-    if (index) {
-      queryFilter = `${queryFilter} OR `;
-    }
-    queryFilter = `${queryFilter}name eq "${mappingName}"`;
-  });
-  return queryFilter;
 }
