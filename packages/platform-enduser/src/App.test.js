73cba086fca754a8f891d87c534c8221e9983778
Mario <mario.garcia@forgerock.com>
2023-06-26 11:56:29 -0500

    IAM-4213 platform-enduser add approvals menu item and main page

--- packages/platform-enduser/src/App.test.js
+++ packages/platform-enduser/src/App.test.js
@@ -1,11 +1,10 @@
 /**
- * Copyright (c) 2020-2022 ForgeRock. All rights reserved.
+ * Copyright (c) 2020-2023 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import BootstrapVue from 'bootstrap-vue';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
 import i18n from '@/i18n';
@@ -13,48 +12,62 @@ import App from '@/App';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
-localVue.use(BootstrapVue);
 
-describe('App.vue', () => {
-  const store = new Vuex.Store({
-    state: {
-      SharedStore: { workforceEnabled: false },
-      UserStore: { userDetails: {} },
+let store;
+let wrapper;
+let $route;
+
+function mountComponent(storeMock) {
+  $route = {
+    meta: { hideSideMenu: true },
+  };
+
+  wrapper = shallowMount(App, {
+    localVue,
+    store: storeMock,
+    i18n,
+    mocks: {
+      $route,
     },
-    getters: {
-      'UserStore/userDetails': (state) => state.UserStore.userDetails,
-      UserStore: (state) => state.UserStore,
+    stubs: {
+      RouterLink: true,
+      RouterView: true,
+      Notifications: true,
     },
   });
+}
 
-  let wrapper;
-  let $route;
-
-  beforeEach(() => {
-    $route = {
-      meta: { hideSideMenu: true },
-    };
-
-    wrapper = shallowMount(App, {
-      localVue,
-      store,
-      i18n,
-      mocks: {
-        $route,
+describe('App.vue', () => {
+  beforeEach(async () => {
+    store = new Vuex.Store({
+      state: {
+        SharedStore: { workforceEnabled: false },
+        UserStore: { userDetails: {} },
+        menusFile: 'menus.platform',
       },
-      stubs: {
-        RouterLink: true,
-        RouterView: true,
-        Notifications: true,
+      getters: {
+        'UserStore/userDetails': (state) => state.UserStore.userDetails,
+        UserStore: (state) => state.UserStore,
+        menusFile: (state) => state.menusFile,
       },
     });
   });
 
-  afterAll(() => {
-    wrapper = null;
+  it('Loaded Menus File should load default items', async () => {
+    mountComponent(store);
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.menuItems.length).toEqual(7);
   });
 
-  it('App loaded', () => {
-    expect(wrapper.name()).toBe('App');
+  it('Loaded Menus File with governance V3 Enabled', async () => {
+    store.state.SharedStore.governanceEnabled = true;
+    store.state.SharedStore.governanceEnabledV3 = true;
+    mountComponent(store);
+    await wrapper.vm.$nextTick();
+    const inbox = wrapper.vm.menuItems.find((item) => item.displayName === 'sideMenu.inbox');
+    const approvals = inbox.subItems.find((item) => item.displayName === 'sideMenu.approvals');
+    expect(wrapper.vm.menuItems.length).toEqual(7);
+    expect(inbox).toBeTruthy();
+    expect(approvals).toBeTruthy();
   });
 });
