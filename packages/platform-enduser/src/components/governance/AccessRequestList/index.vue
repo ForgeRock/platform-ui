c800b2a2454fbf1a998640d9335f0f621d116c99
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2023-06-28 15:33:19 -0500

    IAM-4229 request modal base

--- packages/platform-enduser/src/components/governance/AccessRequestList/index.vue
+++ packages/platform-enduser/src/components/governance/AccessRequestList/index.vue
@@ -3,102 +3,108 @@
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
-  <BTable
-    v-if="items.length"
-    class="access-request-list"
-    thead-class="d-none"
-    tbody-tr-class="cursor-pointer"
-    hover
-    :fields="fields"
-    :items="items">
-    <template #cell(details)="{ item }">
-      <div class="mb-2">
-        <small data-testid="request-type">
-          {{ item.details.type }}
-        </small>
-      </div>
-      <BMedia
-        no-body
-        class="mb-2">
-        <BMediaAside class="align-self-center">
-          <FrIcon
-            v-if="getRequestObjectType(item.rawData.requestType) === 'role'"
-            class="mr-1 md-28 rounded-circle"
-            :name="item.details.icon" />
-          <BImg
-            v-else
-            width="24"
-            height="24"
-            class="align-self-center"
-            :src="item.details.icon"
-            :alt="$t('common.logo')" />
-        </BMediaAside>
-        <BMediaBody>
-          <h2
-            class="m-0 h5"
-            data-testid="request-item-name">
-            {{ item.details.name }}
-          </h2>
-          <small
-            class="text-muted"
-            data-testid="request-item-description">
-            {{ item.details.description }}
+  <div>
+    <BTable
+      v-if="items.length"
+      class="access-request-list"
+      thead-class="d-none"
+      tbody-tr-class="cursor-pointer"
+      hover
+      :fields="fields"
+      :items="items"
+      @row-clicked="handleRowClick">
+      <template #cell(details)="{ item }">
+        <div class="mb-2">
+          <small data-testid="request-type">
+            {{ item.details.type }}
           </small>
-        </BMediaBody>
-      </BMedia>
-      <BMedia
-        no-body
-        class="mb-2">
-        <BMediaAside class="align-self-center mr-2">
-          <BImg
-            class="rounded-circle"
-            height="18"
-            width="18"
-            :alt="item.details.requesteeInfo.givenName"
-            :aria-hidden="true"
-            :src="item.details.requesteeInfo.profileImage || require('@forgerock/platform-shared/src/assets/images/avatar.png')" />
-        </BMediaAside>
-        <BMediaBody class="align-self-center">
-          <div
-            class="text-truncate"
-            data-testid="request-item-user">
-            {{ item.details.requesteeInfo.givenName }} {{ item.details.requesteeInfo.sn }}
+        </div>
+        <BMedia
+          no-body
+          class="mb-2">
+          <BMediaAside class="align-self-center">
+            <FrIcon
+              v-if="getRequestObjectType(item.rawData.requestType) === 'role'"
+              class="mr-1 md-28 rounded-circle"
+              :name="item.details.icon" />
+            <BImg
+              v-else
+              width="24"
+              height="24"
+              class="align-self-center"
+              :src="item.details.icon"
+              :alt="$t('common.logo')" />
+          </BMediaAside>
+          <BMediaBody>
+            <h2
+              class="m-0 h5"
+              data-testid="request-item-name">
+              {{ item.details.name }}
+            </h2>
+            <small
+              class="text-muted"
+              data-testid="request-item-description">
+              {{ item.details.description }}
+            </small>
+          </BMediaBody>
+        </BMedia>
+        <BMedia
+          no-body
+          class="mb-2">
+          <BMediaAside class="align-self-center mr-2">
+            <BImg
+              class="rounded-circle"
+              height="18"
+              width="18"
+              :alt="item.details.requesteeInfo.givenName"
+              :aria-hidden="true"
+              :src="item.details.requesteeInfo.profileImage || require('@forgerock/platform-shared/src/assets/images/avatar.png')" />
+          </BMediaAside>
+          <BMediaBody class="align-self-center">
+            <div
+              class="text-truncate"
+              data-testid="request-item-user">
+              {{ item.details.requesteeInfo.givenName }} {{ item.details.requesteeInfo.sn }}
+            </div>
+          </BMediaBody>
+        </BMedia>
+        <div class="d-flex align-items-center">
+          <div>
+            <small
+              class="text-muted"
+              data-testid="request-item-date">
+              {{ getDateString(item.details.date) }}
+            </small>
           </div>
-        </BMediaBody>
-      </BMedia>
-      <div class="d-flex align-items-center">
-        <div>
-          <small
-            class="text-muted"
-            data-testid="request-item-date">
-            {{ getDateString(item.details.date) }}
-          </small>
+          <span
+            style="padding-bottom: 5px;"
+            class="mx-2 opacity-50">
+            .
+          </span>
+          <div>
+            <small
+              class="text-muted"
+              data-testid="request-item-id">
+              {{ $t('governance.accessRequest.idLabel', { id: item.details.id }) }}
+            </small>
+          </div>
+          <div class="ml-2">
+            <BImg
+              height="24"
+              :src="getPriorityImageSrc(item.details.priority)" />
+          </div>
         </div>
-        <span
-          style="padding-bottom: 5px;"
-          class="mx-2 opacity-50">
-          .
-        </span>
-        <div>
-          <small
-            class="text-muted"
-            data-testid="request-item-id">
-            {{ $t('governance.accessRequest.idLabel', { id: item.details.id }) }}
-          </small>
-        </div>
-        <div class="ml-2">
-          <BImg
-            height="24"
-            :src="getPriorityImageSrc(item.details.priority)" />
-        </div>
-      </div>
-    </template>
-    <template #cell(actions)="{ item }">
-      <slot
-        :item="item"
-        name="actions" />
-    </template>
-  </BTable>
+      </template>
+      <template #cell(actions)="{ item }">
+        <slot
+          :item="item"
+          name="actions" />
+      </template>
+    </BTable>
+    <FrRequestModal
+      :type="modalType"
+      :item="modalItem" />
+  </div>
 </template>
 
 <script>
@@ -112,6 +118,7 @@ import {
 import dayjs from 'dayjs';
 import FrIcon from '@forgerock/platform-shared/src/components/Icon';
 import AppSharedUtilsMixin from '@forgerock/platform-shared/src/mixins/AppSharedUtilsMixin';
+import FrRequestModal, { REQUEST_MODAL_TYPES } from '../RequestModal';
 
 /**
  * A table that displays an array of access request objects
@@ -125,6 +132,7 @@ export default {
     BMediaBody,
     BTable,
     FrIcon,
+    FrRequestModal,
   },
   mixins: [AppSharedUtilsMixin],
   props: {
@@ -147,6 +155,8 @@ export default {
           class: 'w-175px',
         },
       ],
+      modalType: REQUEST_MODAL_TYPES.DETAILS,
+      modalItem: null,
     };
   },
   watch: {
@@ -158,6 +168,11 @@ export default {
     },
   },
   methods: {
+    handleRowClick(item) {
+      this.modalItem = item;
+      this.modalType = REQUEST_MODAL_TYPES.DETAILS;
+      this.$bvModal.show('request_modal');
+    },
     /**
      * Converts access request objects to have information at the top level
      * that is necessary for the table display
