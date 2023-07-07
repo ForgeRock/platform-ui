4cc17ef91ee910785aa12a06296fdbee4da5ba6d
Mario <mario.garcia@forgerock.com>
2023-07-07 12:16:08 -0500

    IAM-4217 platform-enduser platform-shared Add request actions modals

--- packages/platform-shared/src/views/Governance/CertificationTask/CertificationForwardModal/index.vue
+++ packages/platform-shared/src/views/Governance/CertificationTask/CertificationForwardModal/index.vue
@@ -13,64 +13,29 @@ of the MIT license. See the LICENSE file for details. -->
       :title="$t('governance.certificationTask.actionsModal.forwardItem')"
       :static="isTesting"
       @ok="forwardItem"
-      @hidden="reset"
       cancel-variant="link"
       ok-variant="info"
       size="lg">
-      <p>
-        {{ $t('governance.certificationTask.actionsModal.forwardItemDescription') }}:
-      </p>
-      <div class="mb-4">
-        <BFormRadioGroup
-          class="mb-4"
-          stacked
-          v-model="forwardToType"
-          :options="forwardToOptions"
-        />
-        <FrGovResourceSelect
-          v-model="forwardToResource"
-          :resource-path="isUserSelected ? 'user' : 'role'"
-          :label="$t('governance.certificationTask.actionsModal.forwardTo')"
-          validation="required"
-        />
-      </div>
-
-      <!-- comment -->
-      <FrField
-        v-model="comment"
-        class="mb-4"
-        type="textarea"
-        validation="required"
-        :label="$t('governance.certificationTask.actionsModal.comment')" />
+      <FrForwardModalContent
+        :description-text="$t('governance.certificationTask.actionsModal.forwardItemDescription')"
+        :comment-label="$t('governance.certificationTask.actionsModal.comment')"
+        @request-comment="updateComment"
+        @request-update-actors="updateActors"
+      />
     </BModal>
   </ValidationObserver>
 </template>
 
 <script>
-import {
-  BFormRadioGroup,
-  BModal,
-} from 'bootstrap-vue';
+import { BModal } from 'bootstrap-vue';
 import { ValidationObserver } from 'vee-validate';
-import FrField from '@forgerock/platform-shared/src/components/Field';
-import FrGovResourceSelect from '@forgerock/platform-shared/src/components/governance/GovResourceSelect';
-
-/**
- * @constant {Object}
- * @default
- */
-const FORWARD_TO_TYPES = {
-  role: 'role',
-  user: 'user',
-};
+import FrForwardModalContent from '@forgerock/platform-shared/src/views/Governance/ForwardModalContent';
 
 export default {
   name: 'CertificationForwardModal',
   components: {
-    BFormRadioGroup,
     BModal,
-    FrField,
-    FrGovResourceSelect,
+    FrForwardModalContent,
     ValidationObserver,
   },
   props: {
@@ -90,14 +55,8 @@ export default {
   data() {
     return {
       comment: '',
-      forwardToUser: '',
-      forwardToRole: '',
-      forwardToType: FORWARD_TO_TYPES.user,
-      forwardToOptions: [
-        { text: this.$t('governance.certificationTask.actionsModal.toUser'), value: FORWARD_TO_TYPES.user },
-        { text: this.$t('governance.certificationTask.actionsModal.toRole'), value: FORWARD_TO_TYPES.role },
-      ],
       isTesting: false,
+      newActorId: '',
     };
   },
   methods: {
@@ -105,34 +64,16 @@ export default {
       const payload = {
         id: this.bulk ? undefined : this.id,
         comment: this.comment,
-        newActorId: this.forwardToType === FORWARD_TO_TYPES.user
-          ? this.forwardToUser
-          : this.forwardToRole,
+        newActorId: this.newActorId,
       };
       const emitter = this.bulk ? 'forward-bulk' : 'forward-item';
       this.$emit(emitter, payload);
     },
-    reset() {
-      this.comment = '';
-      this.forwardToUser = '';
-      this.forwardToRole = '';
+    updateActors(value) {
+      this.newActorId = value;
     },
-  },
-  computed: {
-    isUserSelected() {
-      return this.forwardToType === 'user';
-    },
-    forwardToResource: {
-      get() {
-        return this.isUserSelected ? this.forwardToUser : this.forwardToRole;
-      },
-      set(value) {
-        if (this.isUserSelected) {
-          this.forwardToUser = value;
-        } else {
-          this.forwardToRole = value;
-        }
-      },
+    updateComment(value) {
+      this.comment = value;
     },
   },
 };
