43e67c56d62ac82b23f2e62b7bfccb248d370236
Mario <mario.garcia@forgerock.com>
2023-07-27 15:09:45 -0500

    IAM-4487 platform-enduser add API integration changes for Approvals request actions

--- packages/platform-enduser/src/api/governance/AccessRequestApi.js
+++ packages/platform-enduser/src/api/governance/AccessRequestApi.js
@@ -25,15 +25,14 @@ export function getUserApprovals(userId, params, filter) {
  * @param {String} requestId ID of request
  * @param {String} phaseName Phase name in which the request is
  * @param {String} action action being realized to the request
- * @param {String} comments Comments to leave on request
+ * @param {String} comment Comment to leave on request
  * @param {Array} updatedActors Actor who the request will be forwarded to
  * @returns {Promise}
  */
-export function requestAction(/* requestId, action, phaseName, comments, updatedActors */) {
-  // const url = `/governance/requests/${requestId}?_action=${action}&phaseName=${phaseName}`;
-  return Promise.resolve();
-  // return generateIgaApi().post(url, { comments, updatedActors });
-  // return generateIgaApi().post('/requests/{requestId}/phases/{phaseName}/comment', { comment });
+export function requestAction(requestId, action, phaseName, comment, updatedActors) {
+  const url = `/governance/requests/${requestId}?_action=${action}&phaseName=${phaseName}`;
+  const requestBody = action === 'reassign' ? { comment, updatedActors } : { comment };
+  return generateIgaApi().post(url, requestBody);
 }
 
 export function saveNewRequest(payload) {
--- packages/platform-enduser/src/views/governance/Directory/DirectReportDetail/RevokeRequestModal/RevokeRequestModal.test.js
+++ packages/platform-enduser/src/views/governance/Directory/DirectReportDetail/RevokeRequestModal/RevokeRequestModal.test.js
@@ -1,9 +1,8 @@
 /**
- * Copyright 2023 ForgeRock AS. All Rights Reserved
+ * Copyright (c) 2023 ForgeRock. All rights reserved.
  *
- * Use of this code requires a commercial software license with ForgeRock AS
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
  */
 
 import { mount } from '@vue/test-utils';
