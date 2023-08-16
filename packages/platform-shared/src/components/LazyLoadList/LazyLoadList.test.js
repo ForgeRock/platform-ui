59e9eff85632b40a81df9945705a5568be0b4fe0
Andrew Hertel <andrew.hertel@forgerock.com>
2023-08-16 16:40:51 -0700

    IAM-4276 Show Library in Decision Node Script Editor

---
+++ packages/platform-shared/src/components/LazyLoadList/LazyLoadList.test.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2023 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { mount } from '@vue/test-utils';
+import i18n from '@forgerock/platform-shared/src/i18n';
+import flushPromises from 'flush-promises';
+import LazyLoadList from './index';
+
+describe('LazyLoadList', () => {
+  function mountComponent(propsData, overrideData = {}) {
+    const wrapper = mount(LazyLoadList, {
+      i18n,
+      propsData: {
+        additionalItemsAvailable: false,
+        listItems: [],
+        ...propsData,
+      },
+    });
+    wrapper.setData(overrideData);
+    return wrapper;
+  }
+
+  it('does not load additional items if none are available', async () => {
+    const wrapper = mountComponent();
+    expect(wrapper.find('.spinner-small').exists()).toBeFalsy();
+    await flushPromises();
+    expect(wrapper.find('.spinner-small').exists()).toBeFalsy();
+  });
+
+  it('displays spinner while loading', async () => {
+    const wrapper = mountComponent();
+    expect(wrapper.find('.spinner-small').exists()).toBeFalsy();
+    wrapper.setProps({
+      listItems: [],
+      additionalItemsAvailable: true,
+    });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('.spinner-small').exists()).toBeTruthy();
+  });
+
+  it('displays items after loading', async () => {
+    const wrapper = mountComponent();
+    wrapper.setProps({
+      listItems: [{
+        _id: 'id',
+      }],
+      additionalItemsAvailable: true,
+    });
+
+    expect(wrapper.find('.list-group-item').exists()).toBeTruthy();
+  });
+
+  it('emits request to lazy load list', async () => {
+    const wrapper = mountComponent({
+      additionalItemsAvailable: true,
+    });
+
+    expect(wrapper.emitted('query-additional-items')).toBeTruthy();
+  });
+});
