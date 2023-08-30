4120a08b9874954ba894bd524dea901ac409c4ff
Angel Hernandez <angel.hernandez@forgerock.com>
2023-08-30 13:03:39 -0600

    IAM-4489 Applied the <FrField> component to the BooleanAttributeInputCallback component so the checkbox styling would align with the rest of our form elements

--- packages/platform-login/src/components/callbacks/BooleanAttributeInputCallback/index.vue
+++ packages/platform-login/src/components/callbacks/BooleanAttributeInputCallback/index.vue
@@ -1,35 +1,26 @@
-<!-- Copyright (c) 2020-2021 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
-  <div class="row text-left mb-2">
-    <div class="col-1">
-      <input
-        type="hidden"
-        :name="name"
-        :ref="name"
-        :value="value">
-      <input
-        class="ml-1 mt-1"
-        type="checkbox"
-        :id="'boolean_callback_' + index"
-        :value="value"
-        :checked="value"
-        @change="onToggle()">
-    </div>
-    <div class="col-10">
-      <label
-        class="ml-2"
-        :for="'boolean_callback_' + index">
-        {{ getTranslation(callback.getPrompt()) }}
-      </label>
-    </div>
+  <div class="text-left mb-2">
+    <input
+      type="hidden"
+      :name="name"
+      :ref="name"
+      :value="value">
+    <FrField
+      v-model="value"
+      type="checkbox"
+      :id="'boolean_callback_' + index"
+      :label="getTranslation(callback.getPrompt())"
+      @change="onToggle()" />
   </div>
 </template>
 
 <script>
 import TranslationMixin from '@forgerock/platform-shared/src/mixins/TranslationMixin';
+import FrField from '@forgerock/platform-shared/src/components/Field';
 
 export default {
   name: 'BooleanAttributeInputCallback',
@@ -46,19 +37,17 @@ export default {
       default: 0,
     },
   },
-  mounted() {
-    this.name = `callback_${this.index}`;
-    this.value = this.callback.getInputValue();
+  components: {
+    FrField,
   },
   data() {
     return {
-      name: '',
-      value: false,
+      name: `callback_${this.index}`,
+      value: this.callback?.getInputValue() || false,
     };
   },
   methods: {
     onToggle() {
-      this.value = !this.value;
       this.callback.setInputValue(this.value);
     },
   },
