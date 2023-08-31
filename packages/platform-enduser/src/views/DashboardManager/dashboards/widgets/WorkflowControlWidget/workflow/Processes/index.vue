4cba11defa563bb81ed010cd69d41d6de7e82f78
Andres Castillo <andres.castillo@forgerock.com>
2023-08-31 07:41:19 -0500

    IAM-4715 Precursor: update deprecated named / scoped slot syntax

--- packages/platform-enduser/src/views/DashboardManager/dashboards/widgets/WorkflowControlWidget/workflow/Processes/index.vue
+++ packages/platform-enduser/src/views/DashboardManager/dashboards/widgets/WorkflowControlWidget/workflow/Processes/index.vue
@@ -12,41 +12,41 @@ of the MIT license. See the LICENSE file for details. -->
           :key="id"
           @hide="reset(id)"
           @show="$emit('loadProcess', process)">
-          <div
-            slot="list-item-header"
-            class="d-inline-flex w-100 media">
-            <div class="media-body align-self-center">
-              <h6>{{ process.name }}</h6>
-            </div>
-            <div class="d-flex ml-3 align-self-center">
-              <div
-                class="btn btn-sm btn-link float-right btn-cancel"
-                :ref="`cancel-${id}`">
-                {{ $t('pages.workflow.cancel') }}
+          <template #list-item-header>
+            <div class="d-inline-flex w-100 media">
+              <div class="media-body align-self-center">
+                <h6>{{ process.name }}</h6>
               </div>
-              <div class="btn btn-sm btn-link float-right btn-edit">
-                {{ $t('pages.workflow.start') }}
+              <div class="d-flex ml-3 align-self-center">
+                <div
+                  class="btn btn-sm btn-link float-right btn-cancel"
+                  :ref="`cancel-${id}`">
+                  {{ $t('pages.workflow.cancel') }}
+                </div>
+                <div class="btn btn-sm btn-link float-right btn-edit">
+                  {{ $t('pages.workflow.start') }}
+                </div>
               </div>
             </div>
-          </div>
+          </template>
 
-          <div
-            slot="list-item-collapse-body"
-            class="d-inline-flex w-100">
-            <FrProcess
-              :process-definition="process.processDefinition"
-              :ref="id"
-              @cancel="cancel"
-              @startProcess="(payload) => $emit('startProcess', payload)" />
-          </div>
+          <template #list-item-collapse-body>
+            <div class="d-inline-flex w-100">
+              <FrProcess
+                :process-definition="process.processDefinition"
+                :ref="id"
+                @cancel="cancel"
+                @startProcess="(payload) => $emit('startProcess', payload)" />
+            </div>
+          </template>
         </FrListItem>
       </template>
       <FrListItem v-else>
-        <div
-          slot="list-item-header"
-          class="text-muted text-center w-100">
-          {{ $t('pages.workflow.noProcess') }}
-        </div>
+        <template #list-item-header>
+          <div class="text-muted text-center w-100">
+            {{ $t('pages.workflow.noProcess') }}
+          </div>
+        </template>
       </FrListItem>
     </FrListGroup>
   </div>
