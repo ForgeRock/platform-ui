94d9a17b2f6dae4b7e45f60220dc30b07898ecca
Angel Hernandez <angel.hernandez@forgerock.com>
2023-09-25 20:29:13 -0600

    IAM-2507 Added better test coverage for the update password journey and the validation goto service

--- packages/platform-shared/src/mixins/LoginMixin/index.vue
+++ packages/platform-shared/src/mixins/LoginMixin/index.vue
@@ -73,7 +73,7 @@ export function verifyGotoUrlAndRedirect(url, realm, isAdmin = false, isGotoOnFa
 
   // This aligns the goto parameter order to match what was used in AM and what is documented in the Docs
   const alignGotoPrecedence = process.env.VUE_APP_ALIGN_GOTO_PRECEDENCE;
-  if (alignGotoPrecedence === 'true') {
+  if (alignGotoPrecedence === 'true' || alignGotoPrecedence === true) {
     gotoUrl = !isGotoOnFail
       ? JSON.stringify({ goto: url })
       : JSON.stringify({ goto: urlParams.get('gotoOnFail') });
@@ -105,16 +105,17 @@ export function verifyGotoUrlAndRedirect(url, realm, isAdmin = false, isGotoOnFa
   })
     .post('/users?_action=validateGoto', gotoUrl, { withCredentials: true })
     .then((res) => {
-      if (!isDefaultPath(res.data.successURL) && res.data.successURL !== 'undefined' && res.data.successURL !== null) {
-        return res.data.successURL;
+      const { successURL } = res.data;
+      if (!isDefaultPath(successURL) && successURL !== 'undefined' && successURL !== null && successURL !== 'null') {
+        return successURL;
       }
-      if (isDefaultPath(res.data.successURL) && isGotoOnFail) {
+      if (isDefaultPath(successURL) && isGotoOnFail) {
         return false;
       }
-      if (isDefaultPath(res.data.successURL) && !isDefaultPath(url)) {
+      if (isDefaultPath(successURL) && !isDefaultPath(url)) {
         return url;
       }
-      if (isDefaultPath(res.data.successURL) && isSamlURL(JSON.parse(gotoUrl).goto)) {
+      if (isDefaultPath(successURL) && isSamlURL(JSON.parse(gotoUrl).goto)) {
         return JSON.parse(gotoUrl).goto;
       }
       return redirectUserBasedOnType();
