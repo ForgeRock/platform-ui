ad853e80d8928857fddb5c1f61828a5a67e9ca58
Angel Hernandez <angel.hernandez@forgerock.com>
2023-10-13 11:08:09 -0600

    IAM-4975 Implemented the new Run Report Template view for auto analytics

--- packages/platform-shared/src/api/AutoApi.js
+++ packages/platform-shared/src/api/AutoApi.js
@@ -6,35 +6,54 @@
  */
 
 import { generateAutoAccessReports } from '@forgerock/platform-shared/src/api/BaseApi';
+import encodeQueryString from '../utils/encodeQueryString';
 
 /**
- * Gets the Normal View behavior data
+  * Returns a list of report templates
+  * @param {Object} params Additional query parameters to be encoded
+  *
+  * @returns {Promise}
+  */
+export async function getReportTemplates(params) {
+  const { data } = await generateAutoAccessReports().get(`templates${encodeQueryString(params)}`);
+  return data;
+}
+
+/**
+  * Returns a list of report runs
+  * @param {Object} params Additional query parameters to be encoded
+  *
+  * @returns {Promise}
+  */
+export async function getReportRuns(params) {
+  const { data } = await generateAutoAccessReports().get(`reports/runs${encodeQueryString(params)}`);
+  return data;
+}
+
+/**
+ * Posts a run report request
  *
- * @param {string} userName Platform user's username
- * @param {array} dateRange Array containing the start date (index 0) and end date (index 1)
- * @param {string} template Name of the template
- * @param {number} numberOfItems Number of results
- * @param {boolean} isEnableUsers True for the request on a specific user, false for a request of the tenant only
- * @param {boolean} isEnableAll True for the request on the teant, false for a request of a user
- * @returns {object} Contains job id, status, and status message
+ * @param {String} template Name of the template
+ * @param {Object} payload run report payload
+ * @returns {Object} Contains job id, status, and status message
  */
-export async function runAutoAccessTemplate(username, dateRange, template, numberOfItems, isEnableUsers, isEnableAll) {
+export async function runAutoAccessTemplate(template, payload) {
   const { data: res } = await generateAutoAccessReports().post(`templates/${template}?_action=run`, {
-    parameters: `{"userName":"${username}","startDate":"${dateRange[0]}","endDate":"${dateRange[1]}","numberOfItems":${numberOfItems},"enable_users":${isEnableUsers},"enable_all":${isEnableAll}}`,
+    parameters: JSON.stringify(payload),
   });
   return res;
 }
 
 /**
- * Gets the Normal View behavior data
+ * Gets a run report result
  *
- * @param {string} userName Platform user's username
- * @param {array} dateRange Array containing the start date (index 0) and end date (index 1)
- * @param {string} template Name of the template
- * @param {number} numberOfItems Number of results
- * @param {boolean} isEnableUsers True for the request on a specific user, false for a request of the tenant only
- * @param {boolean} isEnableAll True for the request on the teant, false for a request of a user
- * @returns {object} Contains report count results, and array of results
+ * @param {String} userName Platform user's username
+ * @param {Array} dateRange Array containing the start date (index 0) and end date (index 1)
+ * @param {String} template Name of the template
+ * @param {Number} numberOfItems Number of results
+ * @param {Boolean} isEnableUsers True for the request on a specific user, false for a request of the tenant only
+ * @param {Boolean} isEnableAll True for the request on the teant, false for a request of a user
+ * @returns {Object} Contains report count results, and array of results
  */
 export async function getAutoAccessReportResult(username, dateRange, template, numberOfItems = 2, isEnableUsers = true, isEnableAll = false) {
   const { id, status } = await runAutoAccessTemplate(username, dateRange, template, numberOfItems, isEnableUsers, isEnableAll);
