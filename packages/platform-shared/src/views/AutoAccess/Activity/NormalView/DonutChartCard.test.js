fb6f73bdbc2befb8d9c6344fd50d5835d017b34f
Chuck Masucci <chuck.masucci@forgerock.com>
2023-10-17 16:14:48 -0700

    IAM-4978 Updated tests for Normal View Donut Charts

--- packages/platform-shared/src/views/AutoAccess/Activity/NormalView/DonutChartCard.test.js
+++ packages/platform-shared/src/views/AutoAccess/Activity/NormalView/DonutChartCard.test.js
@@ -10,8 +10,23 @@ import DonutChartCard from './DonutChartCard';
 import i18n from '@/i18n';
 
 describe('DonutChartCard', () => {
-  function setup() {
-    return mount(DonutChartCard, { i18n });
+  const defaultProps = {
+    feature: 'city',
+    isLoading: false,
+    shouldCompare: false,
+    userInfo: {
+      userName: 'TestUser',
+    },
+  };
+
+  function setup(props) {
+    return mount(DonutChartCard, {
+      i18n,
+      propsData: {
+        ...defaultProps,
+        ...props,
+      },
+    });
   }
 
   describe('@renders', () => {
@@ -19,6 +34,21 @@ describe('DonutChartCard', () => {
       const wrapper = setup();
       const donutChartCard = wrapper.findComponent(DonutChartCard);
       expect(donutChartCard.exists()).toBeTruthy();
+    });
+
+    it('displays a second column when shouldCompare is true', async () => {
+      const wrapper = setup({ shouldCompare: true });
+      const tenantchart = wrapper.find('.tenantchart');
+      const userchart = wrapper.find('.userchart');
+      expect(tenantchart.classes()).toContain('tenantchart');
+      expect(userchart.classes()).toContain('userchart');
+    });
+
+    it('displays and image and chart name when shouldCompare is true', async () => {
+      const wrapper = setup({ shouldCompare: true });
+      const chartName = wrapper.find('.chartName > div');
+      expect(chartName.find('img')).toBeTruthy();
+      expect(chartName.find('h5').text()).toBe(defaultProps.userInfo.userName);
     });
   });
 });
