e5da0e44a19a7840e71856c28daf80f226f661bc
Andrew Hertel <andrew.hertel@forgerock.com>
2023-11-10 14:25:24 -0800

    IAM-5018 Orchestration Node Edit Panel

--- packages/platform-shared/src/components/resource/RelationshipEdit/index.vue
+++ packages/platform-shared/src/components/resource/RelationshipEdit/index.vue
@@ -27,7 +27,6 @@ of the MIT license. See the LICENSE file for details. -->
 
     <FrField
       v-model="relationshipField.value"
-      open-direction="bottom"
       :allow-empty="true"
       :close-on-select="closeOnSelect"
       :disabled="disabled"
