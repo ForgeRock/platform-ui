73adf55d0bc1a86d1bbbd1b774b230373a835e73
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2023-12-11 18:01:15 -0600

    IAM-5201 [VPAT] autofocus first field in journey

--- packages/platform-login/src/components/callbacks/ChoiceCallback/index.vue
+++ packages/platform-login/src/components/callbacks/ChoiceCallback/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2020-2022 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2023 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -10,6 +10,7 @@ of the MIT license. See the LICENSE file for details. -->
     <BFormRadioGroup
       v-model="selected.value"
       :options="selected.options"
+      :autofocus="autofocus"
       text-field="text"
       stacked
       @input="callback.setInputValue($event)" />
@@ -18,6 +19,7 @@ of the MIT license. See the LICENSE file for details. -->
     v-else
     v-model="selected.value"
     type="select"
+    :autofocus="autofocus"
     :floating-label="floatingLabel"
     :label="selected.label"
     :name="selected.name"
@@ -44,6 +46,10 @@ export default {
     TranslationMixin,
   ],
   props: {
+    autofocus: {
+      type: Boolean,
+      default: false,
+    },
     callback: {
       type: Object,
       required: true,
