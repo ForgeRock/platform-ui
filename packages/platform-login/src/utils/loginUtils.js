9705475289cb709e1fa44bd84da910f69629eda1
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2024-01-02 14:55:21 -0600

    IAM-5460 added more autocomplete attributes

--- packages/platform-login/src/utils/loginUtils.js
+++ packages/platform-login/src/utils/loginUtils.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2023-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -7,13 +7,28 @@
 
 /**
  * Contains the list of form labels and their corresponding `autocomplete` value.
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
  */
 const autocompleteFieldLookup = {
+  // identification
   username: 'username',
   'user name': 'username',
+  prefix: 'honorific-prefix',
   'first name': 'given-name',
   'last name': 'family-name',
+  'middle name': 'additional-name',
+  'display name': 'nickname',
+  nickname: 'nickname',
   'email address': 'email',
+  'street address': 'street-address',
+  // Phone
+  'phone number': 'tel',
+  'country code': 'tel-country-code',
+  'area code': 'tel-area-code',
+  'phone extension': 'tel-extension',
+  // preferences
+  language: 'language',
+  'preferred language': 'language',
 };
 
 /**
