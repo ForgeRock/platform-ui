e3a8d00745d07ab6b15b808db59ef395e6c9cff6
Andrew Hertel <andrew.hertel@forgerock.com>
2024-01-05 14:26:13 -0800

    IAM-5525 Fix script editor variables

--- packages/platform-shared/src/components/ScriptEditor/ScriptEditor.test.js
+++ packages/platform-shared/src/components/ScriptEditor/ScriptEditor.test.js
@@ -1,11 +1,12 @@
 /**
- * Copyright (c) 2020-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2020-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
 import { shallowMount } from '@vue/test-utils';
+import { Form as VeeForm } from 'vee-validate';
 import ScriptEditor from './index';
 
 describe('ScriptEditor', () => {
@@ -21,6 +22,9 @@ describe('ScriptEditor', () => {
               isFraas: false,
             },
           },
+        },
+        stubs: {
+          VeeForm,
         },
       },
       props: {
