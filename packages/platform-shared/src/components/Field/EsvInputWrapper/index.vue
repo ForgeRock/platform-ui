02e847cdd7c08e7b48bdd1869b9b042ac46bca29
Dave Ernsting <dave.ernsting@forgerock.com>
2024-01-08 12:11:21 +0000

    IAM-5542 Addresses positioning issues with ESV dropdown in tree editor

--- packages/platform-shared/src/components/Field/EsvInputWrapper/index.vue
+++ packages/platform-shared/src/components/Field/EsvInputWrapper/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2023-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -195,9 +195,12 @@ export default {
   height: 23px;
 }
 
-:deep(.field-type-selectWithActions),
+:deep(.field-type-selectWithActions) {
+  top: 14px;
+}
+
 :deep(.field-type-select) {
-  top: 10px;
+  top: 12px;
 }
 
 :deep(.field-type-selectWithActions .btn),
