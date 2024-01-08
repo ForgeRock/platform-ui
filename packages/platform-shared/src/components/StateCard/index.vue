1432d71bef677f19b0a9a9f453a59eef07482db9
Dave Ernsting <dave.ernsting@forgerock.com>
2024-01-08 13:39:10 +0000

    IAM-5551 IAM-5535 fixes position and shadow styling in global and CORS settings

--- packages/platform-shared/src/components/StateCard/index.vue
+++ packages/platform-shared/src/components/StateCard/index.vue
@@ -1,11 +1,11 @@
-<!-- Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2021-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <BCard
     no-body
-    class="my-4">
+    class="my-4 shadow-none">
     <FrListGroup no-margin>
       <FrListItem
         v-for="(listItem, key) in listItems"
