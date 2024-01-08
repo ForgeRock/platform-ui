840ce84e019ca06c343a792c627cf3acc5826ec3
Dave Ernsting <dave.ernsting@forgerock.com>
2024-01-08 13:39:10 +0000

    IAM-5551 IAM-5535 fixes position and shadow styling in global and CORS settings

--- packages/platform-shared/src/components/ListItem/index.vue
+++ packages/platform-shared/src/components/ListItem/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2019-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2019-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -117,7 +117,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.media {
+:deep(.media) {
   height: 38px;
 
   & > * {
