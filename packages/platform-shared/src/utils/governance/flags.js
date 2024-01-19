51e3a34a55d729396e2f775e2c6bd120779b4771
Michael Wong <michael.wong@forgerock.com>
2024-01-18 16:21:26 -0800

    IAM-5575 access review - acknowledge action appears correctly for rule based grants

--- packages/platform-shared/src/utils/governance/flags.js
+++ packages/platform-shared/src/utils/governance/flags.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2023-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -26,8 +26,8 @@ export function isRoleBased(item) {
 }
 
 export function isRuleBased(item) {
-  if (!item.role) return false;
-  return !!item.role?.condition;
+  if (!item.relationship) return false;
+  return !!item.relationship?.conditional;
 }
 
 export function isAcknowledgeType(item) {
