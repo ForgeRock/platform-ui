5b3a99abf4661bc5c3b5ceade5836f79e9a9e745
Andrew Hertel <andrew.hertel@forgerock.com>
2024-01-18 16:23:27 -0800

    IAM-5503 Rename orchestrations to workflows

--- packages/platform-shared/src/api/BaseApi.js
+++ packages/platform-shared/src/api/BaseApi.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2019-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -122,7 +122,7 @@ export function generateIgaApi(requestOverride = {}) {
 }
 
 /**
- * Generates an IGA Orchestration API Axios instance
+ * Generates an IGA Workflow API Axios instance
  * @param {object} requestOverride Takes an object of AXIOS parameters that can be used to either add
  * on extra information or override default properties https://github.com/axios/axios#request-config
  *
