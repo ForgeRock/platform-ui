94d22bfcf0b81c61f1a0282a59cc56223af9fe08
Michael Wong <michael.wong@forgerock.com>
2024-01-29 12:10:34 -0800

    IAM-5742 approvals - view details button for approvals now works properly

--- packages/platform-enduser/src/views/governance/Approvals/index.vue
+++ packages/platform-enduser/src/views/governance/Approvals/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2023-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -110,7 +110,7 @@ of the MIT license. See the LICENSE file for details. -->
                     </template>
                     <BDropdownItem
                       data-testid="dropdown-action-details"
-                      @click="openModal(item, 'DETAILS')">
+                      @click="viewDetails(item)">
                       <FrIcon
                         name="list_alt"
                         class="mr-2"
