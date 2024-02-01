11919c36a964fa4649500a04f4a701fe49926c63
Andres Castillo <andres.castillo@forgerock.com>
2024-01-31 22:45:05 -0500

    IAM-5668 fix routing error on e2e

--- e2e/passwords.sh
+++ e2e/passwords.sh
@@ -8,8 +8,8 @@ export CYPRESS_AM_PASSWORD=$(kubectl --namespace $kubernetes_namespace get secre
 export CYPRESS_ACCESS_TOKEN=`curl -u idm-provisioning:$(kubectl --namespace $kubernetes_namespace get secret amster-env-secrets -o jsonpath='{.data.IDM_PROVISIONING_CLIENT_SECRET}' | base64 --d) --data 'grant_type=client_credentials&scope=fr:idm:*' -k "https://$(kubectl --namespace $kubernetes_namespace get configmaps platform-config -o jsonpath='{.data.FQDN}')/am/oauth2/access_token"`
 export CYPRESS_FQDN=`kubectl --namespace $kubernetes_namespace get configmaps platform-config -o jsonpath='{.data.FQDN}'`
 export CYPRESS_BASE_URL="https://$CYPRESS_FQDN"
-export CYPRESS_MENUS_FILE="menus.e2e"
-export CYPRESS_ROUTES_FILE="routes.e2e"
+export CYPRESS_MENUS_FILE="menus.platform"
+export CYPRESS_ROUTES_FILE="routes.platform"
 export TZ="Europe/London"
 
 $@
