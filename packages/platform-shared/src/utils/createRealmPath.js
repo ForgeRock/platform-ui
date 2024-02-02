01f1fc1c87399fd7c7d56761adf6fbfe176ebd66
Angel Hernandez <angel.hernandez@forgerock.com>
2024-02-01 21:21:29 -0700

    IAM-5127 updated the realm transformation logic to ignore any given path that already has "realms" in the path

--- packages/platform-shared/src/utils/createRealmPath.js
+++ packages/platform-shared/src/utils/createRealmPath.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2022 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -14,7 +14,11 @@ export default function createRealmPath(realm) {
     .split('/')
     // check array for empty strings keep only realm names
     .filter((realmName) => realmName.length >= 1)
-    // join array with /realms/ EX. one/realms/two
+    // filter out any realm names that equal to "realms"
+    // so we prevent an undesired repetitive output in the
+    // join method below such as realms/realms/realms.
+    .filter((realmName) => realmName !== 'realms')
+    // join array with /realms/ EX. firstRealm/realms/secondRealm
     .join('/realms/');
   return `realms/${realmPath}`;
 }
