9d0391cc842b42f78bab91dbed94c29ad51bba89
Huck Elliott <huck.elliott@forgerock.com>
2024-02-13 15:21:51 -0800

    IAM-5844 decouples am dashboard config from workforce saml applications and implements new idm 'My Applications' endpoint to get info for enduser ui that was previously gleaned from dashboard config

---
+++ packages/platform-enduser/src/api/ProfileApi.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2024 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { generateIdmApi } from '@forgerock/platform-shared/src/api/BaseApi';
+/**
+  * Returns a list of user's saml apps
+  * @param {String} userManagedObject user object name example: user, alpha_user, or bravo_user
+  * @param {String} Id user's _id
+  *
+  * @returns {Promise}
+  */
+// eslint-disable-next-line import/prefer-default-export
+export function getUserSamlApplications(userManagedObject, id) {
+  return generateIdmApi().get(`profile/managed/${userManagedObject}/${id}/applications?_fields=*&_queryFilter=/templateName+eq+"saml"`);
+}
