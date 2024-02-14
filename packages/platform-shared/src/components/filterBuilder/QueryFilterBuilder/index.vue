5eacc4057635033648c1ac730525ffa0a5e9ba2b
Julio Ruiz <julio.ruiz@forgerock.com>
2024-02-13 23:40:05 -0500

    IAM-5455 Fix bad request when validates query filter on QueryFilterBuilder component

--- packages/platform-shared/src/components/filterBuilder/QueryFilterBuilder/index.vue
+++ packages/platform-shared/src/components/filterBuilder/QueryFilterBuilder/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2022-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2022-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -336,6 +336,7 @@ export default {
      * @returns {Object} Validated filter if successful, false if not
      */
     validateFilter(filterString) {
+      if (!filterString) return new Promise((resolve) => resolve(false));
       const idmInstance = this.getRequestService();
       const payload = { _queryFilter: filterString };
       return idmInstance.post('/util/validateQueryFilter?_action=validate', payload).then((query) => query.data, () => false);
