ee88c7456a926590c126ed7d59ee7595add07f28
Andres Castillo <andres.castillo@forgerock.com>
2024-02-15 06:58:10 -0500

    IAM-5687 Fix ESV dropdown styling is incorrect in Journey Nodes

--- packages/platform-shared/src/components/Field/EsvDropdown/index.vue
+++ packages/platform-shared/src/components/Field/EsvDropdown/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2023-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -121,12 +121,12 @@ export default {
   margin-left: 0 !important;
 }
 
-.b-dropdown-text {
+:deep(.b-dropdown-text) {
   margin-bottom: 0.5rem;
   margin-top: 0.5rem;
 }
 
-.b-dropdown-form {
+:deep(.b-dropdown-form) {
   padding: 0;
 }
 
