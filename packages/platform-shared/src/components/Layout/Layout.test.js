8468558d12e9c1a2c4d7fd428da94a262b45f538
Dave Ernsting <dave.ernsting@forgerock.com>
2024-02-19 12:59:48 +0000

    IAM-5869 moves docs and help links tests to use component rather than E2E testing

--- packages/platform-shared/src/components/Layout/Layout.test.js
+++ packages/platform-shared/src/components/Layout/Layout.test.js
@@ -6,6 +6,7 @@
  */
 
 import { mount, RouterLinkStub } from '@vue/test-utils';
+import Navbar from '@forgerock/platform-shared/src/components/Navbar/';
 import { setupTestPinia } from '@/utils/testPiniaHelpers';
 import { findByTestId } from '@/utils/testHelpers';
 import i18n from '@/i18n';
@@ -47,8 +48,8 @@ describe('Layout', () => {
         releaseInfo: {
           currentVersion: '123',
         },
+        ...props,
       },
-      ...props,
     });
   }
 
@@ -70,6 +71,28 @@ describe('Layout', () => {
 
       expect(release.text()).toContain('ID Cloud Version 123');
       expect(link.props().to).toEqual({ name: 'TenantSettings', params: { resourceName: 'details' } });
+    });
+  });
+
+  describe('setting URLs for external help and docs sites', () => {
+    it('should set the correct help URL', () => {
+      const wrapper = setup();
+      const navBar = wrapper.findComponent(Navbar);
+      expect(navBar.props().helpUrl).toBe('https://backstage.forgerock.com/');
+    });
+
+    it('should set the correct docs link for cloud environments', () => {
+      const wrapper = setup({
+        isFraas: true,
+      });
+      const navBar = wrapper.findComponent(Navbar);
+      expect(navBar.props().docsLink).toBe('https://backstage.forgerock.com/docs/idcloud/latest/index.html');
+    });
+
+    it('should set the correct docs link for non cloud environments', () => {
+      const wrapper = setup();
+      const navBar = wrapper.findComponent(Navbar);
+      expect(navBar.props().docsLink).toBe('https://backstage.forgerock.com/docs/index.html');
     });
   });
 });
--- packages/platform-shared/src/components/Navbar/Navbar.test.js
+++ packages/platform-shared/src/components/Navbar/Navbar.test.js
@@ -1,11 +1,12 @@
 /**
- * Copyright (c) 2019-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2019-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+import i18n from '@/i18n';
 import { setupTestPinia } from '../../utils/testPiniaHelpers';
 import Navbar from './index';
 
@@ -14,20 +15,57 @@ describe('Navbar Component', () => {
 
   beforeEach(() => {
     setupTestPinia();
-    wrapper = shallowMount(Navbar, {
+  });
+
+  function setup(props) {
+    wrapper = mount(Navbar, {
       global: {
         mocks: {
-          $t: () => {},
           $store: {
             state: {},
           },
         },
-        stubs: ['RouterLink'],
+        plugins: [i18n],
       },
+      props,
     });
-  });
+  }
 
-  it('Component successfully loaded', () => {
-    expect(wrapper.vm.profileImage).toEqual('');
+  describe('showing links', () => {
+    it('shows a docs link when configured to and passed a value', () => {
+      setup({
+        showDocsLink: true,
+        docsLink: 'https://test.com',
+      });
+
+      expect(wrapper.find('a[href="https://test.com"]').text()).toContain('Docs');
+    });
+
+    it('hides the docs link when configured to', () => {
+      setup({
+        showDocsLink: false,
+        docsLink: 'https://test.com',
+      });
+
+      expect(wrapper.find('a[href="https://test.com"]').exists()).toBe(false);
+    });
+
+    it('shows a help link when configured to and passed a value', () => {
+      setup({
+        showHelpLink: true,
+        helpUrl: 'https://test-help.com',
+      });
+
+      expect(wrapper.find('a[href="https://test-help.com"]').text()).toContain('Help & Support');
+    });
+
+    it('hides the help link when configured to', () => {
+      setup({
+        showHelpLink: false,
+        helpUrl: 'https://test-help.com',
+      });
+
+      expect(wrapper.find('a[href="https://test-help.com"]').exists()).toBe(false);
+    });
   });
 });
