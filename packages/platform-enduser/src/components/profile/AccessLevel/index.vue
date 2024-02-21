677823930c1a82a4a25f33ecf54589fa65e829c3
Julio Ruiz <julio.ruiz@forgerock.com>
2024-02-20 22:06:27 -0500

    IAM-4965 Fix Personal Data Sharing styles on End User UI

--- packages/platform-enduser/src/components/profile/AccessLevel/index.vue
+++ packages/platform-enduser/src/components/profile/AccessLevel/index.vue
@@ -1,17 +1,17 @@
-<!-- Copyright (c) 2020-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <dl class="row">
     <dt class="col-xl-5 mb-3">
-      {{ parent.$t('pages.profile.consent.accountAccess') }}
+      {{ $t('pages.profile.consent.accountAccess') }}
     </dt>
     <dd class="col-xl-7 mb-4">
       <ul class="list-unstyled text-muted">
         <li
           class="py-1"
-          v-for="field in props.fields"
+          v-for="field in fields"
           :key="field.name">
           <FrIcon
             class="md-16 text-success font-weight-bolder mr-2"
@@ -30,6 +30,12 @@ import FrIcon from '@forgerock/platform-shared/src/components/Icon';
 export default {
   components: {
     FrIcon,
+  },
+  props: {
+    fields: {
+      type: Array,
+      default: () => [],
+    },
   },
 };
 </script>
