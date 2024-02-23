7b4943f352332e076bac05ee06ddb6b21cb8e67e
Andrew Hertel <andrew.hertel@forgerock.com>
2024-02-23 15:54:10 -0800

    IAM-5878 Governance events filter builder

---
+++ packages/platform-shared/src/components/filterBuilder/GovernanceFilterBuilder.test.js
@@ -0,0 +1,223 @@
+/**
+ * Copyright (c) 2022-2024 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { flushPromises, mount } from '@vue/test-utils';
+import GovernanceFilterBuilder from './GovernanceFilterBuilder';
+import i18n from '@/i18n';
+
+const subFilters1 = {
+  operator: 'contains', field: '', value: '', uniqueIndex: 2, temporalValue: 'before',
+};
+const subFilters2 = {
+  operator: 'equals', field: 'sn', value: 'name', uniqueIndex: 3, temporalValue: 'after',
+};
+const subFilters2Not = {
+  operator: 'not_equals', field: 'sn', value: 'name', uniqueIndex: 1, temporalValue: 'before',
+};
+const subFilterStartsWith = {
+  operator: 'starts_with', field: 'sn', value: 'name', uniqueIndex: 1, temporalValue: 'before',
+};
+const subFilters3 = {
+  operator: 'equals', field: 'sn', value: 'name', uniqueIndex: 1, temporalValue: 'before',
+};
+const subFilters3Not = {
+  operator: 'not_equals', field: 'sn', value: 'name', uniqueIndex: 1, temporalValue: 'before',
+};
+const subFilters4 = {
+  operator: 'equals', field: 'sn', value: 'name', uniqueIndex: 4, temporalValue: 'before',
+};
+
+describe('GovernanceFilterBuilder', () => {
+  function mountComponent(props, overrideData = {}) {
+    const wrapper = mount(GovernanceFilterBuilder, {
+      global: {
+        plugins: [i18n],
+        mocks: {
+          $store: {
+            state: {
+              SharedStore: {
+                workforceEnabled: true,
+              },
+            },
+          },
+        },
+      },
+      props: {
+        disabled: false,
+        resourceName: 'user',
+        properties: [{ value: 'sn' }],
+        filterValue: {},
+        hideGroup: false,
+        prefixGroupText: 'prefixGroupText',
+        showTemporalValueField: true,
+        ...props,
+      },
+    });
+    wrapper.setData(overrideData);
+    return wrapper;
+  }
+
+  describe('Unit tests', () => {
+    it('Creates default group', async () => {
+      const wrapper = mountComponent();
+      await flushPromises();
+      expect(wrapper.vm.getDefaultGroup()).toEqual({ subfilters: [subFilters1], operator: 'or', uniqueIndex: 3 });
+
+      expect(wrapper.vm.getDefaultGroup([subFilters4])).toEqual({
+        subfilters: [subFilters4], operator: 'or', uniqueIndex: 4,
+      });
+    });
+
+    it('Creates default rule', () => {
+      const wrapper = mountComponent();
+      expect(wrapper.vm.getDefaultRule()).toEqual(subFilters1);
+      expect(wrapper.vm.getDefaultRule('after', 'sn', 'equals', 'name')).toEqual(subFilters2);
+    });
+
+    it('Creates unique index', async () => {
+      const wrapper = mountComponent();
+      await flushPromises();
+      wrapper.vm.uniqueIndex = 0;
+      expect(wrapper.vm.getUniqueIndex()).toEqual(1);
+    });
+  });
+
+  describe('Component tests', () => {
+    it('Updates filter with new group', async () => {
+      const filterBefore = { filterValue: { or: [{ equals: { right: { literal: 'name' }, left: 'user.before.sn' } }] } };
+      const wrapper = mountComponent(filterBefore);
+      await flushPromises();
+      const addRule = {
+        depth: 0, index: 0, path: '0', value: { field: 'sn' },
+      };
+      const filterAfter = {
+        subfilters: [subFilters3, { operator: 'or', subfilters: [subFilters1], uniqueIndex: 3 }],
+        operator: 'or',
+        uniqueIndex: 0,
+      };
+
+      const filterBuilderGroup = wrapper.findComponent('.pb-3.background-none');
+      filterBuilderGroup.vm.$emit('add-rule', addRule);
+      expect(wrapper.vm.queryFilter).toEqual(filterAfter);
+    });
+
+    it('Updates filter with new rule', async () => {
+      const filterBefore = { filterValue: { or: [{ equals: { right: { literal: 'name' }, left: 'user.before.sn' } }] } };
+      const wrapper = mountComponent(filterBefore);
+      await flushPromises();
+      const addRule = {
+        depth: 0, index: 0, path: '0', value: { field: 'sn' }, type: 'row',
+      };
+      const filterAfter = {
+        subfilters: [subFilters3, subFilters1],
+        operator: 'or',
+        uniqueIndex: 0,
+      };
+
+      const filterBuilderGroup = wrapper.findComponent('.pb-3.background-none');
+      filterBuilderGroup.vm.$emit('add-rule', addRule);
+      expect(wrapper.vm.queryFilter).toEqual(filterAfter);
+    });
+
+    it('Updates filter with operator change', () => {
+      const filterBefore = { filterValue: { or: [{ starts_with: { prefix: { literal: 'name' }, value: 'user.before.sn' } }] } };
+      const wrapper = mountComponent(filterBefore);
+      const ruleChange = {
+        depth: 0, path: '0', value: 'and',
+      };
+      const filterAfter = { subfilters: [subFilterStartsWith], operator: 'and', uniqueIndex: 0 };
+
+      const filterBuilderGroup = wrapper.findComponent('.pb-3.background-none');
+      filterBuilderGroup.vm.$emit('operator-change', ruleChange);
+      expect(wrapper.vm.queryFilter).toEqual(filterAfter);
+    });
+
+    it('Updates nothing if incorrect event name is passed', () => {
+      const filterBefore = { filterValue: { or: [{ starts_with: { prefix: { literal: 'name' }, value: 'user.before.sn' } }] } };
+      const wrapper = mountComponent(filterBefore);
+      const ruleChange = {
+        depth: 0, path: '0', value: 'and',
+      };
+      const filterAfter = { subfilters: [subFilterStartsWith], operator: 'or', uniqueIndex: 0 };
+
+      // have to call directly as there is no event linked to this specific event
+      wrapper.vm.updateFilter('invalid-event-name', ruleChange);
+      expect(wrapper.vm.queryFilter).toEqual(filterAfter);
+    });
+
+    it('Updates filter with rule change', () => {
+      const filterBefore = { filterValue: { or: [{ starts_with: { prefix: { literal: 'name' }, value: 'user.before.sn' } }] } };
+      const wrapper = mountComponent(filterBefore);
+      const ruleChange = {
+        depth: 0, index: 0, path: '0', value: { operator: 'not_equals' },
+      };
+      const filterAfter = { subfilters: [subFilters2Not], operator: 'or', uniqueIndex: 0 };
+
+      const filterBuilderGroup = wrapper.findComponent('.pb-3.background-none');
+      filterBuilderGroup.vm.$emit('rule-change', ruleChange);
+      expect(wrapper.vm.queryFilter).toEqual(filterAfter);
+    });
+
+    it('Updates filter by removing rule', () => {
+      const filterBefore = {
+        filterValue: {
+          or: [
+            {
+              not_equals: {
+                right: {
+                  literal: 'name',
+                },
+                left: 'user.before.sn',
+              },
+            },
+            {
+              equals: {
+                right: {
+                  literal: 'active',
+                },
+                left: 'user.before.accountStatus',
+              },
+            },
+          ],
+        },
+      };
+      const wrapper = mountComponent(filterBefore);
+      const removeRule = { depth: 0, index: 1, path: '0' };
+      const filterAfter = { operator: 'or', subfilters: [subFilters3Not], uniqueIndex: 0 };
+
+      const filterBuilderGroup = wrapper.findComponent('.pb-3.background-none');
+      filterBuilderGroup.vm.$emit('remove-rule', removeRule);
+      expect(wrapper.vm.queryFilter).toEqual(filterAfter);
+    });
+
+    it('toggles between basic and advanced editor', async () => {
+      const initialFilter = { filterValue: { or: [{ starts_with: { prefix: { literal: 'name' }, value: 'user.before.sn' } }] } };
+      const wrapper = mountComponent(initialFilter);
+
+      expect(wrapper.find('.fr-script-editor').exists()).toBe(false);
+      const advancedButton = wrapper.findAllComponents('button').filter((item) => item.text().includes('Advanced Editor'))[0];
+      advancedButton.trigger('click');
+      await flushPromises();
+      expect(wrapper.find('.fr-script-editor').exists()).toBe(true);
+      const basicButton = wrapper.findAllComponents('button').filter((item) => item.text().includes('Basic Editor'))[0];
+      basicButton.trigger('click');
+      await flushPromises();
+      expect(wrapper.find('.fr-script-editor').exists()).toBe(false);
+    });
+
+    it('parses and checks filter string', async () => {
+      const initialFilter = { filterValue: { or: [{ starts_with: { prefix: { literal: 'name' }, value: 'user.before.sn' } }] } };
+      const wrapper = mountComponent(initialFilter);
+
+      const advancedButton = wrapper.findAllComponents('button').filter((item) => item.text().includes('Advanced Editor'))[0];
+      advancedButton.trigger('click');
+      await flushPromises();
+      wrapper.findComponent('.form-group').vm.$emit('input', { source: '{"filterString":"stringValue"}' });
+      expect(wrapper.emitted('filter-update')[0]).toEqual([{ filterString: 'stringValue' }]);
+    });
+  });
+});
--- packages/platform-shared/src/components/filterBuilder/utils/filterBuilderUtils.js
+++ packages/platform-shared/src/components/filterBuilder/utils/filterBuilderUtils.js
@@ -1,9 +1,150 @@
 /**
- * Copyright (c) 2022 ForgeRock. All rights reserved.
+ * Copyright (c) 2022-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+
+const filterFieldMap = {
+  starts_with: 'value',
+  not_starts_with: 'value',
+  ends_with: 'value',
+  has_changed: 'left',
+  has_not_changed: 'left',
+  not_ends_with: 'value',
+  equals: 'left',
+  not_equals: 'left',
+  gte: 'left',
+  gt: 'left',
+  lte: 'left',
+  lt: 'left',
+  contains: 'in_string',
+  not_contains: 'in_string',
+};
+const filterValueMap = {
+  starts_with: 'prefix',
+  not_starts_with: 'prefix',
+  ends_with: 'suffix',
+  not_ends_with: 'suffix',
+  equals: 'right',
+  not_equals: 'right',
+  gte: 'right',
+  gt: 'right',
+  lte: 'right',
+  lt: 'right',
+  contains: 'search_string',
+  not_contains: 'search_string',
+};
+const operatorMap = {
+  has_changed: 'not_equals',
+  has_not_changed: 'equals',
+};
+
+/**
+ * Converts filter from shared filter builder format to IGA format
+ * {
+ *   operator: 'or',
+ *   subfilters: [
+ *     {
+ *       field: 'sn',
+ *       operator: 'starts_with',
+ *       value: 'koya',
+ *       temporalValue: 'before',
+ *     },
+ *   ],
+ * },
+ * {
+ *   or: [
+ *     {
+ *       starts_with: {
+ *         prefix: {
+ *           literal: 'koya',
+ *         },
+ *         value: 'user.before.sn',
+ *       },
+ *     },
+ *   ],
+ * },
+ * @param {Object} filter component filter to convert
+ * @param {String} resourceName name of resource filter is aimed against
+ * @param {Array} properties list of properties available
+ */
+export function convertToIGAFilter(filter, resourceName = 'user', properties) {
+  const convertedFilter = {
+    [filter.operator]: [],
+  };
+  filter.subfilters.forEach((filterRule) => {
+    if (filterRule.operator === 'or' || filterRule.operator === 'and') {
+      convertedFilter[filter.operator].push(convertToIGAFilter(filterRule, resourceName, properties));
+    } else {
+      const operator = operatorMap[filterRule.operator] || filterRule.operator;
+      const subfilter = { [operator]: {} };
+      if (filterRule.operator.endsWith('_changed')) {
+        // Special consideration for has changed and has not changed
+        subfilter[operator].left = `${resourceName}.before.${filterRule.field}`;
+        subfilter[operator].right = `${resourceName}.after.${filterRule.field}`;
+      } else {
+        subfilter[operator][filterValueMap[operator]] = filterRule.value.startsWith(`${resourceName}.`) ? filterRule.value : { literal: filterRule.value };
+        const selectedProperty = properties.find((property) => property.value === filterRule.field);
+        if (selectedProperty?.type === 'array' && selectedProperty.path) {
+          // We need to set arrays of relationships back to in_string_array instead of in_string
+          subfilter[operator].in_string_array = `${resourceName}.${filterRule.temporalValue}.${filterRule.field}`;
+        } else {
+          subfilter[operator][filterFieldMap[operator]] = `${resourceName}.${filterRule.temporalValue}.${filterRule.field}`;
+        }
+      }
+      convertedFilter[filter.operator].push(subfilter);
+    }
+  });
+  return convertedFilter;
+}
+
+/**
+ * Takes IGA formatted filter and converts to format that can be consumed by shared filter builder
+ * @property {Object} filter - IGA filter to convert
+ * @returns {Object} filterBuilder-formatted filter
+ */
+export function convertFromIGAFilter(filter, currentUniqueIndex = 0) {
+  let uniqueIndex = currentUniqueIndex;
+  const igaFilter = Object.keys(filter).length ? filter : { or: [{ starts_with: { prefix: { literal: '' }, value: 'user.before.' } }] };
+  const filterEntry = Object.entries(igaFilter)[0];
+  const convertedFilter = {
+    operator: filterEntry[0],
+    subfilters: [],
+    uniqueIndex,
+  };
+  filterEntry[1].forEach((filterRule) => {
+    const [operator, fieldValueObject] = Object.entries(filterRule)[0];
+    if (operator === 'or' || operator === 'and') {
+      const subfilter = convertFromIGAFilter(filterRule, uniqueIndex + 1);
+      convertedFilter.subfilters.push(subfilter.convertedFilter);
+      uniqueIndex = subfilter.uniqueIndex;
+    } else {
+      uniqueIndex += 1;
+      const subfilter = {
+        operator,
+        uniqueIndex,
+      };
+      // find the key that field converts to
+      subfilter.field = fieldValueObject[filterFieldMap[operator]] || fieldValueObject.in_string_array || '';
+      // find the key that value converts to
+      subfilter.value = fieldValueObject[filterValueMap[operator]] || fieldValueObject.search_string_array || '';
+      if (typeof subfilter.value === 'object') {
+        // if there is a literal value, we want to remove that layer
+        subfilter.value = subfilter.value.literal;
+      }
+
+      if (subfilter.field) {
+        // pull temporal value from field [resourceName, temporalValue, fieldName]
+        const splitField = subfilter.field.split('.');
+        [, subfilter.temporalValue, subfilter.field] = splitField;
+      }
+      convertedFilter.subfilters.push(subfilter);
+    }
+  });
+
+  return { convertedFilter, uniqueIndex };
+}
 
 /**
  * Locates group within path to add or update
@@ -31,6 +172,7 @@ export function findGroup(filter, paths, pathIndex, depth) {
  * NOTE: Need to use basic for loop, as forEach is not available for strings
  *
  * @property {string} queryFilterString - stringified query filter
+ * @property {Number} maxDepth - Maximum depth allowed in filter string
  *
  * @returns {Boolean} Whether current query filter is three layers of depth or less
  */
@@ -58,4 +200,95 @@ export function checkIfWithinThreeLayers(queryFilterString, maxDepth) {
     }
   }
   return withinThreeLayers;
+}
+
+/**
+ * Checks if IGA filter has depth greater than basic editor allows
+ * @param {Object} filter Current IGA filter
+ * @param {Number} currentDepth recursive integer uses to determine depth
+ * @param {Number} maxDepth Maximum allowed depth
+ * @returns Boolean of whether filter is with specified depth
+ */
+export function checkIGAFilterWithinNLayers(filter, currentDepth, maxDepth) {
+  if (currentDepth > maxDepth) {
+    return false;
+  }
+  const layer = Object.entries(filter)[0][1];
+  return layer.every((rule) => {
+    if (rule.or || rule.and) {
+      return checkIGAFilterWithinNLayers(rule, currentDepth + 1, maxDepth);
+    }
+    return true;
+  });
+}
+
+/**
+ * Checks if stringified IGA filter contains key signifying it contains array value
+ * @param {String} filter stringified filter to check if it includes array key
+ * @returns Boolean of whether filter contains array key
+ */
+export function checkIGAFilterForArrays(filter) {
+  return filter.includes('search_string_array');
+}
+
+/**
+ * Checks if IGA filter contains matching before/after conditions for each property in same level
+ * @param {String} filter IGA filter to validate
+ * @returns Boolean of whether filter is valid
+ */
+export function checkIGAFilterMeetsRequirements(filter) {
+  const layer = Object.values(filter)[0];
+  const properties = {};
+  layer.forEach((subfilter) => {
+    // don't need to check for second condition if subfilter is an or or an and, or if equals or not_equals and both sides of comparison are fields
+    if (!(
+      subfilter.or
+      || subfilter.and
+      || (
+        subfilter.equals && (
+          (
+            typeof subfilter.equals.left === 'string'
+            && (subfilter.equals.left.includes('before') || subfilter.equals.left.includes('after'))
+          )
+          && (
+            typeof subfilter.equals.right === 'string'
+            && (subfilter.equals.right.includes('before') || subfilter.equals.right.includes('after'))
+          )
+        )
+      )
+      || (
+        subfilter.not_equals && (
+          (
+            typeof subfilter.not_equals.left === 'string'
+            && (subfilter.not_equals.left.includes('before') || subfilter.not_equals.left.includes('after'))
+          )
+          && (
+            typeof subfilter.not_equals.right === 'string'
+            && (subfilter.not_equals.right.includes('before') || subfilter.not_equals.right.includes('after'))
+          )
+        )
+      )
+    )) {
+      // setup to determine if there is at least one after condition to match each unique before property
+      const subfilterValues = Object.values(subfilter)[0];
+      const value = (subfilterValues.value || subfilterValues.left || subfilterValues.in_string || subfilterValues.in_string_array);
+      const propertyName = value.split('.')[2];
+      if (!properties[propertyName]) {
+        properties[propertyName] = {};
+      }
+      if (value.includes('before')) {
+        properties[propertyName].beforeFound = true;
+      } else if (value.includes('after')) {
+        properties[propertyName].afterFound = true;
+      }
+    }
+  });
+  const allSubfiltersValid = layer.every((subfilter) => {
+    if (subfilter.or || subfilter.and) {
+      return checkIGAFilterMeetsRequirements(subfilter);
+    }
+    return true;
+  });
+  const currentLayerValid = Object.values(properties).every((property) => property.beforeFound && property.afterFound);
+  return allSubfiltersValid && currentLayerValid;
 }
