0873a7d3eed5af870c57d75370bf48d1d0e66014
Dave Ernsting <dave.ernsting@forgerock.com>
2024-02-29 16:42:53 +0000

    IAM-5959 Fixes overrideTranslations import errors

--- packages/platform-shared/src/utils/overrideTranslations.js
+++ packages/platform-shared/src/utils/overrideTranslations.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -133,7 +133,7 @@ function getTranslationOverrides(idmContext, locale, fallbackLocales, packageNam
  * @param {String} packageName the package to override translations in (enduser, login)
  * @returns a resolved promise, after the translations have been overridden for main locale and fallback locale
  */
-export default function overrideTranslations(idmContext, i18n, packageName) {
+export function overrideTranslations(idmContext, i18n, packageName) {
   const { locale, fallbackLocale } = i18n.global;
 
   // i18n fallback locale can be an array of strings or a single string
