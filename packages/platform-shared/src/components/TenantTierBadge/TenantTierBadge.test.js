d196ae5b95b681eae1aa232b572bc26b21684abc
Andres Castillo <andres.castillo@forgerock.com>
2024-03-20 09:36:01 -0500

    IAM-6127 Improvements for Platform UI - Github repo

--- packages/platform-shared/src/components/TenantTierBadge/TenantTierBadge.test.js
+++ packages/platform-shared/src/components/TenantTierBadge/TenantTierBadge.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2022-2024 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -8,7 +8,7 @@
 import { mount } from '@vue/test-utils';
 import TenantTierBadge from './index';
 import { findByTestId } from '../../utils/testHelpers';
-import i18n from '../../../../platform-admin/src/i18n';
+import i18n from '@/i18n';
 
 describe('TenantTierBadge', () => {
   it('renders correct abbreviation: "Dev", and associated css classes for a "development" badge: "blue-tenant, tenant-badge"', () => {
@@ -24,6 +24,6 @@ describe('TenantTierBadge', () => {
     const badge = findByTestId(wrapper, 'tenant-tier-badge');
     expect(badge.classes()).toContain('blue-tenant');
     expect(badge.classes()).toContain('tenant-badge');
-    expect(badge.text()).toBe('Dev');
+    expect(badge.text()).toBe('DEVELOPMENT');
   });
 });
