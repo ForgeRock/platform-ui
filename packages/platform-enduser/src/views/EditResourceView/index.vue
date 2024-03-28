ee06e7aad729fd1cbb2be8604c8fff4499bb4d6c
Alexis Castro <alexis.castro@forgerock.com>
2024-03-28 09:16:04 -0600

    IAM-6036 Refresh Managed Object when saving

--- packages/platform-enduser/src/views/EditResourceView/index.vue
+++ packages/platform-enduser/src/views/EditResourceView/index.vue
@@ -3,7 +3,9 @@
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
-  <FrEditResource @breadcrumb-data-changed="setBreadcrumb($event.route, $event.routeName)" />
+  <FrEditResource
+  @breadcrumb-data-changed="setBreadcrumb($event.route, $event.routeName)"
+  @save-clicked="(refreshData) => refreshData()"/>
 </template>
 
 <script>
