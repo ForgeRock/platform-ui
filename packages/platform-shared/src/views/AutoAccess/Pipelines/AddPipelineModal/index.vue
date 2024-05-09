fcff814b9c0a9fb5786886709060e92febb396bd
Chuck Masucci <chuck.masucci@forgerock.com>
2024-05-09 14:08:01 -0700

    IAM-6440 Fixed Auto Access pipeline name payload

--- packages/platform-shared/src/views/AutoAccess/Pipelines/AddPipelineModal/index.vue
+++ packages/platform-shared/src/views/AutoAccess/Pipelines/AddPipelineModal/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2022-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2022-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -278,7 +278,7 @@ export default {
       this.loading = true;
       savePipeline({
         pipeline_definition_id: id,
-        pipelineName,
+        pipeline_name: pipelineName,
         pipeline_type: pipelineType,
         pipeline_parameters: {
           ...trainingParameters,
