1271b0857b4aa0cc8595e35f0dedf05d8874bbd4
Josh Newton <josh.newton@forgerock.com>
2024-07-01 12:56:31 +0100

    IAM-4363 - Add ability for a script writer to insert library script properties into the code editor

--- packages/platform-shared/src/components/LazyLoadList/index.vue
+++ packages/platform-shared/src/components/LazyLoadList/index.vue
@@ -1,10 +1,9 @@
-<!-- Copyright (c) 2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2023-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <div
-    class="library-div mb-3"
     ref="itemsContainer"
     @scroll="lazyLoadItems">
     <BListGroup flush>
