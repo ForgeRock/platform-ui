6ee1afed7cf466776cdb98b16a1c0328e350d283
Michael Wong <michael.wong@forgerock.com>
2024-07-01 15:06:17 -0700

    IAM-6864 certification - fix issue with org filter not searching on input

---
+++ packages/platform-shared/src/components/governance/GovResourceMultiselect.test.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2023-2024 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { flushPromises, shallowMount } from '@vue/test-utils';
+import * as CommonsApi from '@forgerock/platform-shared/src/api/governance/CommonsApi';
+import GovResourceMultiselect from './GovResourceMultiselect';
+
+describe('GovResourceMultiselect', () => {
+  let wrapper;
+  function mountComponent(props) {
+    return shallowMount(GovResourceMultiselect, {
+      global: {
+        mocks: {
+          $t: (text) => (text),
+        },
+      },
+      props: {
+        description: 'testDescription',
+        label: 'testLabel',
+        name: 'testName',
+        ...props,
+      },
+    });
+  }
+
+  // Simulate the API response for initial values
+  const initialData = [
+    { name: 'App 1', id: 'app1' },
+    { name: 'App 2', id: 'app2' },
+  ];
+  CommonsApi.getResource = jest.fn().mockImplementation((arg1, arg2) => {
+    let returnValue = initialData;
+    if (arg2.queryString === 'app3') returnValue = [{ name: 'App 3', id: 'app3' }];
+    return Promise.resolve({ data: { result: returnValue } });
+  });
+
+  it('emits the input event when the value changes', async () => {
+    wrapper = mountComponent();
+    await flushPromises();
+    const input = wrapper.findComponent('[name="testName"]');
+
+    await input.vm.$emit('input', ['app1']);
+    await flushPromises();
+
+    expect(wrapper.emitted('input')).toBeTruthy();
+    expect(wrapper.emitted('input')[0][0]).toEqual(['app1']);
+  });
+
+  it('fetches initial values when props.value is not empty', async () => {
+    wrapper = mountComponent({ value: ['app3'] });
+    await flushPromises();
+
+    const options = wrapper.vm.selectOptions;
+    expect(options).toEqual([
+      { text: 'App 3', value: 'app3' },
+      { text: 'App 1', value: 'app1' },
+      { text: 'App 2', value: 'app2' },
+    ]);
+  });
+});
---
+++ packages/platform-shared/src/components/governance/GovResourceMultiselect.vue
@@ -0,0 +1,149 @@
+<!-- Copyright (c) 2023-2024 ForgeRock. All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
+<template>
+  <div
+    v-if="initialValuesLoad && optionsLoad"
+    style="height: 50px;">
+    <FrField
+      @input="handleInput"
+      @search-change="debouncedSearch"
+      open-direction="bottom"
+      type="multiselect"
+      :description="description"
+      :internal-search="false"
+      :label="label"
+      :name="name"
+      :options="selectOptions"
+      :value="selectValue">
+      <template #noResult>
+        <slot name="noResult">
+          {{ $t('common.noResultsFound') }}
+        </slot>
+      </template>
+    </FrField>
+  </div>
+</template>
+
+<script setup>
+import {
+  computed,
+  ref,
+} from 'vue';
+import {
+  debounce,
+} from 'lodash';
+import { getResource } from '@forgerock/platform-shared/src/api/governance/CommonsApi';
+import FrField from '@forgerock/platform-shared/src/components/Field';
+
+const emit = defineEmits(['input']);
+
+const props = defineProps({
+  description: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Array,
+    default: () => [],
+  },
+  resource: {
+    type: String,
+    default: 'application',
+  },
+});
+
+// data
+const initialValues = ref([]);
+const initialValuesLoad = ref(false);
+const options = ref([]);
+const optionsLoad = ref(false);
+const selectValue = ref([...props.value]);
+
+// computed
+const selectOptions = computed(() => [...initialValues.value, ...options.value]);
+
+/**
+ * Parses the query string and returns an object containing the query parameters.
+ *
+ * @param {string} queryString - The query string to parse.
+ * @returns {Object} - An object containing the query parameters as key-value pairs.
+ */
+function getQueryParams(queryString) {
+  const queryParams = { queryString };
+  if (props.resource === 'application') {
+    queryParams.authoritative = false;
+  }
+  return queryParams;
+}
+
+/**
+ * Retrieves the initial values for the application picker based on the provided resourceIds.
+ * @param {Array} resourceIds - An array of application IDs.
+ */
+async function getInitialValues(resourceIds) {
+  const initialPromises = resourceIds.map((value) => getResource(props.resource, getQueryParams(value)));
+  try {
+    const initialData = await Promise.all(initialPromises);
+    initialValues.value = initialData.map((resourcePromise) => ({
+      text: resourcePromise.data?.result?.[0]?.name,
+      value: resourcePromise.data?.result?.[0]?.id,
+    })) || [];
+  } finally {
+    initialValuesLoad.value = true;
+  }
+}
+
+/**
+ * Retrieves a list of resources based on the provided query string.
+ * If the initial load has not occurred and there are existing values, it attempts to get the initial values.
+ * Then, it retrieves the resource data for applications and maps it to options with text and value properties.
+ *
+ * @param {String} queryString - The query string to filter the resource list.
+ */
+async function getResourceList(queryString) {
+  if (!optionsLoad.value && props.value.length) {
+    getInitialValues(props.value);
+  } else {
+    initialValuesLoad.value = true;
+  }
+  try {
+    const { data } = await getResource(props.resource, getQueryParams(queryString));
+    options.value = data.result?.map((resource) => ({
+      text: resource.name,
+      value: resource.id,
+    })) || [];
+  } finally {
+    optionsLoad.value = true;
+  }
+}
+
+/**
+ * Handles the input event.
+ * @param {Array} event - The input event object.
+ */
+function handleInput(event) {
+  selectValue.value = event;
+  emit('input', event);
+}
+
+/**
+ * Performs a search for a resource list based on the provided query.
+ * @param {String} query - The search query.
+ */
+function search(query) {
+  getResourceList(query);
+}
+const debouncedSearch = debounce(search, 500);
+
+getResourceList();
+</script>
