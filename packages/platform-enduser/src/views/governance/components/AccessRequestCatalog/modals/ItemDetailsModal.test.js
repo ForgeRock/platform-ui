6f10c23aa2cef6b4f115775a6264e497f79a384f
Michael Wong <michael.wong@forgerock.com>
2024-07-05 13:23:49 -0700

    IAM-6881 access request - grant details modal now converts managed object glossary values from ids to user friendly values

--- packages/platform-enduser/src/views/governance/components/AccessRequestCatalog/modals/ItemDetailsModal.test.js
+++ packages/platform-enduser/src/views/governance/components/AccessRequestCatalog/modals/ItemDetailsModal.test.js
@@ -5,8 +5,9 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { mount } from '@vue/test-utils';
+import { flushPromises, mount } from '@vue/test-utils';
 import { findByTestId } from '@forgerock/platform-shared/src/utils/testHelpers';
+import * as CommonsApi from '@forgerock/platform-shared/src/api/governance/CommonsApi';
 import { BModal, BButton } from 'bootstrap-vue';
 import ItemDetailsModal from './ItemDetailsModal';
 import i18n from '@/i18n';
@@ -39,7 +40,7 @@ describe('ItemDetailsModal', () => {
       },
     },
     itemType: 'role',
-    visible: true,
+    isTesting: true,
   };
 
   function setup(props) {
@@ -59,7 +60,7 @@ describe('ItemDetailsModal', () => {
 
   it('renders item details correctly', async () => {
     wrapper = setup();
-    await wrapper.vm.$nextTick();
+    await flushPromises();
 
     // Assert modal title
     const title = findByTestId(wrapper, 'modal-title');
@@ -72,24 +73,199 @@ describe('ItemDetailsModal', () => {
     // Assert glossary fields
     const { glossarySchema, item } = propsData;
 
-    expect(findByTestId(wrapper, `glossary-title${glossarySchema[0].id}`).text()).toBe(glossarySchema[0].displayName);
-    expect(findByTestId(wrapper, `glossary-title${glossarySchema[1].id}`).text()).toBe(glossarySchema[1].displayName);
-    expect(findByTestId(wrapper, `glossary-item${glossarySchema[0].id}`).text()).toBe('--');
-    expect(findByTestId(wrapper, `glossary-item${glossarySchema[1].id}`).text()).toBe(item.glossary[glossarySchema[1].name]);
+    expect(findByTestId(wrapper, `glossary-title-${glossarySchema[0].name}`).text()).toBe(glossarySchema[0].displayName);
+    expect(findByTestId(wrapper, `glossary-title-${glossarySchema[1].name}`).text()).toBe(glossarySchema[1].displayName);
+    expect(findByTestId(wrapper, `glossary-item-${glossarySchema[0].name}`).text()).toBe(item.glossary[glossarySchema[0].name]);
+    expect(findByTestId(wrapper, `glossary-item-${glossarySchema[1].name}`).text()).toBe(item.glossary[glossarySchema[1].name]);
   });
 
   it('emits the correct event on OK button click', async () => {
     wrapper = setup();
-    await wrapper.vm.$nextTick();
-    await wrapper.setProps({ visible: true });
+    await flushPromises();
 
     // Trigger OK button click
     await wrapper.find('.modal-footer button.btn-primary').trigger('click');
-    await wrapper.vm.$nextTick();
+    await flushPromises();
 
     // Assert that the correct event is emitted
     expect(wrapper.emitted()).toHaveProperty('toggle-item');
     expect(wrapper.emitted()['toggle-item']).toHaveLength(1);
     expect(wrapper.emitted()['toggle-item'][0]).toEqual([propsData.item]);
+  });
+
+  describe('glossary managed objects', () => {
+    CommonsApi.getResource = jest.fn().mockImplementation((resource, queryParams) => {
+      const result = resource === 'user'
+        ? { givenName: `resolved-${resource}-${queryParams.queryString}` }
+        : { name: `resolved-${resource}-${queryParams.queryString}` };
+      return Promise.resolve({ data: { result: [result] } });
+    });
+
+    it('displays blank values for glossary items with no value', async () => {
+      const props = {
+        item: {
+          ...propsData.item,
+          glossary: { roleOwner: '' },
+        },
+      };
+      wrapper = setup(props);
+      await flushPromises();
+
+      const { glossarySchema } = propsData;
+      expect(findByTestId(wrapper, `glossary-item-${glossarySchema[0].name}`).text()).toBe('--');
+    });
+
+    it('displays user name for managed users', async () => {
+      const props = {
+        item: {
+          ...propsData.item,
+          glossary: {
+            singleUser: 'managed/user/id1',
+          },
+        },
+        glossarySchema: [
+          {
+            displayName: 'Single User',
+            name: 'singleUser',
+            type: 'managedObject',
+            managedObjectType: 'openidm/managed/user',
+          },
+        ],
+      };
+      wrapper = setup(props);
+      await flushPromises();
+
+      expect(findByTestId(wrapper, 'glossary-item-singleUser').text()).toBe('resolved-user-id1');
+    });
+
+    it('displays user names for multiple managed users', async () => {
+      const props = {
+        item: {
+          ...propsData.item,
+          glossary: {
+            multipleUsers: [
+              'managed/user/id1',
+              'managed/user/id2',
+              'managed/user/id3',
+            ],
+          },
+        },
+        glossarySchema: [
+          {
+            displayName: 'Multiple Users',
+            name: 'multipleUsers',
+            type: 'managedObject',
+            managedObjectType: 'openidm/managed/user',
+            isMultiValue: true,
+          },
+        ],
+      };
+      wrapper = setup(props);
+      await flushPromises();
+
+      expect(findByTestId(wrapper, 'glossary-item-multipleUsers').text()).toBe('resolved-user-id1 , resolved-user-id2 , resolved-user-id3');
+    });
+
+    it('displays role name for managed roles', async () => {
+      const props = {
+        item: {
+          ...propsData.item,
+          glossary: {
+            singleRole: 'managed/user/role1',
+          },
+        },
+        glossarySchema: [
+          {
+            displayName: 'Single Role',
+            name: 'singleRole',
+            type: 'managedObject',
+            managedObjectType: 'openidm/managed/role',
+          },
+        ],
+      };
+      wrapper = setup(props);
+      await flushPromises();
+
+      expect(findByTestId(wrapper, 'glossary-item-singleRole').text()).toBe('resolved-role-role1');
+    });
+
+    it('displays role names for multiple managed roles', async () => {
+      const props = {
+        item: {
+          ...propsData.item,
+          glossary: {
+            multipleRoles: [
+              'managed/user/role1',
+              'managed/user/role2',
+              'managed/user/role3',
+            ],
+          },
+        },
+        glossarySchema: [
+          {
+            displayName: 'Multiple Roles',
+            name: 'multipleRoles',
+            type: 'managedObject',
+            managedObjectType: 'openidm/managed/role',
+            isMultiValue: true,
+          },
+        ],
+      };
+      wrapper = setup(props);
+      await flushPromises();
+
+      expect(findByTestId(wrapper, 'glossary-item-multipleRoles').text()).toBe('resolved-role-role1, resolved-role-role2, resolved-role-role3');
+    });
+
+    it('displays org name for managed orgs', async () => {
+      const props = {
+        item: {
+          ...propsData.item,
+          glossary: {
+            singleOrg: 'managed/user/org1',
+          },
+        },
+        glossarySchema: [
+          {
+            displayName: 'Single Org',
+            name: 'singleOrg',
+            type: 'managedObject',
+            managedObjectType: 'openidm/managed/organization',
+          },
+        ],
+      };
+      wrapper = setup(props);
+      await flushPromises();
+
+      expect(findByTestId(wrapper, 'glossary-item-singleOrg').text()).toBe('resolved-organization-org1');
+    });
+
+    it('displays org names for multiple managed orgs', async () => {
+      const props = {
+        item: {
+          ...propsData.item,
+          glossary: {
+            multipleOrgs: [
+              'managed/user/org1',
+              'managed/user/org2',
+              'managed/user/org3',
+            ],
+          },
+        },
+        glossarySchema: [
+          {
+            displayName: 'Multiple Users',
+            name: 'multipleOrgs',
+            type: 'managedObject',
+            managedObjectType: 'openidm/managed/organization',
+            isMultiValue: true,
+          },
+        ],
+      };
+      wrapper = setup(props);
+      await flushPromises();
+
+      expect(findByTestId(wrapper, 'glossary-item-multipleOrgs').text()).toBe('resolved-organization-org1, resolved-organization-org2, resolved-organization-org3');
+    });
   });
 });
--- packages/platform-enduser/src/views/governance/components/AccessRequestCatalog/modals/ItemDetailsModal.vue
+++ packages/platform-enduser/src/views/governance/components/AccessRequestCatalog/modals/ItemDetailsModal.vue
@@ -7,8 +7,8 @@ of the MIT license. See the LICENSE file for details. -->
     id="accessRequestItemModal"
     size="lg"
     cancel-variant="link"
-    :visible="visible"
-    :static="visible"
+    :visible="isTesting"
+    :static="isTesting"
     @hidden="$emit('modal-closed')"
     @ok="$emit('toggle-item', item)">
     <!-- Modal Header -->
@@ -35,14 +35,12 @@ of the MIT license. See the LICENSE file for details. -->
         <BMediaBody>
           <small
             class="text-muted"
-            data-testid="modal-title"
-          >
+            data-testid="modal-title">
             {{ modalProps.modalTitle }}
           </small>
           <h2
             class="m-0 h5 modal-title"
-            data-testid="item-name"
-          >
+            data-testid="item-name">
             {{ item.name }}
           </h2>
         </BMediaBody>
@@ -58,44 +56,46 @@ of the MIT license. See the LICENSE file for details. -->
     </template>
 
     <!-- Modal Body -->
-    <!-- Description field (for all types) -->
-    <BRow>
-      <BCol>
-        <small class="mb-1">{{ $t('common.description') }}</small>
-        <p
-          class="mb-4"
-          data-testid="description"
-        >
-          {{ item.description || blankValueIndicator }}
-        </p>
-      </BCol>
-    </BRow>
+    <FrSpinner
+      v-if="!isLoaded"
+      class="my-3" />
+    <template v-else>
+      <!-- Description field (for all types) -->
+      <BRow>
+        <BCol>
+          <small class="mb-1 h5">{{ $t('common.description') }}</small>
+          <p
+            class="mb-4"
+            data-testid="description">
+            {{ item.description || blankValueIndicator }}
+          </p>
+        </BCol>
+      </BRow>
 
-    <!-- Dynamic glossary fields -->
-    <BRow data-testid="glossary-fields">
-      <BCol
-        md="6"
-        class="mb-4"
-        v-for="(glossary, key) in glossarySchema"
-        :key="key">
-        <small
-          class="mb-1"
-          :data-testid="`glossary-title${glossary.id}`"
-        >
-          {{ glossary.displayName }}
-        </small>
-        <p :data-testid="`glossary-item${glossary.id}`">
-          {{ translateGlossaryAttribute(glossary.name) }}
-        </p>
-      </BCol>
-    </BRow>
+      <!-- Dynamic glossary fields -->
+      <BRow data-testid="glossary-fields">
+        <BCol
+          md="6"
+          class="mb-4"
+          v-for="(glossary, key) in glossaryValues"
+          :key="key">
+          <small
+            class="mb-1 h5"
+            :data-testid="`glossary-title-${key}`">
+            {{ glossary.displayName }}
+          </small>
+          <p :data-testid="`glossary-item-${key}`">
+            {{ glossary.value }}
+          </p>
+        </BCol>
+      </BRow>
+    </template>
 
     <!-- Modal Footer -->
     <template #modal-footer="{ ok, cancel }">
       <div
         class="d-flex w-100"
-        :class="modalProps.footerClass"
-      >
+        :class="modalProps.footerClass">
         <BButton
           :variant="modalProps.okVariant"
           @click="ok()">
@@ -121,9 +121,7 @@ of the MIT license. See the LICENSE file for details. -->
  */
 import {
   capitalize,
-  get,
-  includes,
-  split,
+  isEmpty,
 } from 'lodash';
 import {
   BButton,
@@ -135,10 +133,11 @@ import {
   BModal,
   BRow,
 } from 'bootstrap-vue';
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { blankValueIndicator } from '@forgerock/platform-shared/src/utils/governance/constants';
 import { getResource } from '@forgerock/platform-shared/src/api/governance/CommonsApi';
 import FrIcon from '@forgerock/platform-shared/src/components/Icon';
+import FrSpinner from '@forgerock/platform-shared/src/components/Spinner/';
 import i18n from '@/i18n';
 
 const props = defineProps({
@@ -154,7 +153,7 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  visible: {
+  isTesting: {
     type: Boolean,
     default: false,
   },
@@ -163,7 +162,7 @@ const props = defineProps({
 defineEmits(['toggle-item', 'modal-closed']);
 
 // Data
-const ownerName = ref(blankValueIndicator);
+const glossaryValues = ref({});
 
 // Computed
 const modalProps = computed(() => {
@@ -187,42 +186,108 @@ const modalProps = computed(() => {
     okVariant,
   };
 });
+const isLoaded = computed(() => {
+  let allFieldsLoaded = true;
+  Object.values(glossaryValues.value).forEach((value) => {
+    if (!value.isLoaded) allFieldsLoaded = false;
+  });
+  return allFieldsLoaded;
+});
 
 /**
- * Get and parse user full name by ID
- * @param {String} rawId user id in the format returned by the backend
+ * Retrieves the display data for a specific resource.
+ *
+ * @param {string} resourceType - The type of the resource. application, role, user, org
+ * @param {string} id - The ID of the resource.
+ * @returns {Promise} - A promise that resolves to the display data of the resource.
  */
-function getUserFullName(rawId) {
-  if (!rawId) {
-    ownerName.value = blankValueIndicator;
-    return;
+async function getResourceDisplayData(resourceType, id) {
+  try {
+    const { data } = await getResource(resourceType, { queryString: id });
+    if (resourceType === 'user') {
+      const user = data.result[0];
+      return Promise.resolve(i18n.global.t('common.userFullName', {
+        givenName: user.givenName,
+        sn: user.sn,
+      }));
+    }
+    return Promise.resolve(data.result[0].name);
+  } catch {
+    return Promise.resolve(id);
   }
-  const id = split(rawId, '/')[2];
-  getResource('user', { queryString: id })
-    .then(({ data }) => {
-      const userData = get(data, 'result[0]', {});
-      ownerName.value = i18n.global.t('common.userFullName', {
-        givenName: userData?.givenName,
-        sn: userData?.sn,
-      });
-    })
-    .catch(() => {
-      ownerName.value = blankValueIndicator;
-    });
 }
 
 /**
- * Gets the value of the glossary attribute according to the name
- * @param {String} glossaryName glossary attribute name
+ * Sets the value of a glossary property.
+ *
+ * @param {string} propertyName - The name of the glossary property.
+ * @param {any} value - The value to set for the glossary property.
  */
-function translateGlossaryAttribute(glossaryName) {
-  // If it is not an 'owner' attribute it is translated directly from the value in the item.
-  if (!includes(glossaryName, 'Owner')) {
-    return props.item.glossary?.[glossaryName] || blankValueIndicator;
-  }
-  // If it is an owner tribute we search for the user's name
-  const rawId = props.item.glossary?.[`${props.itemType}Owner`];
-  getUserFullName(rawId);
-  return ownerName.value;
+function setGlossaryValue(propertyName, value) {
+  glossaryValues.value[propertyName].value = value;
+  glossaryValues.value[propertyName].isLoaded = true;
 }
+
+/**
+ * Sets the glossary values based on the provided schema and values.
+ *
+ * @param {Object} glossarySchema - The schema object defining the glossary.
+ * @param {Object} values - The values to be set in the glossary.
+ */
+async function setGlossaryValues(glossarySchema, values) {
+  glossarySchema.forEach(async (glossaryItem) => {
+    const propertyName = glossaryItem.name;
+    // no value for glossary property
+    if (!values[propertyName]) {
+      setGlossaryValue(propertyName, blankValueIndicator);
+      return;
+    }
+    // look up value for managed object types
+    if (glossaryItem.type === 'managedObject') {
+      let ids = glossaryItem.isMultiValue ? values[propertyName] : [values[propertyName]];
+      ids = ids.map((id) => id.split('/').pop());
+      const resourceType = glossaryItem.managedObjectType.split('/').pop();
+      const value = await Promise.all(ids.map((id) => getResourceDisplayData(resourceType, id)));
+      setGlossaryValue(propertyName, value.join(', '));
+    } else {
+      // set value for non-managed object types
+      setGlossaryValue(propertyName, values[propertyName] || blankValueIndicator);
+    }
+  });
+}
+
+/**
+ * Retrieves the base glossary array based on the provided glossary schema.
+ *
+ * @param {Object} glossarySchema - The glossary schema object.
+ * @returns {Array} - The base glossary array.
+ */
+function getBaseGlossaryArray(glossarySchema) {
+  const glossary = {};
+  glossarySchema.forEach((glossaryItem) => {
+    glossary[glossaryItem.name] = {
+      displayName: glossaryItem.displayName,
+      isLoaded: false,
+    };
+  });
+  return glossary;
+}
+
+/**
+ * Sets the glossary schema and values for the ItemDetailsModal component.
+ *
+ * @param {Object} glossarySchema - The glossary schema object.
+ * @param {Array} values - The values array.
+ */
+function setGlossarySchemaAndValues(glossarySchema, values) {
+  if (!glossarySchema?.length) return;
+  glossaryValues.value = getBaseGlossaryArray(glossarySchema);
+  setGlossaryValues(glossarySchema, values);
+}
+
+watch(() => props.item, () => {
+  if (!isEmpty(props.item)) {
+    setGlossarySchemaAndValues(props.glossarySchema, props.item.glossary);
+  }
+}, { deep: true, immediate: true });
 </script>
