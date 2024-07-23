1a03eabb8a77ae0aa02ac542ca43d558c2857b26
Josh Newton <josh.newton@forgerock.com>
2024-07-23 11:12:48 +0100

    IAM-6966 - Remove margin-left class causing text in suspendedTextOutputCallbacks to be off center

--- packages/platform-login/src/components/callbacks/SuspendedTextOutputCallback/index.vue
+++ packages/platform-login/src/components/callbacks/SuspendedTextOutputCallback/index.vue
@@ -1,10 +1,10 @@
-<!-- Copyright (c) 2020-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2020-2024 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <div
-    class="row ml-1 mb-2"
+    class="row mb-2"
     ref="textOutputPanel">
     <div
       data-testid="suspend-text-output"
