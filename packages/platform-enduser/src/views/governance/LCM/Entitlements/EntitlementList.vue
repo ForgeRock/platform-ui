07d5b8f1a5f16a32b7ff6103c44116a82d93c5b1
Michael Wong <michael.wong@forgerock.com>
2025-03-13 13:38:10 -0700

    IAM-8393 scopes - view permissions always show as checked and disabled

--- packages/platform-enduser/src/views/governance/LCM/Entitlements/EntitlementList.vue
+++ packages/platform-enduser/src/views/governance/LCM/Entitlements/EntitlementList.vue
@@ -3,7 +3,7 @@
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
-  <BContainer class="my-5 p-0">
+  <BContainer class="my-5">
     <FrHeader
       class="mb-4"
       :title="$t('governance.administer.entitlements.title')"
