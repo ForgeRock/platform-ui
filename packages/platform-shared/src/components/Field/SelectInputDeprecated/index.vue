bef7aa50d8665d43f4b219f91b927c470119f6bf
Andrew Hertel <andrew.hertel@forgerock.com>
2025-03-24 16:24:40 +0100

    IAM-2103 Add enum support to login journeys

--- packages/platform-shared/src/components/Field/SelectInputDeprecated/index.vue
+++ packages/platform-shared/src/components/Field/SelectInputDeprecated/index.vue
@@ -74,6 +74,7 @@ import {
   find,
 } from 'lodash';
 import vueMultiSelectOverrides from '@forgerock/platform-shared/src/composables/vueMultiSelectOverrides';
+import { getTranslation } from '@forgerock/platform-shared/src/utils/translations';
 
 import { useField } from 'vee-validate';
 import uuid from 'uuid/v4';
@@ -215,7 +216,7 @@ export default {
         formattedOptions = this.options.map((option) => {
           const formattedOption = typeof (option) === 'string' ? option.trim() : option;
           return {
-            text: formattedOption,
+            text: getTranslation(formattedOption),
             value: formattedOption,
           };
         });
