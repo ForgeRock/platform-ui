c940406efcb467a535d7452fccc88b6535f2d521
Michael Wong <michael.wong@forgerock.com>
2025-03-28 17:08:19 -0700

    IAM-8479 user lcm - add ability to create a user lcm form

--- packages/platform-shared/src/api/governance/RequestFormAssignmentsApi.test.js
+++ packages/platform-shared/src/api/governance/RequestFormAssignmentsApi.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024 ForgeRock. All rights reserved.
+ * Copyright (c) 2024-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -67,11 +67,27 @@ describe('RequestFormAssignments Api', () => {
     await RequestFormAssignmentsApi.getApplicationRequestFormAssignment(appId, objectType);
     expect(getMock).toBeCalledWith(`${formAssignmentUrl}?_queryFilter=objectId eq "application/${appId}/${objectType}/create"`);
   });
-});
 
-it('getFormApplications should call api with the correct query', async () => {
-  const formId = '123';
+  it('getFormApplications should call api with the correct query', async () => {
+    const formId = '123';
 
-  await RequestFormAssignmentsApi.getFormApplications(formId);
-  expect(getMock).toBeCalledWith(`${formAssignmentUrl}?_queryFilter=objectId co "application/" and formId eq "${formId}"`);
+    await RequestFormAssignmentsApi.getFormApplications(formId);
+    expect(getMock).toBeCalledWith(`${formAssignmentUrl}?_queryFilter=objectId co "application/" and formId eq "${formId}"`);
+  });
+
+  it('getFormAssignmentByLcmTypeAndOperation should call api with the correct query', async () => {
+    const lcmType = 'type1';
+    const operation = 'operation1';
+
+    await RequestFormAssignmentsApi.getFormAssignmentByLcmTypeAndOperation(lcmType, operation);
+    expect(getMock).toBeCalledWith(`${formAssignmentUrl}?_queryFilter=objectId eq "lcm/${lcmType}/${operation}"`);
+  });
+
+  it('getFormLcmType should call api with the correct query', async () => {
+    const formId = '123';
+    const lcmType = 'type1';
+
+    await RequestFormAssignmentsApi.getFormLcmType(formId, lcmType);
+    expect(getMock).toBeCalledWith(`${formAssignmentUrl}?_queryFilter=objectId co "lcm/${lcmType}/" and formId eq "${formId}"`);
+  });
 });
