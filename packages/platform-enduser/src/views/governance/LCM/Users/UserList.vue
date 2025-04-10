8eb497e1d541ee005c97e9c7de70f1e7ac69f685
Michael Wong <michael.wong@forgerock.com>
2025-04-10 08:31:37 -0700

    IAM-8352 - user lcm show list view columns only if user has permissions to view those fields. Add status column

--- packages/platform-enduser/src/views/governance/LCM/Users/UserList.vue
+++ packages/platform-enduser/src/views/governance/LCM/Users/UserList.vue
@@ -9,9 +9,11 @@ of the MIT license. See the LICENSE file for details. -->
       :title="$t('governance.administer.users.title')"
       :subtitle="$t('governance.administer.users.subtitle')" />
     <FrGovResourceList
+      v-if="userColumns.length && queryFields.length"
       class="mb-5"
       resource="user"
       :columns="userColumns"
+      :query-fields="queryFields"
       :resource-function="getManagedResourceList">
       <template #cell(name)="{ item }">
         <FrUserBasicInfo
@@ -20,6 +22,13 @@ of the MIT license. See the LICENSE file for details. -->
       </template>
       <template #cell(email)="{ item }">
         {{ item.mail }}
+      </template>
+      <template #cell(status)="{ item }">
+        <BBadge
+          class="w-100px"
+          :variant="item.accountStatus === 'active' ? 'success' : 'light'">
+          {{ $t(item.accountStatus === 'active' ? 'common.active' : 'common.inactive') }}
+        </BBadge>
       </template>
     </FrGovResourceList>
   </BContainer>
@@ -31,37 +40,53 @@ of the MIT license. See the LICENSE file for details. -->
  * Supports searching and pagination
  */
 // import { useRouter } from 'vue-router';
+import { computed, ref } from 'vue';
 import {
+  BBadge,
   BContainer,
 } from 'bootstrap-vue';
 import FrGovResourceList from '@forgerock/platform-shared/src/components/governance/GovResourceList';
 import FrHeader from '@forgerock/platform-shared/src/components/PageHeader';
 import FrUserBasicInfo from '@forgerock/platform-shared/src/components/UserGroupList/UserBasicInfo';
 import { getManagedResourceList } from '@forgerock/platform-shared/src/api/ManagedResourceApi';
+import { getResourceTypePrivilege } from '@forgerock/platform-shared/src/api/PrivilegeApi';
+import { showErrorMessage } from '@forgerock/platform-shared/src/utils/notification';
 import i18n from '@/i18n';
 
 // composables
 // const router = useRouter();
 
 // data
-const userColumns = [
+const userViewPrivileges = ref([]);
+const availableColumns = [
   {
-    key: 'name',
-    label: i18n.global.t('common.name'),
-    initialSort: true,
-    sortable: true,
-    sortKey: 'givenName',
+    column: {
+      key: 'name',
+      label: i18n.global.t('common.name'),
+      initialSort: true,
+      sortable: true,
+      sortKey: 'givenName',
+    },
+    permissions: ['userName', 'givenName', 'sn'],
   },
   {
-    key: 'email',
-    label: i18n.global.t('common.email'),
-    sortable: true,
-    sortKey: 'mail',
+    column: {
+      key: 'email',
+      label: i18n.global.t('common.email'),
+      sortable: true,
+      sortKey: 'mail',
+    },
+    permissions: ['mail'],
   },
   {
-    key: 'actions',
-    label: '',
-    class: 'w-70px',
+    column: {
+      key: 'status',
+      label: i18n.global.t('common.status'),
+      class: 'w-250px',
+      sortable: true,
+      sortKey: 'accountStatus',
+    },
+    permissions: ['accountStatus'],
   },
 ];
 
@@ -76,4 +101,70 @@ const userColumns = [
 //   });
 // }
 
+/**
+ * Checks if the current user has the required privilege for a specific field.
+ *
+ * @param {string} field - The name of the field to check privileges for.
+ * @returns {boolean} - Returns true if the user has the privilege, otherwise false.
+ */
+function hasPrivilege(field) {
+  return userViewPrivileges.value.indexOf(field) > -1;
+}
+
+const columnsWithPermissions = computed(() => availableColumns.map((column) => {
+  if (column.permissions.every((permission) => hasPrivilege(permission))) {
+    return column;
+  }
+  return null;
+}).filter((column) => column !== null));
+
+const userColumns = computed(() => {
+  const columns = columnsWithPermissions.value.map((column) => column.column);
+
+  columns.push({
+    key: 'actions',
+    label: '',
+    class: 'w-70px',
+  });
+
+  return columns;
+});
+
+const queryFields = computed(() => {
+  const fields = [];
+
+  columnsWithPermissions.value.forEach((column) => {
+    column.permissions.forEach((permission) => {
+      fields.push(permission);
+    });
+  });
+
+  return fields;
+});
+
+/**
+ * Fetches the list of privileges for the user.
+ * This function is asynchronous and retrieves data related to user permissions.
+ */
+async function getPrivileges() {
+  try {
+    const { data } = await getResourceTypePrivilege('managed/alpha_user');
+    userViewPrivileges.value = data.VIEW?.allowed
+      ? data.VIEW.properties || []
+      : [];
+  } catch (error) {
+    showErrorMessage(error, i18n.global.t('governance.user.errorGettingPrivileges'));
+  }
+}
+
+getPrivileges();
+
 </script>
+
+<style lang="scss" scoped>
+:deep {
+  .w-70px {
+    width: 70px;
+  }
+}
+</style>
