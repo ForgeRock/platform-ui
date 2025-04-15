89ab081615e71ceb1bb918351c4decbf53bcadb7
Michael Wong <michael.wong@forgerock.com>
2025-04-15 16:03:04 -0700

    IAM-8531 entitlement lcm - entitlement detail view shows application name, object type, and account attribute. separated detail form into two sections, one for entitlement attributes and one for glossary attributes

--- packages/platform-enduser/src/views/governance/LCM/Entitlements/Edit/Tabs/Details.vue
+++ packages/platform-enduser/src/views/governance/LCM/Entitlements/Edit/Tabs/Details.vue
@@ -4,10 +4,8 @@ This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <BCard>
-    <h3 class="h5 mb-4">
-      {{ $t('governance.administer.entitlements.generalSettings') }}
-    </h3>
     <FrDefaultEntitlementForm
+      show-details
       :application-id="applicationId"
       :object-type="objectType"
       :entitlement="entitlement"
