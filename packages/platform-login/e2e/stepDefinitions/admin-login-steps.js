446bdec810b9423ebc164892ba48e309319584df
Sergio Martínez <sergiomartinez@pingidentity.com>
2025-06-12 16:41:50 -0500

    IAM-8723 - Update admin login tests

--- packages/platform-login/e2e/stepDefinitions/admin-login-steps.js
+++ packages/platform-login/e2e/stepDefinitions/admin-login-steps.js
@@ -5,25 +5,18 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { When } from '@badeball/cypress-cucumber-preprocessor';
+import { deleteIDMUser } from '@e2e/api/managedApi.e2e';
 
-/**
- * Logs in the user with valid or invalid credentials.
- * @param {string} scenario - The type of credentials to use ('valid' or 'invalid').
- */
-When('admin logs in with {string} credentials', (scenario) => {
-  switch (scenario) {
-    case 'invalid':
-      cy.findByLabelText(/User Name/i, { timeout: 20000 }).type('invalidUsername');
-      cy.findAllByLabelText(/Password/i).first().type('invalidPassword');
-      cy.findByRole('button', { name: 'Next' }).click();
-      break;
-    case 'valid':
-      cy.loginAsAdmin();
-      break;
-    default:
-      // eslint-disable-next-line no-console
-      console.error('Invalid step');
-      break;
+after(() => {
+  if (Cypress.spec.relative.includes('admin-login.feature') && Cypress.env('endUserId')) {
+    cy.loginAsAdmin();
+    cy.log(`Deleting created IDM end user ${Cypress.env('endUserName')} via API`).then(() => {
+      deleteIDMUser(Cypress.env('endUserId'));
+      Cypress.env('endUserName', '');
+      Cypress.env('endUserFirstName', '');
+      Cypress.env('endUserLastName', '');
+      Cypress.env('endUserPassword', '');
+      Cypress.env('endUserId', '');
+    });
   }
 });
