c8169c55f271fe183c13ec7275c0cb9735909424
Huck Elliott <huck.elliott@forgerock.com>
2025-09-15 13:32:17 -0700

    IAM-9323 adds Metadata tab to user resource page in platform-admin ui representing userMeta relationship's createDate, lastChanged.date, and loginCount properties

---
+++ packages/platform-shared/src/components/resource/EditResource/CustomTabs/MetadataTab/index.vue
@@ -0,0 +1,46 @@
+<!-- Copyright (c) 2025 ForgeRock. All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
+<template>
+  <BTab :title="$t('common.metadata')">
+    <FrField
+      class="p-3"
+      name="createDateField"
+      type="datetime"
+      :label="$t('common.createDate')"
+      :adjust-for-timezone="false"
+      :disabled="true"
+      :show-seconds="false"
+      :value="meta.createDate" />
+    <FrField
+      class="p-3"
+      name="lastChangedField"
+      type="datetime"
+      :label="$t('common.lastChangedDate')"
+      :adjust-for-timezone="false"
+      :disabled="true"
+      :show-seconds="false"
+      :value="meta.lastChanged?.date" />
+    <FrField
+      class="p-3"
+      :label="$t('common.loginCount')"
+      :disabled="true"
+      name="loginCountField"
+      :value="meta.loginCount"
+      type="number" />
+  </BTab>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { BTab } from 'bootstrap-vue';
+import FrField from '@forgerock/platform-shared/src/components/Field';
+
+defineProps({
+  meta: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
