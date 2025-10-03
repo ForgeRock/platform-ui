49d7207857331aabb0e54eed4055b10a1b069fc2
Pablo Torterolo <pablotorterolo@pingidentity.com>
2025-10-03 15:58:51 -0300

    fix: Improve semantics and accessibility in CountCard component

--- packages/platform-shared/src/components/CountCard/CountCard.test.js
+++ packages/platform-shared/src/components/CountCard/CountCard.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -33,7 +33,7 @@ describe('CountCard Component', () => {
     });
 
     expect(wrapper.find('.fr-spinner').exists()).toBe(true);
-    expect(wrapper.find('h1').exists()).toBe(false);
+    expect(wrapper.find('p').exists()).toBe(false);
   });
 
   it('CountCard to hide loader and show correct count when loader prop set to false', () => {
@@ -52,9 +52,9 @@ describe('CountCard Component', () => {
     });
 
     expect(wrapper.find('.fr-spinner').exists()).toBe(false);
-    expect(wrapper.find('h1').exists()).toBe(true);
+    expect(wrapper.find('p').exists()).toBe(true);
 
-    const h1 = findByTestId(wrapper, 'counter-unit_test_count');
-    expect(h1.text()).toBe('100');
+    const p = findByTestId(wrapper, 'counter-unit_test_count');
+    expect(p.text()).toBe('100');
   });
 });
