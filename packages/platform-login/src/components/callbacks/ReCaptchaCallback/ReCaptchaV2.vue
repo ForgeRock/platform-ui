41814817c38eac0b5fd83a55eacfc158313488c7
sam gostling <sam.gostling@pingidentity.com>
2025-10-15 16:10:50 +0100

    OPENAM-24401 Add expired CAPTCHA handling to reCaptcha component

--- packages/platform-login/src/components/callbacks/ReCaptchaCallback/ReCaptchaV2.vue
+++ packages/platform-login/src/components/callbacks/ReCaptchaCallback/ReCaptchaV2.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2022-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2022-2025 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -19,7 +19,7 @@ of the MIT license. See the LICENSE file for details. -->
             ref="tmpRecaptcha"
             v-if="recaptchaSiteKey"
             @verify="handleCaptchaCallback"
-            @expired="handleCaptchaError"
+            @expired="handleCaptchaExpired"
             @error="handleCaptchaError"
             :sitekey="recaptchaSiteKey" />
         </div>
@@ -79,6 +79,11 @@ export default {
         recaptchaScript.setAttribute('defer', '');
         document.head.appendChild(recaptchaScript);
       }
+    },
+    handleCaptchaExpired() {
+      this.value = '';
+      this.$emit('disable-next-button', true);
+      this.showErrorMessage('error', this.$t('pages.selfservice.captchaError'));
     },
     handleCaptchaError() {
       this.showErrorMessage('error', this.$t('pages.selfservice.captchaError'));
