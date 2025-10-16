518fa99fab9565c0764de181584254217cc30e6a
Chuck Masucci <chuck.masucci@pingidentity.com>
2025-10-16 13:41:27 -0700

    IAM-9302 Added new Filter Builder complex logic to disable Basic editor

--- packages/platform-shared/src/components/filterBuilder/QueryFilterBuilder/QueryFilterBuilder.test.js
+++ packages/platform-shared/src/components/filterBuilder/QueryFilterBuilder/QueryFilterBuilder.test.js
@@ -1,213 +1,200 @@
 /**
- * Copyright (c) 2022-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2022-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount, flushPromises } from '@vue/test-utils';
 import QueryFilterBuilder from './index';
-import * as QFD from '../utils/QueryFilterDefaults';
+import i18n from '@/i18n';
+import { findByText } from '@/utils/testHelpers';
 
-// Various test values
-// this.editProperty.value = true;
-// this.editProperty.value = '/userName co "test"';
-// this.editProperty.value = '(/city eq "" or /city eq "")';
-// this.editProperty.value = '(/city eq "" or /city eq "") or (/city eq "" or /city eq "")';
-// this.editProperty.value = '!(/userName co "test")';
-// this.editProperty.value = '!((/city eq "" or /city eq ""))';
-// this.editProperty.value = '!(/city eq "" and !(/city eq ""))';
-// this.editProperty.value = '!((/city eq "") and (/city eq "" and /city eq ""))';
-// this.editProperty.value = '!((/city eq "" or !((/city eq "" and /city eq ""))))';
-// this.editProperty.value = '!((!((/city eq "" and /city eq "")) or !((/city eq "" and /city eq ""))))';
-// this.editProperty.value = '(/userName co "test" or (/ co "" or (/sn co "" or /sn co "" or (/sn co "" or /sn co "" or /sn co ""))))';
-// this.editProperty.value = '(/userName co "test" or (/ co "" or (/sn co "" or /sn co "")))';
+function setup(props = {}, { validateFilterMock } = {}) {
+  // Stub before mount to avoid network call (allow override)
+  QueryFilterBuilder.methods.validateFilter = validateFilterMock || jest.fn().mockResolvedValue({
+    subfilters: [
+      { operator: 'co', field: '/', value: 'test' },
+    ],
+  });
 
-const subFilters1 = {
-  operator: 'co', field: '/', value: '', uniqueIndex: 1,
-};
-const subFilters2 = {
-  operator: 'eq', field: '/sn', value: 'name', uniqueIndex: 2,
-};
-const subFilters2Not = {
-  operator: '!eq', field: '/sn', value: 'name', uniqueIndex: 2,
-};
-const subFilters3 = {
-  operator: 'eq', field: '/sn', value: 'name', uniqueIndex: 3,
-};
-const subFilters3Not = {
-  operator: '!eq', field: '/sn', value: 'name', uniqueIndex: 3,
-};
-const subFilters4 = {
-  operator: 'eq', field: '/sn', value: 'name', uniqueIndex: 4,
-};
-const subFilterAccount = {
-  operator: 'eq', field: '/accountStatus', value: 'active', uniqueIndex: 1,
-};
-
-describe('QueryFilterBuilder', () => {
-  let wrapper;
-  QueryFilterBuilder.created = jest.fn();
-
-  beforeEach(() => {
-    wrapper = shallowMount(QueryFilterBuilder, {
-      global: {
-        stubs: { 'router-link': true },
-        mocks: {
-          $t: () => {},
-          $store: { state: { userId: 'foo' } },
+  return mount(QueryFilterBuilder, {
+    props: {
+      resourceName: 'user',
+      queryFilterString: '/ co ""',
+      properties: [
+        { label: 'Option 1', value: 'option1', type: 'string' },
+        { label: 'Option 2', value: 'option2', type: 'string' },
+        { label: 'Option 3', value: 'option3', type: 'string' },
+        { label: 'Option 4', value: 'option4', type: 'string' },
+        { label: 'Option 5', value: 'option5', type: 'string' },
+      ],
+      disabled: false,
+      hideAdvanced: false,
+      hideGroup: false,
+      prefixGroupText: '',
+      ...props,
+    },
+    global: {
+      plugins: [i18n],
+      mocks: {
+        $store: {
+          state: {
+            isFraas: true,
+          },
         },
       },
-      props: { resourceName: 'user' },
+    },
+  });
+}
+
+describe('@renders', () => {
+  it('mounts successfully', async () => {
+    const wrapper = setup();
+    await flushPromises();
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders a single FilterBuilderGroup when in basic mode', async () => {
+    const wrapper = setup();
+    await flushPromises();
+    const groups = wrapper.findAllComponents({ name: 'FilterBuilderGroup' });
+    expect(groups.length).toBe(1);
+  });
+
+  it('switches to Advanced Editor when button clicked and renders Script Editor then back to Basic', async () => {
+    const wrapper = setup();
+    await flushPromises();
+
+    expect(wrapper.findAllComponents({ name: 'FilterBuilderGroup' }).length).toBe(1);
+
+    const advancedBtn = findByText(wrapper, 'button', 'Advanced Editor');
+    expect(advancedBtn.exists()).toBe(true);
+    await advancedBtn.trigger('click');
+    await flushPromises();
+
+    const scriptEditor = wrapper.findComponent({ name: 'ScriptEditor' });
+    expect(scriptEditor.exists()).toBe(true);
+
+    const basicBtn = findByText(wrapper, 'button', 'Basic Editor');
+    expect(basicBtn.exists()).toBe(true);
+    await basicBtn.trigger('click');
+    await flushPromises();
+
+    expect(wrapper.findAllComponents({ name: 'FilterBuilderGroup' }).length).toBe(1);
+  });
+
+  it('renders disabled multiselect elements when disabled prop true', async () => {
+    const wrapper = setup({ disabled: true });
+    await flushPromises();
+    const multiselects = wrapper.findAll('.multiselect');
+    expect(multiselects.length).toBeGreaterThan(0);
+    multiselects.forEach((ms) => {
+      expect(ms.classes()).toContain('multiselect--disabled');
+      const input = ms.find('input.multiselect__input');
+      if (input.exists()) {
+        expect(input.element.disabled).toBe(true);
+      }
     });
   });
 
-  it('Creates default group', () => {
-    expect(wrapper.vm.getDefaultGroup()).toEqual({ subfilters: [subFilters1], operator: 'or', uniqueIndex: 2 });
-
-    expect(wrapper.vm.getDefaultGroup([subFilters4])).toEqual({
-      subfilters: [subFilters4], operator: 'or', uniqueIndex: 3,
+  it('renders enabled multiselect elements when disabled prop false', async () => {
+    const wrapper = setup({ disabled: false });
+    await flushPromises();
+    const multiselects = wrapper.findAll('.multiselect');
+    expect(multiselects.length).toBeGreaterThan(0);
+    multiselects.forEach((ms) => {
+      expect(ms.classes()).not.toContain('multiselect--disabled');
+      const input = ms.find('input.multiselect__input');
+      if (input.exists()) {
+        expect(input.element.disabled).toBe(false);
+      }
     });
   });
 
-  it('Creates default rule', () => {
-    expect(wrapper.vm.getDefaultRule()).toEqual(subFilters1);
-    expect(wrapper.vm.getDefaultRule('eq', '/sn', 'name')).toEqual(subFilters2);
+  it('shows Advanced Editor button when hideAdvanced false', async () => {
+    const wrapper = setup({ hideAdvanced: false });
+    await flushPromises();
+    const advancedBtn = findByText(wrapper, 'button', 'Advanced Editor');
+    expect(advancedBtn.exists()).toBe(true);
   });
 
-  it('Creates unique index', () => {
-    wrapper.vm.uniqueIndex = 0;
-    expect(wrapper.vm.getUniqueIndex()).toEqual(1);
+  it('hides Advanced Editor button when hideAdvanced true', async () => {
+    const wrapper = setup({ hideAdvanced: true });
+    await flushPromises();
+    const advancedBtn = findByText(wrapper, 'button', 'Advanced Editor');
+    expect(advancedBtn?.exists()).toBeFalsy();
   });
 
-  it('converts filter object to string', () => {
-    let returnObject = wrapper.vm.toFilterString({ operator: 'or', subfilters: [subFilters2] }, 0);
-    expect(returnObject).toEqual('/sn eq "name"');
+  it('updates filterString and emits filter-update when selecting property, operator and value', async () => {
+    const wrapper = setup();
+    await flushPromises();
 
-    returnObject = wrapper.vm.toFilterString({ operator: 'or', subfilters: [subFilters2Not] }, 0);
-    expect(returnObject).toEqual('!(/sn eq "name")');
+    // Select property Option 3
+    const propertySelect = wrapper.find('.rule-property-col .multiselect');
+    expect(propertySelect.exists()).toBe(true);
+    await propertySelect.trigger('click');
+    await flushPromises();
+    const propertyOptions = wrapper.findAll('.rule-property-col .multiselect__option');
+    const option3 = propertyOptions.find((o) => o.text().includes('Option 3'));
+    expect(option3).toBeTruthy();
+    await option3.trigger('click');
+    await flushPromises();
 
-    returnObject = wrapper.vm.toFilterString({ operator: 'or', subfilters: [subFilters2Not, subFilters3Not] }, 0);
-    expect(returnObject).toEqual('(!(/sn eq "name") or !(/sn eq "name"))');
+    // Select operator Contains (value: co)
+    const operatorSelect = wrapper.find('.rule-condition-col .multiselect');
+    expect(operatorSelect.exists()).toBe(true);
+    await operatorSelect.trigger('click');
+    await flushPromises();
+    const operatorOptions = wrapper.findAll('.rule-condition-col .multiselect__option');
+    const containsOption = operatorOptions.find((o) => /Contains/i.test(o.text()));
+    if (containsOption) {
+      await containsOption.trigger('click');
+      await flushPromises();
+    }
 
-    returnObject = wrapper.vm.toFilterString({
-      operator: 'or',
-      subfilters: [subFilters2Not, { operator: 'and', subfilters: [subFilters3, subFilters4], uniqueIndex: 5 }],
-    }, 0);
-    expect(returnObject).toEqual('(!(/sn eq "name") or (/sn eq "name" and /sn eq "name"))');
+    // Enter value test123
+    const valueInput = wrapper.find('.rule-value-col input');
+    expect(valueInput.exists()).toBe(true);
+    await valueInput.setValue('test123');
+    await flushPromises();
+
+    // Verify emission
+    const emitted = wrapper.emitted('filter-update');
+    expect(emitted).toBeTruthy();
+
+    // Last emission should contain updated queryFilter with selected field/operator/value
+    const lastEmission = emitted[emitted.length - 1][0];
+    expect(lastEmission.subfilters[0].field).toBe('/option3');
+    expect(lastEmission.subfilters[0].operator).toBe('co');
+    expect(lastEmission.subfilters[0].value).toBe('test123');
   });
 
-  it('Checks for boolean operators', () => {
-    let returnObject = wrapper.vm.hasAdvancedProperty({ operator: 'or', subfilters: [subFilters2] }, false);
-    expect(returnObject).toBeFalsy();
+  it('shows ScriptEditor with complex filter and disables Basic Editor button', async () => {
+    const complexFilter = '/effectiveGroups[_ref eq "managed/alpha_group/Test"]';
 
-    returnObject = wrapper.vm.hasAdvancedProperty({ operator: 'or', subfilters: [{ operator: false, uniqueIndex: 2 }] }, false);
-    expect(returnObject).toBeTruthy();
-  });
-
-  it('normalize query object', () => {
-    const shouldNotChange = { operator: 'or', subfilter: subFilters3, uniqueIndex: 2 };
-    expect(wrapper.vm.normalizeQueryFilterObject(shouldNotChange)).toEqual(shouldNotChange);
-
-    const shouldChange = { operator: 'or', subfilters: [{ operator: '!', subfilter: subFilters3, uniqueIndex: 2 }] };
-    expect(wrapper.vm.normalizeQueryFilterObject(shouldChange)).toEqual({ operator: 'or', subfilters: [subFilters3Not] });
-  });
-
-  describe('UpdateFilter', () => {
-    it('Updates filter with new rule', () => {
-      const addRule = {
-        depth: 0, index: 0, path: '0', value: { field: '/userName' },
-      };
-      const qfBefore = { subfilters: [subFilters3], operator: 'or', uniqueIndex: 4 };
-      const resp = {
-        subfilters: [subFilters3, { operator: 'or', subfilters: [subFilters1], uniqueIndex: 2 }],
-        operator: 'or',
-        uniqueIndex: 4,
-      };
-
-      wrapper.setData({ queryFilter: qfBefore });
-      wrapper.vm.updateFilter('add-rule', addRule);
-      expect(wrapper.vm.queryFilter).toEqual(resp);
+    const validateFilterMock = jest.fn().mockResolvedValue({
+      field: '/effectiveGroups',
+      subfilter: {
+        field: '_ref',
+        value: 'managed/alpha_group/Test',
+        operator: 'eq',
+      },
+      operator: 'complex',
     });
 
-    it('Updates filter with rule change', () => {
-      const ruleChange = {
-        depth: 0, index: 0, path: '0', value: { operator: '!eq' },
-      };
-      const qfBefore = { subfilters: [subFilters2], operator: 'or', uniqueIndex: 1 };
-      const resp = { subfilters: [subFilters2Not], operator: 'or', uniqueIndex: 1 };
+    const wrapper = setup({ queryFilterString: complexFilter }, { validateFilterMock });
+    await flushPromises();
 
-      wrapper.setData({ queryFilter: qfBefore });
-      wrapper.vm.updateFilter('rule-change', ruleChange);
-      expect(wrapper.vm.queryFilter).toEqual(resp);
-    });
+    expect(validateFilterMock).toHaveBeenCalled();
 
-    it('Updates filter by removing rule', () => {
-      const removeRule = { depth: 0, index: 1, path: '0' };
-      const qfBefore = {
-        subfilters: [
-          subFilters3Not,
-          subFilterAccount,
-        ],
-        operator: 'or',
-      };
-      const resp = { operator: 'or', subfilters: [subFilters3Not] };
+    // Should be in advanced mode (isBasic === false) so ScriptEditor visible
+    const scriptEditor = wrapper.findComponent({ name: 'ScriptEditor' });
+    expect(scriptEditor.exists()).toBe(true);
 
-      wrapper.setData({ queryFilter: qfBefore });
-      wrapper.vm.updateFilter('remove-rule', removeRule);
-      expect(wrapper.vm.queryFilter).toEqual(resp);
-    });
-  });
-
-  describe('To Filter String', () => {
-    it('If only one subfilter and its !', () => {
-      const filter = { subfilter: subFilters3Not };
-      const newString = wrapper.vm.toFilterString(filter, 0);
-      expect(newString).toEqual('!!(/sn eq "name")');
-    });
-
-    it('If only one subfilter and its not !', () => {
-      const filter = { subfilter: subFilters3 };
-      const newString = wrapper.vm.toFilterString(filter, 0);
-      expect(newString).toEqual('/sn eq "name"');
-    });
-
-    it('Returns correct group if there are multiple', () => {
-      const filter = { subfilter: subFilters3 };
-      const newString = wrapper.vm.toFilterString(filter, 1);
-      expect(newString).toEqual('(/sn eq "name")');
-    });
-  });
-
-  describe('To Type', () => {
-    it('Returns a string', () => {
-      QFD.getTypeFromValue = jest.fn().mockReturnValueOnce('string');
-      const newVal = wrapper.vm.toType('string', 'field');
-      expect(newVal).toBe('"string"');
-    });
-
-    it('Returns a number', () => {
-      QFD.getTypeFromValue = jest.fn().mockReturnValueOnce('number');
-      const newVal = wrapper.vm.toType('1', 'field');
-      expect(newVal).toBe('1');
-    });
-
-    it('Returns a boolean', () => {
-      QFD.getTypeFromValue = jest.fn().mockReturnValueOnce('boolean');
-      const newVal = wrapper.vm.toType('true', 'field');
-      expect(newVal).toBeTruthy();
-    });
-
-    it('Returns the default', () => {
-      QFD.getTypeFromValue = jest.fn().mockReturnValueOnce('object');
-      const newVal = wrapper.vm.toType('string', 'field');
-      expect(newVal).toBe('"string"');
-    });
-
-    it('Returns a default if no type found', () => {
-      QFD.getTypeFromValue = jest.fn().mockReturnValueOnce(null);
-      const newVal = wrapper.vm.toType('string', 'field');
-      expect(newVal).toBe('"string"');
-    });
+    // Basic Editor button should be present but disabled (allowBasicMode false)
+    const basicBtn = findByText(wrapper, 'button', 'Basic Editor');
+    expect(basicBtn.exists()).toBe(true);
+    expect(basicBtn.attributes('disabled')).toBeDefined();
   });
 });
