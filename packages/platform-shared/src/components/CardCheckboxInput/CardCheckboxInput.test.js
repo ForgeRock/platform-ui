2791f043be4bd2718200c25d27ae685d19af3bb5
Zafar Ahmad <zahmad@pingidentity.com>
2025-11-25 12:08:26 +0530

    IAM-9639 CardRadioInput and CardCheckboxInput A11Y tests implementation
    IAM-9639 Resolving the review comments

--- packages/platform-shared/src/components/CardCheckboxInput/CardCheckboxInput.test.js
+++ packages/platform-shared/src/components/CardCheckboxInput/CardCheckboxInput.test.js
@@ -7,6 +7,7 @@
 
 import { mount } from '@vue/test-utils';
 import CardCheckboxInput from './index';
+import { runA11yTest } from '../../utils/testHelpers';
 
 describe('CardCheckboxInput Component', () => {
   function setup(props = {}, slotContent = '') {
@@ -17,32 +18,66 @@ describe('CardCheckboxInput Component', () => {
       slots: {
         default: slotContent,
       },
+      global: {
+        mocks: {
+          $t: () => {},
+        },
+      },
     });
     return wrapper;
   }
 
-  it('has a checkbox input', () => {
-    const wrapper = setup();
-    expect(wrapper.find('input[type="checkbox"]').exists()).toBe(true);
+  describe('@a11y', () => {
+    it('should pass accessibility tests when the value is set to default false', async () => {
+      const wrapper = setup({}, '<div>Test Content</div>');
+      await runA11yTest(wrapper);
+    });
+
+    it('should pass accessibility tests when disabled is set to true', async () => {
+      const wrapper = setup({ disabled: true }, '<div>Test Content</div>');
+      await runA11yTest(wrapper);
+    });
+
+    it('should pass accessibility tests when the cardBodyClass is set', async () => {
+      const wrapper = setup({ cardBodyClass: 'card-body-test-class' }, '<div>Test Content</div>');
+      await runA11yTest(wrapper);
+    });
+
+    it('should pass accessibility tests when all props are set', async () => {
+      const wrapper = setup({ value: false, disabled: true, cardBodyClass: 'card-body-test-class' }, '<div>Test Content</div>');
+      await runA11yTest(wrapper);
+    });
+
+    it('should pass accessibility tests when slot is empty', async () => {
+      const wrapper = setup({ value: false, disabled: true, cardBodyClass: 'card-body-test-class' }, '');
+      await runA11yTest(wrapper);
+    });
   });
 
-  it('uses a card', () => {
-    const wrapper = setup();
-    expect(wrapper.find('input[type="checkbox"]').attributes('class')).toBe('card-input-element');
-  });
+  describe('Component behavior', () => {
+    it('has a checkbox input', () => {
+      const wrapper = setup();
+      expect(wrapper.find('input[type="checkbox"]').exists()).toBe(true);
+    });
 
-  it('has a default value of false', () => {
-    const wrapper = setup();
-    expect(wrapper.find('input[type="checkbox"]').attributes('value')).toBe('false');
-  });
+    it('uses a card', () => {
+      const wrapper = setup();
+      expect(wrapper.find('input[type="checkbox"]').attributes('class')).toBe('card-input-element');
+    });
 
-  it('sets value to true based on prop', () => {
-    const wrapper = setup({ value: true });
-    expect(wrapper.find('input[type="checkbox"]').attributes('value')).toBe('true');
-  });
+    it('has a default value of false', () => {
+      const wrapper = setup();
+      expect(wrapper.find('input[type="checkbox"]').attributes('value')).toBe('false');
+    });
 
-  it('renders default slot content', () => {
-    const wrapper = setup({}, '<div>Test</div>');
-    expect(wrapper.find('div').text()).toBe('Test');
+    it('sets value to true based on prop', () => {
+      const wrapper = setup({ value: true });
+      expect(wrapper.find('input[type="checkbox"]').attributes('value')).toBe('true');
+    });
+
+    it('renders default slot content', () => {
+      const wrapper = setup({}, '<div>Test</div>');
+      expect(wrapper.find('div').text()).toBe('Test');
+    });
   });
 });
