3aacbd1c475dd69937d4fe1845f817cb542db11d
Zafar Ahmad <zahmad@pingidentity.com>
2025-11-27 13:01:32 +0530

    IAM-9640 List item and list group axe tests

--- packages/platform-shared/src/components/ListItem/ListItem.test.js
+++ packages/platform-shared/src/components/ListItem/ListItem.test.js
@@ -1,12 +1,37 @@
 /**
- * Copyright (c) 2019-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2019-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import ListItem from './index';
+import { runA11yTest } from '../../utils/testHelpers';
+
+describe('a11y tests', () => {
+  function setup(props = {}, slotData = {}) {
+    const wrapper = mount(ListItem, {
+      props: {
+        ...props,
+      },
+      slots: slotData,
+    });
+    return wrapper;
+  }
+  it('should pass accessibility tests with default props', async () => {
+    const wrapper = setup();
+    await runA11yTest(wrapper);
+  });
+
+  it('should pass accessibility tests with custom slot content', async () => {
+    const wrapper = setup({ panelShown: true }, {
+      'list-item-header': '<div>Custom List Header</div>',
+      'list-item-collapse-body': '<div>Custom Collapse Body</div>',
+    });
+    await runA11yTest(wrapper);
+  });
+});
 
 describe('ListItem Component', () => {
   it('ListItem successfully loaded', () => {
@@ -17,7 +42,6 @@ describe('ListItem Component', () => {
         },
       },
     });
-
-    expect(wrapper.vm.id).toEqual('listItem3');
+    expect(wrapper.vm.id).toEqual(`listItem${wrapper.vm._uid}`);
   });
 });
