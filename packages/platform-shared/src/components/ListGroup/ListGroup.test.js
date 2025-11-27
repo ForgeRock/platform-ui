3d1447662eeffaa6e8caa9c89945f7420ff453ae
Zafar Ahmad <zahmad@pingidentity.com>
2025-11-27 13:01:32 +0530

    IAM-9640 List item and list group axe tests

--- packages/platform-shared/src/components/ListGroup/ListGroup.test.js
+++ packages/platform-shared/src/components/ListGroup/ListGroup.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2019-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -7,6 +7,37 @@
 
 import { mount } from '@vue/test-utils';
 import ListGroup from './index';
+
+import { runA11yTest } from '../../utils/testHelpers';
+
+describe('a11y tests', () => {
+  function setup(props = {}, slotData = {}) {
+    const wrapper = mount(ListGroup, {
+      props: {
+        ...props,
+      },
+      slots: slotData,
+    });
+    return wrapper;
+  }
+
+  it('should pass accessibility tests with default props', async () => {
+    const wrapper = setup();
+    await runA11yTest(wrapper);
+  });
+
+  it('should pass accessibility tests with title and subtitle props', async () => {
+    const wrapper = setup({ title: 'Test Title', subtitle: 'Test Subtitle' });
+    await runA11yTest(wrapper);
+  });
+
+  it('should pass accessibility tests with custom slot content', async () => {
+    const wrapper = setup({ noMargin: true }, {
+      'list-group-header': '<div>Custom Group Header</div>',
+    });
+    await runA11yTest(wrapper);
+  });
+});
 
 describe('ListGroup Component', () => {
   let wrapper;
