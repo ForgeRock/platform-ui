f246da793ff2060cc3ad18af7fecccf72618ae85
Zafar Ahmad <zahmad@pingidentity.com>
2025-12-11 20:04:50 +0530

    IAM-9734 Added jest-axe to ListOfLists component

--- packages/platform-shared/src/components/ListOfLists/ListOfLists.test.js
+++ packages/platform-shared/src/components/ListOfLists/ListOfLists.test.js
@@ -1,14 +1,35 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import ListOfLists from './index';
+import { runA11yTest } from '../../utils/testHelpers';
 
 describe('ListOfLists', () => {
+  describe('@a11y', () => {
+    it('should pass accessibility tests with default props', async () => {
+      const wrapper = mount(ListOfLists, {
+        global: {
+          mocks: {
+            $t: (label) => label,
+          },
+        },
+        props: {
+          label: 'Test Label',
+          description: 'This is a test list',
+          items: {},
+          name: 'test',
+          value: ['test'],
+        },
+      });
+      await runA11yTest(wrapper);
+    });
+  });
+
   it('ListOfLists sets listValues when there is a value', () => {
     const wrapper = shallowMount(ListOfLists, {
       global: {
