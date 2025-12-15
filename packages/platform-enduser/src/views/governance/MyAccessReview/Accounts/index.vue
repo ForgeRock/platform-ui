c4e4f57eef8a12565f275978c26e83a9971f4339
Pablo Torterolo <pablotorterolo@pingidentity.com>
2025-12-15 13:03:35 -0300

    IAM-7886 Title added to actions column in governance table

--- packages/platform-enduser/src/views/governance/MyAccessReview/Accounts/index.vue
+++ packages/platform-enduser/src/views/governance/MyAccessReview/Accounts/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2023-2025 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2023-2026 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -52,8 +52,8 @@ export default {
         },
         {
           key: 'actions',
-          label: '',
-          class: 'p-3 fr-no-resize sticky-right w-100px',
+          label: this.$t('common.actions'),
+          class: 'fr-no-resize sticky-right w-120px',
           sortable: false,
         },
       ],
