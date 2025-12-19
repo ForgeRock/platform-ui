0a57306c75720401b75a7a9325e79f813838bbc2
vgupta <vgupta@pingidentity.com>
2025-12-19 15:55:24 +0530

    IAM-9773 enable a11y scan in LocaleDropdown component

--- packages/platform-shared/src/components/LocaleDropdown/LocaleDropdown.test.js
+++ packages/platform-shared/src/components/LocaleDropdown/LocaleDropdown.test.js
@@ -1,14 +1,57 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { mount, shallowMount } from '@vue/test-utils';
+import { flushPromises, mount, shallowMount } from '@vue/test-utils';
+import { runA11yTest } from '@forgerock/platform-shared/src/utils/testHelpers';
 import LocaleDropdown from './index';
 
 describe('LocaleDropdown', () => {
+  describe('@a11y', () => {
+    let wrapper;
+    function mountComponent(propsData = {}) {
+      return mount(LocaleDropdown, {
+        global: {
+          mocks: {
+            $t: (msg) => msg,
+          },
+        },
+        props: { ...propsData },
+      });
+    }
+
+    afterEach(() => {
+      if (wrapper?.unmount) {
+        wrapper.unmount();
+      }
+    });
+
+    const a11yScenarios = [
+      { name: 'there are no dropdown items', props: { dropdownItems: [], title: 'Locale Dropdown' } },
+      {
+        name: 'dropdown items are present and add button is enabled',
+        props: {
+          showAdd: true,
+          dropdownItems: [{
+            locale: 'one',
+            active: true,
+          }, {
+            locale: 'two',
+            active: false,
+          }],
+          title: 'Locale Dropdown',
+        },
+      },
+    ];
+    it.each(a11yScenarios)('no a11y violations when $name', async ({ props }) => {
+      wrapper = mountComponent(props);
+      await flushPromises();
+      await runA11yTest(wrapper);
+    });
+  });
   it('Does not show "Add Button" when prop not set', () => {
     const wrapper = shallowMount(LocaleDropdown, {
       global: {
