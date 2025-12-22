3e77ac267bb4f9d35d82ff1ae8af07eddc754397
Zafar Ahmad <zahmad@pingidentity.com>
2025-12-22 14:48:52 +0530

    IAM-9778 Added jest-axe unit test cases for ButtonWithSpinner component

--- packages/platform-shared/src/components/ButtonWithSpinner/ButtonWithSpinner.test.js
+++ packages/platform-shared/src/components/ButtonWithSpinner/ButtonWithSpinner.test.js
@@ -1,14 +1,53 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2025 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import ButtonWithSpinner from './index';
+import { runA11yTest } from '../../utils/testHelpers';
 
 describe('ButtonWithSpinner', () => {
+  describe('@a11y', () => {
+    function setupWrapper(props = {}) {
+      return mount(ButtonWithSpinner, {
+        global: {
+          mocks: {
+            $t: (label) => label,
+          },
+        },
+        props,
+      });
+    }
+
+    it('Component should be accessible when button is enabled', async () => {
+      const wrapper = setupWrapper({ disabled: false });
+      await runA11yTest(wrapper);
+    });
+
+    it('Component should be accessible when button is disabled', async () => {
+      const wrapper = setupWrapper({ disabled: true });
+      await runA11yTest(wrapper);
+    });
+
+    it('Component should be accessible when spinner is showing', async () => {
+      const wrapper = setupWrapper({ showSpinner: true });
+      await runA11yTest(wrapper);
+    });
+
+    it('Component should be accessible when showSpinner is hidden', async () => {
+      const wrapper = setupWrapper({ showSpinner: false });
+      await runA11yTest(wrapper);
+    });
+
+    it('Component should be accessible with custom labels', async () => {
+      const wrapper = setupWrapper({ buttonText: 'Custom Label', spinnerText: 'Loading Custom Label' });
+      await runA11yTest(wrapper);
+    });
+  });
+
   it('ButtonWithSpinner successfully loaded', () => {
     const wrapper = shallowMount(ButtonWithSpinner, {
       global: {
