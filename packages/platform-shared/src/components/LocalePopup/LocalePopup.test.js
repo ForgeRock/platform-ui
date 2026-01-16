2596800d873524b1fc5b9422d6444a97fe0fcff5
vgupta <vgupta@pingidentity.com>
2026-01-16 11:25:59 +0530

    IAM-9789 enable a11y check on LocalePopup component and refactor BDropDown to BPopover

--- packages/platform-shared/src/components/LocalePopup/LocalePopup.test.js
+++ packages/platform-shared/src/components/LocalePopup/LocalePopup.test.js
@@ -1,24 +1,65 @@
 /**
- * Copyright (c) 2025 ForgeRock. All rights reserved.
+ * Copyright (c) 2025-2026 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
 import { nextTick } from 'vue';
-import { mount } from '@vue/test-utils';
-import { findByText } from '@forgerock/platform-shared/src/utils/testHelpers';
+import { DOMWrapper, flushPromises, mount } from '@vue/test-utils';
+import {
+  createAppContainer,
+  findAllByText,
+  findByText,
+  runA11yTest,
+} from '@forgerock/platform-shared/src/utils/testHelpers';
+import { mockValidation } from '@forgerock/platform-shared/src/testing/utils/mockValidation';
 import LocalePopup from './LocalePopup';
 
+mockValidation(['required']);
+
 describe('LocalePopup.vue', () => {
-  const mountComponent = (props = {}) => mount(LocalePopup, {
-    props,
-    global: {
-      stubs: ['FrField', 'VeeForm'],
-      mocks: {
-        $t: (msg) => msg,
+  const mountComponent = (props = {}) => {
+    const wrapper = mount(LocalePopup, {
+      props,
+      attachTo: createAppContainer(),
+      global: {
+        mocks: {
+          $t: (msg) => msg,
+        },
       },
-    },
+    });
+
+    const domWrapper = new DOMWrapper(document.body);
+    return { wrapper, domWrapper };
+  };
+
+  const openPopover = async (wrapper) => {
+    wrapper.vm.popoverShow = true;
+    await flushPromises();
+  };
+
+  const originalGetBoundingClientRect = global.Element.prototype.getBoundingClientRect;
+  beforeAll(() => {
+    // Mocking the size of elements so Popper.js doesn't bail out
+    // as it thinks there's no space to render the popover
+    global.Element.prototype.getBoundingClientRect = () => ({
+      width: 100,
+      height: 30,
+      top: 0,
+      left: 0,
+      right: 100,
+      bottom: 30,
+    });
+  });
+
+  afterAll(() => {
+    // Restore the original getBoundingClientRect function
+    global.Element.prototype.getBoundingClientRect = originalGetBoundingClientRect;
+  });
+
+  beforeEach(() => {
+    document.body.innerHTML = '';
   });
 
   // Sample translations to use in tests
@@ -27,50 +68,69 @@ describe('LocalePopup.vue', () => {
     { locale: 'fr', value: 'Bonjour' },
   ];
 
+  describe('@a11y', () => {
+    it('component should not have any accessibility violations', async () => {
+      const { wrapper } = mountComponent({ translations });
+      await flushPromises();
+      await runA11yTest(wrapper);
+    });
+  });
+
   it('renders the dropdown and translation fields', async () => {
-    const wrapper = mountComponent({ translations });
+    const { wrapper, domWrapper } = mountComponent({ translations });
+    await flushPromises();
     expect(wrapper.html()).toContain('translations.dropdownTitle');
-    expect(wrapper.findAll('[name="translationLocale"]').length).toBe(2);
-    expect(wrapper.findAll('[name="translationKey"]').length).toBe(2);
+
+    await openPopover(wrapper);
+
+    expect(domWrapper.html()).toContain('translations.dropdownTitle');
+    expect(domWrapper.findAll('[name="translationLocale"]').length).toBe(2);
+    expect(domWrapper.findAll('[name="translationKey"]').length).toBe(2);
   });
 
   it('adds a translation entry when add button is clicked', async () => {
-    const wrapper = mountComponent({ translations });
+    const { wrapper, domWrapper } = mountComponent({ translations });
     await nextTick();
-    const addButtons = wrapper.findAllComponents({ name: 'BButton' }).filter((btn) => btn.html().includes('add'));
+    await openPopover(wrapper);
+
+    const addButtons = findAllByText(domWrapper, '.btn-link', 'add');
     await addButtons[0].trigger('click');
     await nextTick();
-    expect(wrapper.findAll('[name="translationLocale"]').length).toBe(3);
+    expect(domWrapper.findAll('[name="translationLocale"]').length).toBe(3);
   });
 
   it('removes a translation entry when remove button is clicked', async () => {
-    const wrapper = mountComponent({ translations });
+    const { wrapper, domWrapper } = mountComponent({ translations });
     await nextTick();
-    const removeButton = findByText(wrapper, '.btn-link', 'remove');
+    await openPopover(wrapper);
 
+    const removeButton = findByText(domWrapper, '.btn-link', 'remove');
     await removeButton.trigger('click');
     await nextTick();
-    expect(wrapper.findAll('[name="translationLocale"]').length).toBe(1);
+    expect(domWrapper.findAll('[name="translationLocale"]').length).toBe(1);
   });
 
   it('emits update:translations when apply is clicked', async () => {
-    const wrapper = mountComponent({ translations });
-    await nextTick();
+    const { wrapper, domWrapper } = mountComponent({ translations });
+    await flushPromises();
+    await openPopover(wrapper);
+
     // Simulate clicking apply (primary button)
-    const applyButton = findByText(wrapper, '.btn-primary', 'apply');
-    applyButton.trigger('click');
-    // Set closeAction and call hideDropdown manually, as this component does not seem to respond to the click event here
+    const applyButton = findByText(domWrapper, '.btn-primary', 'apply');
+    await applyButton.trigger('click');
+
+    // Set closeAction and call hidePopover manually, as this component does not seem to respond to the click event here
     wrapper.vm.closeAction = 'update';
-    wrapper.vm.hideDropdown({ preventDefault: jest.fn() });
+    wrapper.vm.hidePopover();
     expect(wrapper.emitted('update:translations')).toBeTruthy();
   });
 
   it('does not emit update:translations when cancel is clicked', async () => {
-    const wrapper = mountComponent({ translations });
+    const { wrapper } = mountComponent({ translations });
     await nextTick();
     // Simulate clicking cancel (link button)
     wrapper.vm.closeAction = 'close';
-    wrapper.vm.hideDropdown({ preventDefault: jest.fn() });
+    wrapper.vm.hidePopover();
     await nextTick();
     expect(wrapper.emitted('update:translations')).toBeFalsy();
   });
