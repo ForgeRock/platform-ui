931020c9a98d3ab528f6df6211ff6fa27eb45249
Chitta Ranjan Sahoo <cranjansahoo@pingidentity.com>
2026-01-29 14:11:58 +0530

    IAM-7889 Replaced semantic header and footer with div tag in Add delegate modal for accessibility fix

--- packages/platform-enduser/src/views/governance/Directory/Delegates/AddDelegateModal/AddDelegateModal.test.js
+++ packages/platform-enduser/src/views/governance/Directory/Delegates/AddDelegateModal/AddDelegateModal.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023-2025 ForgeRock. All rights reserved.
+ * Copyright (c) 2023-2026 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -52,6 +52,44 @@ describe('AddDelegateModal', () => {
         isTesting: true,
       },
     });
+  });
+
+  it('modal should not render semantic header and footer elements', () => {
+    expect(wrapper.find('header').exists()).toBe(false);
+    expect(wrapper.find('footer').exists()).toBe(false);
+  });
+
+  it('close button should be rendered within div tag with modal-header class', () => {
+    const modalHeader = wrapper.find('div.modal-header');
+    expect(modalHeader.exists()).toBe(true);
+
+    const closeButton = modalHeader.find('button.close');
+    expect(closeButton.exists()).toBe(true);
+  });
+
+  it('save and cancel buttons should be rendered within div tag with modal-footer class', () => {
+    const modalFooter = wrapper.find('div.modal-footer');
+    expect(modalFooter.exists()).toBe(true);
+
+    const cancelButton = modalFooter.find('button.btn-link');
+    const saveButton = modalFooter.find('[data-testid="save-button"]');
+
+    expect(cancelButton.exists()).toBe(true);
+    expect(cancelButton.text()).toBe('common.cancel');
+    expect(saveButton.exists()).toBe(true);
+    expect(saveButton.text()).toBe('common.save');
+  });
+
+  it('cancel button should reset the modal data', async () => {
+    const modalFooter = wrapper.find('div.modal-footer');
+    const cancelButton = modalFooter.find('button.btn-link');
+    await cancelButton.trigger('click');
+    await flushPromises();
+
+    expect(wrapper.vm.delegates).toEqual([]);
+    expect(wrapper.vm.enableTimeConstraint).toBe(false);
+    expect(wrapper.vm.startDate).toEqual('');
+    expect(wrapper.vm.endDate).toEqual('');
   });
 
   it('save button disabled when no user is selected', () => {
