b29e8167676da5758d1e6082fb9f8873bd215543
Zafar Ahmad <zahmad@pingidentity.com>
2026-02-19 10:37:21 +0530

    IAM-9992 Implemented jest-axe a11y tests for DateInput and DateTimeInput

--- packages/platform-shared/src/components/Field/DateInput/DateInput.test.js
+++ packages/platform-shared/src/components/Field/DateInput/DateInput.test.js
@@ -1,12 +1,13 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2026 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import DateInput from './index';
+import { runA11yTest } from '../../../utils/testHelpers';
 
 const defaultMixinProps = {
   id: '',
@@ -47,5 +48,24 @@ describe('DateInput', () => {
     expect(wrapper.emitted().input[0][0]).toBe('4000-01-01');
     wrapper.vm.emitDateValue('');
     expect(wrapper.emitted().input[1][0]).toBe('');
+  });
+
+  describe('@a11y', () => {
+    it('has no accessibility violations', async () => {
+      const wrapper = mount(DateInput, {
+        props: {
+          ...defaultMixinProps,
+          ...defaultProps,
+          label: 'test',
+        },
+        global: {
+          mocks: {
+            $t: (text) => (text),
+          },
+        },
+      });
+
+      await runA11yTest(wrapper);
+    });
   });
 });
--- packages/platform-shared/src/components/Field/DateTimeInput/DateTimeInput.test.js
+++ packages/platform-shared/src/components/Field/DateTimeInput/DateTimeInput.test.js
@@ -1,12 +1,13 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2026 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import DateTimeInput from './index';
+import { runA11yTest } from '../../../utils/testHelpers';
 
 const defaultMixinProps = {
   id: '',
@@ -83,5 +84,24 @@ describe('DateTimeInput', () => {
     });
     wrapper.vm.emitDateTimeValue();
     expect(wrapper.emitted().input[2][0]).toContain('T01:01:01Z');
+  });
+
+  describe('@a11y', () => {
+    it('has no accessibility violations', async () => {
+      const wrapper = mount(DateTimeInput, {
+        props: {
+          ...defaultMixinProps,
+          ...defaultProps,
+          label: 'test',
+        },
+        global: {
+          mocks: {
+            $t: (text) => (text),
+          },
+        },
+      });
+
+      await runA11yTest(wrapper);
+    });
   });
 });
