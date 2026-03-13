e05501775d32bdcc823117e5ffab517bcc7dca35
Zafar Ahmad <zahmad@pingidentity.com>
2026-03-13 16:53:51 +0530

    IAM-10064 Added jest-axe tests for DurationInput

--- packages/platform-shared/src/components/Field/DurationInput/DurationInput.test.js
+++ packages/platform-shared/src/components/Field/DurationInput/DurationInput.test.js
@@ -1,11 +1,12 @@
 /**
- * Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+ * Copyright (c) 2021-2026 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
+import { runA11yTest } from '@forgerock/platform-shared/src/utils/testHelpers';
 import DurationInput from './index';
 
 const defaultProps = {
@@ -76,5 +77,23 @@ describe('DurationInput', () => {
     });
 
     expect(wrapper.vm.durationUnit).toBe('months');
+  });
+
+  describe('@a11y', () => {
+    it('has no accessibility violations', async () => {
+      const wrapper = mount(DurationInput, {
+        props: {
+          ...defaultProps,
+          label: 'Duration Input',
+        },
+        global: {
+          mocks: {
+            $t: (text) => (text),
+          },
+        },
+      });
+      await wrapper.vm.$nextTick();
+      await runA11yTest(wrapper);
+    });
   });
 });
