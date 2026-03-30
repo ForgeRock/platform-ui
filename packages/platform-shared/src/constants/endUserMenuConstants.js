ab18ad01c5682c02b694f5158d0fde3040bf6f9e
Matt Kormann <matthew.kormann@pingidentity.com>
2026-03-30 17:36:28 -0400

    IGA-4265 Remove gov dev flags for Role LCM

--- packages/platform-shared/src/constants/endUserMenuConstants.js
+++ packages/platform-shared/src/constants/endUserMenuConstants.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 ForgeRock. All rights reserved.
+ * Copyright (c) 2025-2026 ForgeRock. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -190,7 +190,6 @@ export const DEFAULT_MENU_ITEMS = [
       {
         id: END_USER_MENU_CONSTANTS.LCM_ROLES,
         routeTo: { name: 'AdministerRoles' },
-        available: (flag) => flag.governanceDevEnabled,
       },
     ],
   },
