563c52ba00388ce474b4495dd8b4ef581228d9c3
Andrew Hertel <andrew.hertel@forgerock.com>
2026-05-05 15:40:59 +0200

    IAM-10810 Remove P1Integration feature flag

--- packages/platform-shared/src/store/modules/Shared.js
+++ packages/platform-shared/src/store/modules/Shared.js
@@ -208,10 +208,6 @@ const mutations = {
         }
       }
 
-      if (env.VUE_APP_ENABLE_PINGONE_INTEGRATION?.toString() === 'true') {
-        state.pingOneIntegrationsEnabled = true;
-      }
-
       if (env.VUE_APP_ENABLE_PROXY_CONNECT?.toString() === 'true') {
         state.proxyConnectEnabled = true;
       }
