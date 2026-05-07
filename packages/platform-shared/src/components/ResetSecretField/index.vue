47012b6335a767e09b071c5fa08475829c75ff19
Sam Jennings <sam.jennings@workday.com>
2026-05-07 16:02:46 -0700

    IAM-10655-authentication-add-edit-module

--- packages/platform-shared/src/components/ResetSecretField/index.vue
+++ packages/platform-shared/src/components/ResetSecretField/index.vue
@@ -1,4 +1,4 @@
-<!-- Copyright (c) 2021-2023 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2021-2026 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
@@ -43,6 +43,7 @@ of the MIT license. See the LICENSE file for details. -->
             :label="$t('schemaResetSecret.newSecretText', { field: effectiveLabel })" />
         </BFormGroup>
         <FrAlert
+          v-if="showWarning"
           show
           variant="warning"
           :dismissible="false">
@@ -104,6 +105,13 @@ export default {
     label: {
       type: String,
       default: '',
+    },
+    /**
+     * Whether to show the warning alert inside the reset modal
+     */
+    showWarning: {
+      type: Boolean,
+      default: true,
     },
   },
   data() {
