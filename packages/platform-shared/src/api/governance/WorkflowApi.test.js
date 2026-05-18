d7e75e31f03ae33b88eebf9749b52ecc37fe65cc
akshay.koganur <akoganur@pingidentity.com>
2026-05-18 15:54:17 +0530

    IGA-3825 Changed api call for managing workflows to new api /governance/workflow

--- packages/platform-shared/src/api/governance/WorkflowApi.test.js
+++ packages/platform-shared/src/api/governance/WorkflowApi.test.js
@@ -1,21 +1,20 @@
 /**
- * Copyright 2024 ForgeRock AS. All Rights Reserved
+ * Copyright (c) 2024-2026 ForgeRock. All rights reserved.
  *
- * Use of this code requires a commercial software license with ForgeRock AS
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
  */
 
 import * as BaseApi from '@forgerock/platform-shared/src/api/BaseApi';
 import encodeQueryString from '@forgerock/platform-shared/src/utils/encodeQueryString';
 import * as WorkflowApi from './WorkflowApi';
 
-const workflowUrl = '/orchestration/definition';
+const workflowUrl = '/governance/workflow';
 const deleteMock = jest.fn();
 const getMock = jest.fn();
 const postMock = jest.fn();
 const putMock = jest.fn();
-BaseApi.generateIgaOrchestrationApi = jest.fn(() => ({
+BaseApi.generateIgaApi = jest.fn(() => ({
   delete: deleteMock,
   get: getMock,
   post: postMock,
