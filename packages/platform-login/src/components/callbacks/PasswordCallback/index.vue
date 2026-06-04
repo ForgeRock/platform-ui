f577e428b69e8ac7e18641b475662a4595146e38
Zafar Ahmad <zahmad@pingidentity.com>
2026-06-04 18:35:04 +0530

    IAM-10375 Add aria-required to Username and Password login fields

--- packages/platform-login/src/components/callbacks/PasswordCallback/index.vue
+++ packages/platform-login/src/components/callbacks/PasswordCallback/index.vue
@@ -1,12 +1,12 @@
-<!-- Copyright (c) 2022-2024 ForgeRock. All rights reserved.
+<!-- Copyright (c) 2022-2026 ForgeRock. All rights reserved.
 
 This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details. -->
 <template>
   <FrField
     class="password-field"
-    :is-required-aria="true"
     v-bind="{...$attrs}"
+    :is-required-aria="true"
     v-on="$listeners" />
 </template>
 <script>
