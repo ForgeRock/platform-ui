8b1872b4f91ea8055464412a54375cd0012a1663
Oliver Bradley <oliver.bradley@forgerock.com>
2026-06-22 15:51:41 -0700

    IAM-10898

--- packages/platform-login/src/views/Login/Login.test.js
+++ packages/platform-login/src/views/Login/Login.test.js
@@ -672,6 +672,7 @@ describe('Component Test', () => {
     };
 
     beforeEach(() => {
+      jest.useRealTimers();
       jest.spyOn(FRAuth, 'next').mockImplementation(() => Promise.resolve(new FRStep(authData)));
     });
 
