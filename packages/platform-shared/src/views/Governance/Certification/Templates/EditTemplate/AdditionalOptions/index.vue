f49c88a3596a9a33cb4a819fbc93dfc42a4f5510
Alex Soto <alex.soto@pingidentity.com>
2026-07-06 18:55:39 -0400

    IGA-4654 escalation email error is preventing form navigation

--- packages/platform-shared/src/views/Governance/Certification/Templates/EditTemplate/AdditionalOptions/index.vue
+++ packages/platform-shared/src/views/Governance/Certification/Templates/EditTemplate/AdditionalOptions/index.vue
@@ -1,8 +1,7 @@
-<!-- Copyright 2023-2025 ForgeRock AS. All Rights Reserved
+<!-- Copyright (c) 2023-2026 ForgeRock. All rights reserved.
 
-Use of this code requires a commercial software license with ForgeRock AS
-or with one of its affiliates. All use shall be exclusively subject
-to such license between the licensee and ForgeRock AS. -->
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details. -->
 <template>
   <div class="p-4 flex-grow-1 overflow-auto h-100">
     <BContainer
@@ -250,7 +249,7 @@ to such license between the licensee and ForgeRock AS. -->
               style="min-width: 200px;"
               testid="escalation-action"
               type="select"
-              validation="required"
+              :validation="formFields.escalation ? 'required' : ''"
               :options="escalationActionOptions"
               :searchable="false" />
             <FrField
@@ -260,7 +259,7 @@ to such license between the licensee and ForgeRock AS. -->
               style="min-width: 200px;"
               testid="escalation-email"
               type="select"
-              validation="required"
+              :validation="formFields.escalation ? 'required' : ''"
               :label="$t('governance.editTemplate.emailTemplate')"
               :options="emailTemplateOptions"
               :searchable="false" />
